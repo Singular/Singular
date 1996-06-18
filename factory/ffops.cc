02af91814dd18b74411df95652a7b867df115cd8
Rüdiger Stobbe <stobbe@mathematik.uni-kl.de>
1996-06-18 06:57:00 +0000

    "Now the functionality to handle prime numbers that are bigger than 2^15. This results in various changes and some new functions. "
    git-svn-id: file:///usr/local/Singular/svn/trunk@21 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/ffops.cc
+++ factory/ffops.cc
@@ -1,8 +1,11 @@
 // emacs edit mode for this file is -*- C++ -*-
-// $Id: ffops.cc,v 1.0 1996-05-17 10:59:46 stobbe Exp $
+// $Id: ffops.cc,v 1.1 1996-06-18 06:57:00 stobbe Exp $
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.0  1996/05/17 10:59:46  stobbe
+Initial revision
+
 */
 
 #include "assert.h"
@@ -11,30 +14,51 @@ $Log: not supported by cvs2svn $
 
 int ff_prime = 31991;
 int ff_halfprime = 31991 / 2;
+bool ff_big = false;
 short * ff_invtab = new short [31991];
 
 void ff_setprime ( const int p )
 {
-  ff_prime = p;
-  ff_halfprime = ff_prime / 2;
-  for ( int i = 0; i < ff_prime; i++ ) ff_invtab[i] = 0;
+    ff_prime = p;
+    ff_halfprime = ff_prime / 2;
+    if ( ! ff_big )
+	for ( int i = 0; i < ff_prime; i++ ) ff_invtab[i] = 0;
 }
 
 int ff_newinv ( const int a )
 {
-  int u, r0 = a, r1 = ff_prime, q0 = 1, q1 = 0;
-  while ( ( r0 > 0 ) && ( r1 > 0 ) ) {
-    u = r0 / r1;
-    r0 = r0 % r1;
-    q0 = u*q1 + q0;
-    if ( r0 > 0 ) {
-      u = r1 / r0;
-      r1 = r1 % r0;
-      q1 = u*q0 + q1;
+    int u, r0 = a, r1 = ff_prime, q0 = 1, q1 = 0;
+    while ( ( r0 > 0 ) && ( r1 > 0 ) ) {
+	u = r0 / r1;
+	r0 = r0 % r1;
+	q0 = u*q1 + q0;
+	if ( r0 > 0 ) {
+	    u = r1 / r0;
+	    r1 = r1 % r0;
+	    q1 = u*q0 + q1;
+	}
     }
-  }
-  if ( r0 == 0 )
-    return (ff_invtab[a] = -q1);
-  else
-    return (ff_invtab[a] = q0);
+    if ( r0 == 0 )
+	return (ff_invtab[a] = -q1);
+    else
+	return (ff_invtab[a] = q0);
+}
+
+int ff_biginv ( const int a )
+{
+    int u, r0 = a, r1 = ff_prime, q0 = 1, q1 = 0;
+    while ( ( r0 > 0 ) && ( r1 > 0 ) ) {
+	u = r0 / r1;
+	r0 = r0 % r1;
+	q0 = u*q1 + q0;
+	if ( r0 > 0 ) {
+	    u = r1 / r0;
+	    r1 = r1 % r0;
+	    q1 = u*q0 + q1;
+	}
+    }
+    if ( r0 == 0 )
+	return -q1;
+    else
+	return q0;
 }
--- factory/ffops.h
+++ factory/ffops.h
@@ -1,11 +1,14 @@
 // emacs edit mode for this file is -*- C++ -*-
-// $Id: ffops.h,v 1.0 1996-05-17 10:59:40 stobbe Exp $
+// $Id: ffops.h,v 1.1 1996-06-18 06:57:00 stobbe Exp $
 
 #ifndef INCL_FFOPS_H
 #define INCL_FFOPS_H
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.0  1996/05/17 10:59:40  stobbe
+Initial revision
+
 */
 
 #include "cf_globals.h"
@@ -14,17 +17,19 @@ $Log: not supported by cvs2svn $
 extern int ff_prime;
 extern int ff_halfprime;
 extern short * ff_invtab;
+extern bool ff_big;
 
 int ff_newinv ( const int );
+int ff_biginv ( const int );
 void ff_setprime ( const int );
 
 inline int ff_norm ( const int a )
 {
-  int n = a % ff_prime;
-  if ( n < 0 )
-    return n + ff_prime;
-  else
-    return n;
+    int n = a % ff_prime;
+    if ( n < 0 )
+	return n + ff_prime;
+    else
+	return n;
 }
 
 inline int ff_symmetric( const int a )
@@ -37,51 +42,62 @@ inline int ff_symmetric( const int a )
 
 inline int ff_longnorm ( const long a )
 {
-  int n = (int)(a % (long)ff_prime);
-  if ( n < 0 )
-    return n + ff_prime;
-  else
-    return n;
+    int n = (int)(a % (long)ff_prime);
+    if ( n < 0 )
+	return n + ff_prime;
+    else
+	return n;
+}
+
+inline int ff_bignorm ( const long long int a )
+{
+    int n = (int)(a % (long long int)ff_prime);
+    if ( n < 0 )
+	return n + ff_prime;
+    else
+	return n;
 }
 
 inline int ff_add ( const int a, const int b )
 {
-  return ff_norm( a + b );
+    return ff_norm( a + b );
 }
 
 inline int ff_sub ( const int a, const int b )
 {
-  return ff_norm( a - b );
+    return ff_norm( a - b );
 }
 
 inline int ff_neg ( const int a )
 {
-  return ff_norm( -a );
+    return ff_norm( -a );
 }
 
 inline int ff_mul ( const int a, const int b )
 {
-  return ff_longnorm ( (long)a * (long)b );
+    if ( ff_big )
+	return ff_bignorm( (long long int)a * (long long int)b );
+    else
+	return ff_longnorm ( (long)a * (long)b );
 }
 
 inline int ff_inv ( const int a )
 {
-  register b;
-  if ( (b = (int)(ff_invtab[a])) )
-    return b;
-  else
-    return ff_newinv( a );
+    if ( ff_big )
+	return ff_biginv( a );
+    else {
+	register b;
+	if ( (b = (int)(ff_invtab[a])) )
+	    return b;
+	else
+	    return ff_newinv( a );
+    }
+    
 }
 
 inline int ff_div ( const int a, const int b )
 {
-  return ff_mul( a, ff_inv( b ) );
+    return ff_mul( a, ff_inv( b ) );
 }
 
 #endif /* INCL_FFOPS_H */
-
-
-
-
-
-
