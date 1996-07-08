995f1cc5ea98223893365bb03608bc85b99ade76
Rüdiger Stobbe <stobbe@mathematik.uni-kl.de>
1996-07-08 08:16:06 +0000

    "New definition of the level system. "
    git-svn-id: file:///usr/local/Singular/svn/trunk@36 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_defs.h
+++ factory/cf_defs.h
@@ -1,11 +1,15 @@
 // emacs edit mode for this file is -*- C++ -*-
-// $Id: cf_defs.h,v 1.1 1996-07-02 11:18:57 stobbe Exp $
+// $Id: cf_defs.h,v 1.2 1996-07-08 08:16:06 stobbe Exp $
 
 #ifndef INCL_CF_DEFS_H
 #define incl_cf_defs_H
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.1  1996/07/02 11:18:57  stobbe
+"new switch SW_USE_EZGCD.
+"
+
 Revision 1.0  1996/05/17 10:59:38  stobbe
 Initial revision
 
@@ -13,12 +17,12 @@ Initial revision
 
 #include "conf.h"
 
-//#define HAS_ARITHMETIC_SHIFT
-
 /*BEGINPUBLIC*/
 
-#define LEVELBASE -32000
-#define LEVELQUOT 32000
+#define LEVELBASE -1000000
+#define LEVELTRANS -500000
+#define LEVELQUOT 1000000
+#define LEVELEXPR 1000001
 
 #define UndefinedDomain 32000
 #define PrimePowerDomain 5
