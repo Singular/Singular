ac7e5386431b3de12f3a61dca67e05be29296476
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-03-26 16:42:30 +0000

    version string added
    git-svn-id: file:///usr/local/Singular/svn/trunk@84 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_globals.cc
+++ factory/cf_globals.cc
@@ -1,13 +1,18 @@
 // emacs editmode for this file is -*- C++ -*-
-// $Id: cf_globals.cc,v 1.0 1996-05-17 10:59:43 stobbe Exp $
+// $Id: cf_globals.cc,v 1.1 1997-03-26 16:41:24 schmidt Exp $
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.0  1996/05/17 10:59:43  stobbe
+Initial revision
+
 */
 
 #include "assert.h"
+
 #include "cf_defs.h"
+
 #include "cf_switches.h"
 
+extern const char factoryVersion[] = "@(#) factoryVersion = 1.0";
 CFSwitches cf_glob_switches;
-
--- factory/cf_globals.h
+++ factory/cf_globals.h
@@ -1,15 +1,24 @@
 // emacs editmode for this file is -*- C++ -*-
-// $Id: cf_globals.h,v 1.0 1996-05-17 10:59:38 stobbe Exp $
+// $Id: cf_globals.h,v 1.1 1997-03-26 16:42:30 schmidt Exp $
 
 #ifndef INCL_CF_GLOBALS_H
 #define INCL_CF_GLOBALS_H
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.0  1996/05/17 10:59:38  stobbe
+Initial revision
+
 */
 
 #include "cf_switches.h"
 
 extern CFSwitches cf_glob_switches;
+
+/*BEGINPUBLIC*/
+
+extern const char factoryVersion[];
+
+/*ENDPUBLIC*/
 
 #endif
