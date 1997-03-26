bc428ca07201e22a987b0ca4d72389c736477dc8
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-03-26 16:20:48 +0000

    NDEBUG renamed to NOASSERT ASSERT1 added DEBUGOUTPUT-functionality moved to debug.h WARN- and PVIRT-functionality added
    git-svn-id: file:///usr/local/Singular/svn/trunk@79 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/assert.h
+++ factory/assert.h
@@ -1,57 +1,87 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: assert.h,v 1.1 1996-12-05 18:19:31 schmidt Exp $ */
+/* $Id: assert.h,v 1.2 1997-03-26 16:20:48 schmidt Exp $ */
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.1  1996/12/05 18:19:31  schmidt
+``Unconditional'' check-in.
+Now it is my turn to develop factory.
+
 Revision 1.0  1996/05/17 10:59:37  stobbe
 Initial revision
 
 */
 
-/* It should be possible to include this file multiple times for different */
-/* settings of NDEBUG */
+/* This is for backward compatibility only */
+#if defined (NDEBUG) && ! defined (NOASSERT)
+#define NOASSERT
+#endif
 
-#undef ASSERT
+/* It should be possible to include this file multiple times for different */
+/* settings of NOASSERT */
+
 #undef __ASSERT
+#undef __ASSERT1
 #undef STICKYASSERT
-#undef DEBOUT
-#undef DEBOUTLN
+#undef ASSERT
+#undef ASSERT1
+
+#undef __WARN
+#undef STICKYWARN
+#undef WARN
+
+#undef PVIRT_VOID
+#undef PVIRT_INTCF
+#undef PVIRT_BOOL
+#undef PVIRT_INT
+#undef PVIRT_CHARCC
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifdef NDEBUG
-#define ASSERT(ignore1,ignore2)((void)0)
-#else
-#define ASSERT(expression,message) \
+#define __ASSERT(expression, message, file, line) \
+(fprintf( stderr, "error: " message "\n%s:%u: failed assertion `%s'\n", \
+ file, line, expression ), abort(), 0 )
+#define __ASSERT1(expression, message, parameter1, file, line)  \
+(fprintf( stderr, "error: " message "\n%s:%u: failed assertion `%s'\n", \
+ parameter1, file, line, expression ), abort(), 0 )
+#define STICKYASSERT(expression, message) \
 ((void)((expression) ? 0 : __ASSERT(#expression, message, __FILE__, __LINE__)))
-#endif
 
-#define STICKYASSERT(expression,message) \
+#define __WARN(expression, message, file, line)  \
+(fprintf( stderr, "warning: " message "\n%s:%u: failed assertion `%s'\n", \
+ file, line, expression ), 0 )
+#define STICKYWARN(expression, message) \
+((void)((expression) ? 0 : __WARN(#expression, message, __FILE__, __LINE__)))
+
+#ifndef NOASSERT
+#define ASSERT(expression, message) \
 ((void)((expression) ? 0 : __ASSERT(#expression, message, __FILE__, __LINE__)))
+#define ASSERT1(expression, message, parameter1) \
+((void)((expression) ? 0 : __ASSERT1(#expression, message, parameter1, __FILE__, __LINE__)))
 
-#define __ASSERT(expression, message, file, line)  \
-(fprintf( stderr, "error: %s\n%s:%u: failed assertion `%s'\n", \
- message, file, line, expression ), abort(), 0 )
+#define WARN(expression, message) \
+((void)((expression) ? 0 : __WARN(#expression, message, __FILE__, __LINE__)))
 
-#ifdef DEBUGOUTPUT
-void deb_inc_level();
-void deb_dec_level();
-extern char * deb_level_msg;
-#define DEBINCLEVEL(stream,msg) \
-(stream << deb_level_msg << "entering << " << msg << " >>" << endl, deb_inc_level())
-#define DEBDECLEVEL(stream,msg) \
-(deb_dec_level(), stream << deb_level_msg << "leaving <<" << msg << " >>" << endl)
-#define DEBOUTSL(stream) \
-(stream << deb_level_msg, stream.flush())
-#define DEBOUT(stream,msg,object) \
-(stream << msg << object, stream.flush())
-#define DEBOUTLN(stream,msg,object) \
-(stream << deb_level_msg << msg << object << endl)
-#else
-#define DEBINCLEVEL(stream,msg)
-#define DEBDECLEVEL(stream,msg)
-#define DEBOUTSL(stream)
-#define DEBOUT(stream,msg,object)
-#define DEBOUTLN(stream,msg,object)
-#endif
+#define PVIRT_VOID(msg) \
+{ fprintf( stderr, "pure method( " msg " ) called\n" ); abort(); }
+#define PVIRT_INTCF(msg) \
+{ fprintf( stderr, "pure method( " msg " ) called\n" ); abort(); return 0; }
+#define PVIRT_BOOL(msg) \
+{ fprintf( stderr, "pure method( " msg " ) called\n" ); abort(); return false; }
+#define PVIRT_INT(msg) \
+{ fprintf( stderr, "pure method( " msg " ) called\n" ); abort(); return 0; }
+#define PVIRT_CHARCC(msg) \
+{ fprintf( stderr, "pure method( " msg " ) called\n" ); abort(); return 0; }
+#else /* NOASSERT */
+#define ASSERT(expression, message)
+#define ASSERT1(expression, message, parameter1)
+
+#define WARN(expression, message)
+
+#define PVIRT_VOID(msg) = 0
+#define PVIRT_INTCF(msg) = 0
+#define PVIRT_BOOL(msg) = 0
+#define PVIRT_INT(msg) = 0
+#define PVIRT_CHARCC(msg) = 0
+#endif /* NOASSERT */
