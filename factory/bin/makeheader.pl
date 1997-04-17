273838a4c14a4bdcb08d523aae935d8d49640795
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-04-17 14:31:30 +0000

    o makeheader.pl reads new format now
    git-svn-id: file:///usr/local/Singular/svn/trunk@170 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/bin/makeheader.pl
+++ factory/bin/makeheader.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
-# $ Id: $
+# $Id: makeheader.pl,v 1.2 1997-04-17 14:31:30 schmidt Exp $
 #
-# makeheader - generate a header file out of several header file (ver 1.2).
+# makeheader.pl - generate a header file out of several header file (ver 1.2).
 #
 
 #
@@ -26,12 +26,24 @@ With makeheader it is possible to generate a single header
 file out of several other header files.
 
 To include a header file into the generated header file
-\(= <outfile>) use the command `.INCLUDE <includefile>' on a
-single line in <templatefile>.
-This line is replaced by sections marked with /*BEGINPUBLIC*/
-and /*ENDPUBLIC*/ from <includefile>. <includefile> is looked up
-in all <includedir>s, then in the current directory, then in
-the base directory of <templatefile>.\n";
+\(= <outfile>) use the two lines 
+
+/*MAKEHEADER*/
+#include \"<includefile>\"
+
+in <templatefile>.
+
+If you use the alternate form
+
+/*MAKEHEADER PUBLIC ONLY*/
+#include \"<includefile>\"
+
+only sections marked with /*BEGINPUBLIC*/ and /*ENDPUBLIC*/ are
+pasted from <includefile> into <outfile>.
+
+<includefile> is looked up in all <includedir>s, then in the
+current directory, then in the base directory of
+<templatefile>.\n";
 }
 
 # get list of include directories
@@ -61,22 +73,41 @@ open( OUTFILE, ">$outfile" ) ||
     die "makeheader: Can not open header file $outfile for writing!\n";
 
 while( <INFILE> ) {
-    if ( /^\.INCLUDE/ ) {
-	s/^\.INCLUDE //;
-	chop;
-	$inclfile = &searchpath( $_, $includes ) ||
-	    die "makeheader: Can not open included file $_ for reading!\n";
-	open( INCLFILE, $inclfile ) ||
-	    die "makeheader: Can not open included file $_ for reading!\n";
+    if ( /^\/\*MAKEHEADER(.*)\*\/$/ ) {
+	# check whether we have to read public part only
+	if ( $1 eq " PUBLIC ONLY" ) {
+	    $public_only = 1;
+	} else {
+	    $public_only = 0;
+	}
 
-	print OUTFILE "/* Stuff from $_ */";
-	while ( <INCLFILE> ) {
-	    if ( /\/\*BEGINPUBLIC\*\// ) {
-		while ( <INCLFILE> ) {
-		    last if ( /\/\*ENDPUBLIC\*\// );
-		    print OUTFILE $_;
+	# get name of file to include
+	$_ = <INFILE>; chop;
+	if ( /^\#include \"(.*)\"$/ ) {
+	    $inclfile = $1;
+	} else {
+	    die "makeheader: $_ is not a valid include directive!\n";
+	}
+
+	$inclfile = &searchpath( $inclfile, $includes ) ||
+	    die "makeheader: Can not open included file $inclfile for reading!\n";
+	open( INCLFILE, $inclfile ) ||
+	    die "makeheader: Can not open included file $inclfile for reading!\n";
+
+	print OUTFILE "\n/* Stuff included from $inclfile */\n";
+	if ( $public_only ) {
+	    # read public part only
+	    while ( <INCLFILE> ) {
+		if ( /^\/\*BEGINPUBLIC\*\/$/ ) {
+		    while ( <INCLFILE> ) {
+			last if ( /^\/\*ENDPUBLIC\*\/$/ );
+			print OUTFILE $_;
+		    }
 		}
 	    }
+	} else {
+	    # just paste contents into output
+	    while ( <INCLFILE> ) { print OUTFILE $_; }
 	}
 	close INCLFILE;
     }
