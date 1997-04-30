699a1847b77bff0ee5e4ed7bdd8d0188f12c1973
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-04-30 12:20:28 +0000

    o version string includes VERSION from config.h now
    git-svn-id: file:///usr/local/Singular/svn/trunk@209 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_globals.cc
+++ factory/cf_globals.cc
@@ -1,8 +1,11 @@
 // emacs editmode for this file is -*- C++ -*-
-// $Id: cf_globals.cc,v 1.2 1997-04-07 15:08:12 schmidt Exp $
+// $Id: cf_globals.cc,v 1.3 1997-04-30 12:20:28 schmidt Exp $
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.2  1997/04/07 15:08:12  schmidt
+#include <config.h> added
+
 Revision 1.1  1997/03/26 16:41:24  schmidt
 version string added
 
@@ -18,5 +21,6 @@ Initial revision
 #include "cf_defs.h"
 #include "cf_switches.h"
 
-extern const char factoryVersion[] = "@(#) factoryVersion = 1.0";
+// VERSION is a macro defined in config.h
+extern const char factoryVersion[] = "@(#) factoryVersion = " VERSION;
 CFSwitches cf_glob_switches;
