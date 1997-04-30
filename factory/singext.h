b03713bf687d66c6bc0239eaae0c103dba983a51
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-04-30 12:48:57 +0000

    o some declarations of Singular objects added
    git-svn-id: file:///usr/local/Singular/svn/trunk@212 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/singext.h
+++ factory/singext.h
@@ -1,11 +1,14 @@
 // emacs edit mode for this file is -*- C++ -*-
-// $Id: singext.h,v 1.1 1997-04-15 09:38:18 schmidt Exp $
+// $Id: singext.h,v 1.2 1997-04-30 12:48:57 schmidt Exp $
 
 #ifndef INCL_SINGEXT_H
 #define INCL_SINGEXT_H
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.1  1997/04/15 09:38:18  schmidt
+#include <config.h> added
+
 Revision 1.0  1996/05/17 10:59:42  stobbe
 Initial revision
 
@@ -16,6 +19,14 @@ Initial revision
 #include "cf_gmp.h"
 
 #include "canonicalform.h"
+
+
+// we need this to copy singulars GF(q) table to ours
+extern int nfCharQ;
+extern int nfM1;
+extern int nfMinPoly[];
+extern short nfPlus1Table[];
+
 
 /*BEGINPUBLIC*/
 
