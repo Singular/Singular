d51fb303e2d611a2c499fc37b5f79f7d703426fa
Hans Schönemann <hannes@mathematik.uni-kl.de>
1997-06-11 12:06:47 +0000

    * hannes: added factor/interrupt.h
    git-svn-id: file:///usr/local/Singular/svn/trunk@384 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ libfac/factor/interrupt.h
@@ -0,0 +1,27 @@
+/* Copyright 1997 Michael Messollen. All rights reserved. */
+///////////////////////////////////////////////////////////////////////////////
+#ifndef INTERRUPT_H
+#define INTERRUPT_H
+// FACTORY - Includes
+#include <factory.h>
+// Factor - Includes
+#include "tmpl_inst.h"
+
+/*BEGINPUBLIC*/
+// Set this to a nonzero value to interrupt the computation
+extern int libfac_interruptflag;
+/*ENDPUBLIC*/
+
+inline int
+interrupt_handle( ){
+
+  if ( libfac_interruptflag ){ return 1; }
+  return 0;
+}
+
+#endif /* INTERRUPT_H */
+
+/*
+$Log: not supported by cvs2svn $
+
+*/
