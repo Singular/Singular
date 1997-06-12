3e4591d94bb44722a8e9aa61957fca49e4d3b092
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-06-12 12:28:28 +0000

    o doc fix
    git-svn-id: file:///usr/local/Singular/svn/trunk@387 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_globals.cc
+++ factory/cf_globals.cc
@@ -1,8 +1,11 @@
 // emacs editmode for this file is -*- C++ -*-
-// $Id: cf_globals.cc,v 1.4 1997-05-02 08:56:11 schmidt Exp $
+// $Id: cf_globals.cc,v 1.5 1997-06-12 12:28:28 schmidt Exp $
 
 /*
 $Log: not supported by cvs2svn $
+Revision 1.4  1997/05/02 08:56:11  schmidt
+o reference to #define VERSION replaced by FACTORYVERSION
+
 Revision 1.3  1997/04/30 12:20:28  schmidt
 o version string includes VERSION from config.h now
 
@@ -24,6 +27,6 @@ Initial revision
 #include "cf_defs.h"
 #include "cf_switches.h"
 
-// VERSION is a macro defined in config.h
+// FACTORYVERSION is a macro defined in config.h
 extern const char factoryVersion[] = "@(#) factoryVersion = " FACTORYVERSION;
 CFSwitches cf_glob_switches;
