f8179c83473baba6847100fd00e410c494e5f07e
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-06-19 12:45:45 +0000

    Initial revision
    git-svn-id: file:///usr/local/Singular/svn/trunk@409 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ factory/ftmpl_inst.cc
@@ -0,0 +1,49 @@
+/* emacs edit mode for this file is -*- C++ -*- */
+/* $Id: ftmpl_inst.cc,v 1.1 1997-06-19 12:45:45 schmidt Exp $ */
+
+#include <factoryconf.h>
+
+#include <templates/array.cc>
+#include <templates/factor.cc>
+#include <templates/list.cc>
+#include <templates/functions.h>
+#include <templates/matrix.cc>
+
+#include <factory.h>
+
+template class Factor<CanonicalForm>;
+template class List<CFFactor>;
+template class ListIterator<CFFactor>;
+template class List<CanonicalForm>;
+template class ListIterator<CanonicalForm>;
+template class Array<CanonicalForm>;
+template class List<MapPair>;
+template class ListIterator<MapPair>;
+template class Matrix<CanonicalForm>;
+template class SubMatrix<CanonicalForm>;
+template class Array<REvaluation>;
+
+#ifndef NOSTREAMIO
+template ostream & operator << ( ostream &, const List<CanonicalForm> & );
+template ostream & operator << ( ostream &, const List<CFFactor> & );
+template ostream & operator << ( ostream &, const List<MapPair> & );
+template ostream & operator << ( ostream &, const Array<CanonicalForm> & );
+template ostream & operator << ( ostream &, const Factor<CanonicalForm> & );
+template ostream & operator << ( ostream &, const Matrix<CanonicalForm> & );
+template ostream & operator << ( ostream &, const Array<REvaluation> & );
+#endif /* NOSTREAMIO */
+
+template List<CFFactor> Union ( const List<CFFactor>&, const List<CFFactor>& );
+
+/*
+template CanonicalForm crossprod ( const Array<CanonicalForm>&, const Array<CanonicalForm>& );
+template CanonicalForm prod ( const Array<CanonicalForm>& );
+*/
+
+template CanonicalForm tmax ( const CanonicalForm&, const CanonicalForm& );
+template CanonicalForm tmin ( const CanonicalForm&, const CanonicalForm& );
+
+template int tmax ( const int&, const int& );
+template int tmin ( const int&, const int& );
+
+// place here your own template stuff, not instantiated by factory
