1162cb4693b29efee996dff440178aa922d450a5
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-15 13:01:08 +0000

    * sm_util.h (REArray): new typedef.  All references to 	  Array<REvaluation> changed to REArray.
    	* sm_util.h: doc fix
    
    	* sm_util.h (dinterpol): bug fix of declaration
    
    
    git-svn-id: file:///usr/local/Singular/svn/trunk@516 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/sm_util.h
+++ factory/sm_util.h
@@ -1,14 +1,16 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: sm_util.h,v 1.1 1997-07-14 15:11:08 schmidt Exp $ */
+/* $Id: sm_util.h,v 1.2 1997-07-15 13:01:08 schmidt Exp $ */
 
 #ifndef INCL_SM_UTIL_H
 #define INCL_SM_UTIL_H
 
-\\{{{ docu
-\\
-\\ sm_util.h - header to sm_util.cc.
-\\
-\\}}}
+//{{{ docu
+//
+// sm_util.h - header to sm_util.cc.
+//
+// Contributed by Marion Bruder <bruder@math.uni-sb.de>.
+//
+//}}}
 
 #include <config.h>
 
@@ -20,14 +22,16 @@
 #include "templates/ftmpl_array.h"
 #endif
 
+typedef REArray Array<REvaluation>;
+
 int countmonome( const CanonicalForm & f );
 
 CanonicalForm Leitkoeffizient( const CanonicalForm & f );
 
 void ChinesePoly( int arraylength, const CFArray & Polys, const CFArray & primes, CanonicalForm & result );
 
-CanonicalForm dinterpol( int d, const CanonicalForm & gi, const CFArray & zwischen, const REvaluation & alpha, int s, const CFArray & beta, int ni, int CHAR )
+CanonicalForm dinterpol( int d, const CanonicalForm & gi, const CFArray & zwischen, const REvaluation & alpha, int s, const CFArray & beta, int ni, int CHAR );
 
-CanonicalForm sinterpol( const CanonicalForm & gi, const Array<REvaluation> & xi, CanonicalForm* zwischen, int n );
+CanonicalForm sinterpol( const CanonicalForm & gi, const REArray & xi, CanonicalForm* zwischen, int n );
 
 #endif /* ! INCL_SM_UTIL_H */
