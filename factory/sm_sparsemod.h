57c38ea02933cac84d7da5f16099762c889d0a05
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-15 13:02:49 +0000

    Initial revision
    git-svn-id: file:///usr/local/Singular/svn/trunk@517 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ factory/sm_sparsemod.h
@@ -0,0 +1,21 @@
+/* emacs edit mode for this file is -*- C++ -*- */
+/* $Id: sm_sparsemod.h,v 1.1 1997-07-15 13:01:49 schmidt Exp $ */
+
+#ifndef INCL_SM_SPARSEMOD_H
+#define INCL_SM_SPARSEMOD_H
+
+//{{{ docu
+//
+// sm_sparsemod.h - header to sm_sparsemod.cc.
+//
+// Contributed by Marion Bruder <bruder@math.uni-sb.de>.
+//
+//}}}
+
+#include <config.h>
+
+#include "canonicalform.h"
+
+CanonicalForm sparsemod( const CanonicalForm & F, const CanonicalForm & G );
+
+#endif /* ! INCL_SM_SPARSEMOD_H */
