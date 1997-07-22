70dbb3c0bafdc80a4ac1399a008260966ba1bd84
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-22 13:57:49 +0000

    * cf_switches.cc: doc fix 	(CFSwitches): doc fix
    	* cf_switches.cc (CFSwitches): number of switches replaced by
    	  references to 'const int CFSwitchesMax'
    
    
    git-svn-id: file:///usr/local/Singular/svn/trunk@570 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_switches.cc
+++ factory/cf_switches.cc
@@ -1,15 +1,29 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_switches.cc,v 1.2 1997-06-19 12:23:55 schmidt Exp $ */
+/* $Id: cf_switches.cc,v 1.3 1997-07-22 13:57:49 schmidt Exp $ */
+
+//{{{ docu
+//
+// cf_switches.cc - definition of class CFSwitches.
+//
+// Used by: cf_globals.cc
+//
+//}}}
 
 #include <config.h>
 
-#include "assert.h"
-
-#include "cf_defs.h"
 #include "cf_switches.h"
 
-CFSwitches::CFSwitches ( )
+//{{{ CFSwitches::CFSwitches()
+//{{{ docu
+//
+// CFSwitches::CFSwitches() - default constructor.
+//
+// Turns all switches off.
+//
+//}}}
+CFSwitches::CFSwitches()
 {
-    for ( int i = 0; i < 10; i++ )
+    for ( int i = 0; i < CFSwitchesMax; i++ )
 	switches[i] = false;
 }
+//}}}
