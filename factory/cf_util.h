4b7b156e121f371053a1c0985f2c4402047acb33
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-22 10:16:58 +0000

    * cf_util.h: doc fix
    git-svn-id: file:///usr/local/Singular/svn/trunk@563 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_util.h
+++ factory/cf_util.h
@@ -1,8 +1,14 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_util.h,v 1.2 1997-06-19 12:23:51 schmidt Exp $ */
+/* $Id: cf_util.h,v 1.3 1997-07-22 10:16:58 schmidt Exp $ */
 
 #ifndef INCL_CF_UTIL_H
 #define INCL_CF_UTIL_H
+
+//{{{ docu
+//
+// cf_util.h - header to cf_util.cc.
+//
+//}}}
 
 #include <config.h>
 
