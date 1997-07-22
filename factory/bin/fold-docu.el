25315fc6eb9551d9354ca252d642c1acb3b5c5de
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-22 13:41:32 +0000

    * bin/fold-docu.el (fold-docu): works now for all fold marks
    git-svn-id: file:///usr/local/Singular/svn/trunk@569 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/bin/fold-docu.el
+++ factory/bin/fold-docu.el
@@ -1,9 +1,11 @@
 ;; emacs edit mode for this file is -*- Emacs-Lisp -*-
-;; $Id: fold-docu.el,v 1.1 1997-07-22 13:16:26 schmidt Exp $
+;; $Id: fold-docu.el,v 1.2 1997-07-22 13:41:32 schmidt Exp $
 
 ;;{{{ docu
 ;;
 ;; fold-docu.el - fold all documentation foldings in a file.
+;;
+;; To unfold all documentation, call fold-open-buffer.
 ;;
 ;;}}}
 
@@ -14,7 +16,7 @@
   (interactive)
   (goto-char (point-min))
   (save-excursion
-    (while (re-search-forward "//{{{ .*docu$" nil t)
+    (while (re-search-forward "{{{ .*docu$" nil t)
       (fold-hide))))
 
 (provide 'fold-docu)
