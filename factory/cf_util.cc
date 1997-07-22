26186fd0eb4dc9a4ce1571f230416bc7b48f976a
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-22 10:17:19 +0000

    * cf_util.cc: doc fix 	  (ipower): doc fix
    git-svn-id: file:///usr/local/Singular/svn/trunk@564 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_util.cc
+++ factory/cf_util.cc
@@ -1,15 +1,27 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_util.cc,v 1.2 1997-06-19 12:23:52 schmidt Exp $ */
+/* $Id: cf_util.cc,v 1.3 1997-07-22 10:17:19 schmidt Exp $ */
+
+//{{{ docu
+//
+// cf_util.cc - miscellaneous functions, not necessarily related
+//   to canonical forms.
+//
+//}}}
 
 #include <config.h>
 
-#include "assert.h"
-
-#include "cf_defs.h"
 #include "cf_util.h"
 
-
-int ipower ( int b, int m )
+//{{{ int ipower ( int b, int m )
+//{{{ docu
+//
+// ipower() - calculate b^m in standard integer arithmetic.
+//
+// Note: Beware of overflows.
+//
+//}}}
+int
+ipower ( int b, int m )
 {
     int prod = 1;
 
@@ -22,3 +34,4 @@ int ipower ( int b, int m )
     }
     return prod;
 }
+//}}}
