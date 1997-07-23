e743b32f0ead428621a515ddad45bdd5134975a5
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-07-23 15:55:19 +0000

    * cf_switches.h: doc fix 	(class CFSwitches): doc fix
    	* cf_switches.h (CFSwitchesMax): new constant
    	* cf_switches.h (class CFSwitches): number of switches replaced by
    	  references to 'const int CFSwitchesMax'
    
    
    git-svn-id: file:///usr/local/Singular/svn/trunk@572 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_switches.h
+++ factory/cf_switches.h
@@ -1,24 +1,66 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_switches.h,v 1.3 1997-06-19 12:23:53 schmidt Exp $ */
+/* $Id: cf_switches.h,v 1.4 1997-07-23 15:55:19 schmidt Exp $ */
 
 #ifndef INCL_CF_SWITCHES_H
 #define INCL_CF_SWITCHES_H
 
+//{{{ docu
+//
+// cf_switches.h - header to cf_switches.cc.
+//
+//}}}
+
 #include <config.h>
 
-#include "cf_defs.h"
+//{{{ const int CFSwitchesMax
+//{{{ docu
+//
+// const CFSwitchesMax - maximum number of switches.
+//
+//}}}
+const int CFSwitchesMax = 10;
+//}}}
 
+//{{{ class CFSwitches
+//{{{ docu
+//
+// class CFSwitches - manages boolean switches.
+//
+// An object of type CFSwitches is simply an array of booleans
+// with some comfortable access methods (On, Off).  Each object
+// may contain CFSwitchesMax switches.  When a new object of type
+// CFSwitches is created, all its switches are turned off.
+//
+// Note: No range checking is done when accessing switches.
+//
+// switches: the switches
+//
+//}}}
+//{{{ inline method docu
+//
+// void On( int s )
+// void Off( int s )
+// bool isOn( int s )
+// bool isOff( int s )
+//
+// On(), Off() - switch s on or off, resp.
+//
+// isOn(), isOff() - return true iff s is on or off, resp.
+//
+//}}}
 class CFSwitches
 {
 private:
-    bool switches [10];
+    bool switches [CFSwitchesMax];
 public:
     CFSwitches();
     ~CFSwitches() {}
+
     void On( int s ) { switches[s] = true; }
     void Off( int s ) { switches[s] = false; }
     bool isOn( int s ) const { return switches[s]; }
     bool isOff( int s ) const { return ! switches[s]; }
 };
+//}}}
 
 #endif /* ! INCL_CF_SWITCHES_H */
