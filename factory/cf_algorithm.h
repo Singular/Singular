f42ba19439d9fd2dd585c5eb61afe26050567b07
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-09-01 08:59:53 +0000

    * cf_algorithm.h (subResChain): new declaration
    git-svn-id: file:///usr/local/Singular/svn/trunk@647 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_algorithm.h
+++ factory/cf_algorithm.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_algorithm.h,v 1.3 1997-08-29 11:30:38 schmidt Exp $ */
+/* $Id: cf_algorithm.h,v 1.4 1997-09-01 08:59:53 schmidt Exp $ */
 
 #ifndef INCL_CF_ALGORITHM_H
 #define INCL_CF_ALGORITHM_H
@@ -13,7 +13,7 @@
 // on canonical forms (factorization, gcd, etc.).
 //
 // This header file corresponds to:
-// cf_chinese.cc, cf_factor.cc, cf_linsys.cc
+// cf_chinese.cc, cf_factor.cc, cf_linsys.cc, cf_resultant.cc
 //
 //}}}
 
@@ -44,6 +44,10 @@ bool isSqrFree ( const CanonicalForm & f );
 bool linearSystemSolve( CFMatrix & M );
 
 CanonicalForm determinant( const CFMatrix & M, int n );
+//}}}
+
+//{{{ declarations from cf_resultant.cc
+CFArray subResChain ( const CanonicalForm & f, const CanonicalForm & g, Variable x );
 //}}}
 
 /*ENDPUBLIC*/
