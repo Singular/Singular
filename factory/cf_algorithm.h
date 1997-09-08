38eab603130b954c28838dc7d34d13ea7784b0ae
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-09-08 10:55:04 +0000

    * cf_algorithm.h: doc fix
    git-svn-id: file:///usr/local/Singular/svn/trunk@667 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_algorithm.h
+++ factory/cf_algorithm.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_algorithm.h,v 1.6 1997-09-04 15:21:57 schmidt Exp $ */
+/* $Id: cf_algorithm.h,v 1.7 1997-09-08 10:55:04 schmidt Exp $ */
 
 #ifndef INCL_CF_ALGORITHM_H
 #define INCL_CF_ALGORITHM_H
@@ -26,7 +26,7 @@
 
 /*BEGINPUBLIC*/
 
-//{{{ declarations from cf_algorithm.cc
+//{{{ function declarations from cf_algorithm.cc
 CanonicalForm psr ( const CanonicalForm & f, const CanonicalForm & g, const Variable & x );
 
 CanonicalForm psq ( const CanonicalForm & f, const CanonicalForm & g, const Variable & x );
@@ -36,13 +36,13 @@ void psqr ( const CanonicalForm & f, const CanonicalForm & g, CanonicalForm & q,
 CanonicalForm common_den ( const CanonicalForm & f );
 //}}}
 
-//{{{ declarations from cf_chinese.cc
+//{{{ function declarations from cf_chinese.cc
 void chineseRemainder ( const CanonicalForm x1, const CanonicalForm q1, const CanonicalForm x2, const CanonicalForm q2, CanonicalForm & xnew, CanonicalForm & qnew );
 
 void chineseRemainder ( const CFArray & x, const CFArray & q, CanonicalForm & xnew, CanonicalForm & qnew );
 //}}}
 
-//{{{ declarations from cf_factor.cc
+//{{{ function declarations from cf_factor.cc
 CFFList factorize ( const CanonicalForm & f, bool issqrfree = false );
 
 CFFList factorize ( const CanonicalForm & f, const Variable & alpha );
@@ -52,13 +52,13 @@ CFFList sqrFree ( const CanonicalForm & f, bool sort = false );
 bool isSqrFree ( const CanonicalForm & f );
 //}}}
 
-//{{{ declarations from cf_linsys.cc
+//{{{ function declarations from cf_linsys.cc
 bool linearSystemSolve ( CFMatrix & M );
 
 CanonicalForm determinant ( const CFMatrix & M, int n );
 //}}}
 
-//{{{ declarations from cf_resultant.cc
+//{{{ function declarations from cf_resultant.cc
 CFArray subResChain ( const CanonicalForm & f, const CanonicalForm & g, const Variable & x );
 
 CanonicalForm resultant ( const CanonicalForm & f, const CanonicalForm & g, const Variable & x );
