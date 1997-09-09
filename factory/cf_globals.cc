e6da16580a6a7ea5df19aa6083e6487b4e69477f
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-09-09 09:16:20 +0000

    * cf_globals.cc: #include fix, doc fix
    git-svn-id: file:///usr/local/Singular/svn/trunk@682 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_globals.cc
+++ factory/cf_globals.cc
@@ -1,13 +1,40 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_globals.cc,v 1.6 1997-06-19 12:27:00 schmidt Exp $ */
+/* $Id: cf_globals.cc,v 1.7 1997-09-09 09:16:20 schmidt Exp $ */
+
+//{{{ docu
+//
+// cf_globals.cc - definitions of global variables.
+//
+// Used by: canonicalform.cc, int_int.cc, ffops.h, imm.h
+//
+//}}}
 
 #include <config.h>
 
-#include "assert.h"
-
-#include "cf_defs.h"
 #include "cf_switches.h"
 
-// FACTORYVERSION is a macro defined in config.h
+//{{{ extern const char factoryVersion[];
+//{{{ docu
+//
+// factoryVersion - factory version string.
+//
+// factoryVersion is initialized from #define FACTORYVERSION
+// which is defined in config.h.  It is not used in factory
+// itself.
+//
+//}}}
 extern const char factoryVersion[] = "@(#) factoryVersion = " FACTORYVERSION;
+//}}}
+
+//{{{ CFSwitches cf_glob_switches;
+//{{{ docu
+//
+// cf_glob_switches - factory switches.
+//
+// This is the only object of type CFSwitches in factory.  It is
+// used either directly in the low level algorithms or by the
+// functions On(), Off(), isOn() defined in canonicalform.cc.
+//
+//}}}
 CFSwitches cf_glob_switches;
+//}}}
