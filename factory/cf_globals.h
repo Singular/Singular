edde26f369efc4485312ee39665fdc41db1da1df
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-09-09 09:21:02 +0000

    * cf_globals.h: doc fix
    git-svn-id: file:///usr/local/Singular/svn/trunk@683 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_globals.h
+++ factory/cf_globals.h
@@ -1,19 +1,25 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_globals.h,v 1.3 1997-06-19 12:24:29 schmidt Exp $ */
+/* $Id: cf_globals.h,v 1.4 1997-09-09 09:21:02 schmidt Exp $ */
 
 #ifndef INCL_CF_GLOBALS_H
 #define INCL_CF_GLOBALS_H
 
+//{{{ docu
+//
+// cf_globals.h - header to cf_globals.cc.
+//
+//}}}
+
 #include <config.h>
 
 #include "cf_switches.h"
-
-extern CFSwitches cf_glob_switches;
 
 /*BEGINPUBLIC*/
 
 extern const char factoryVersion[];
 
 /*ENDPUBLIC*/
+
+extern CFSwitches cf_glob_switches;
 
 #endif /* ! INCL_CF_GLOBALS_H */
