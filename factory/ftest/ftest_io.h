3abd3ce89c94f6fdff0cd6eb4861219bbe9e2283
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-09-29 13:09:43 +0000

    * ftest_io.cc (ftestPrintResult( .., CF ), ftestPrintResult( .., 	  int )): functions ftestPrintCanonicalForm() and ftestPrintInt() 	  renamed to ftestPrintResult.  All callers changed.  Decarations 	  adapted.
    git-svn-id: file:///usr/local/Singular/svn/trunk@754 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/ftest/ftest_io.h
+++ factory/ftest/ftest_io.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: ftest_io.h,v 1.1 1997-09-24 07:29:47 schmidt Exp $ */
+/* $Id: ftest_io.h,v 1.2 1997-09-29 13:09:43 schmidt Exp $ */
 
 #ifndef INCL_FTEST_IO_H
 #define INCL_FTEST_IO_H
@@ -15,7 +15,7 @@
 CanonicalForm ftestGetCanonicalForm ( const char * stringF );
 Variable ftestGetVariable ( const char * stringVariable );
 
-void ftestPrintCanonicalForm ( const char * resultName, const CanonicalForm & result );
-void ftestPrintInt ( const char * resultName, const int result );
+void ftestPrintResult ( const char * resultName, const CanonicalForm & result );
+void ftestPrintResult ( const char * resultName, const int result );
 
 #endif /* ! INCL_FTEST_IO_H */
