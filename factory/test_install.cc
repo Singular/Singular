a116adc2bbbe584724ed51a832945b9b08efe444
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-10-23 13:21:55 +0000

    * test_install.cc (main): prints factoryConfiguration
    git-svn-id: file:///usr/local/Singular/svn/trunk@839 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/test_install.cc
+++ factory/test_install.cc
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: test_install.cc,v 1.1 1997-10-21 11:24:44 schmidt Exp $ */
+/* $Id: test_install.cc,v 1.2 1997-10-23 13:21:55 schmidt Exp $ */
 
 //{{{ docu
 //
@@ -15,6 +15,7 @@ main ()
     // this will try to locate the GF(q) tables
     setCharacteristic( 2, 3, 'Z' );
     cout << factoryVersion << endl;
+    cout << factoryConfiguration << endl;
     cout << "Factory lives!!" << endl;
     return 0;
 }
