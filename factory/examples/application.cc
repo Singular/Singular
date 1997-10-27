97ff24ae31c55c79d9f90a23e5801466bab2ecd4
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-10-27 15:52:02 +0000

    Initial revision
    git-svn-id: file:///usr/local/Singular/svn/trunk@847 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ factory/examples/application.cc
@@ -0,0 +1,61 @@
+/* emacs edit mode for this file is -*- C++ -*- */
+/* $Id: application.cc,v 1.1 1997-10-27 15:52:02 schmidt Exp $ */
+
+//{{{ docu
+//
+// application.cc - sample Factory application.
+//
+//}}}
+
+#include <factory.h>
+
+int
+main()
+{
+    // test for existence of GF(q) tables
+    setCharacteristic( 3, 2, 'z' );
+    cout << factoryVersion << endl;
+    cout << "Factory lives!" << endl;
+
+    // now for something completely different
+    setCharacteristic( 0 );
+    On( SW_RATIONAL );
+
+    Variable x( 'x' );
+    Variable y( 'y' );
+    Variable z( 'z' );
+    CanonicalForm f;
+    CanonicalForm g;
+
+    // call some of Factory's functions and methods and print
+    // their results
+    cout << "Do not forget to terminate canonical forms by `;' in input!" << endl;
+
+    cout << "f(x, y, z) = ";
+    cin >> f;
+
+    cout << "g(x, y, z) = ";
+    cin >> g;
+
+    cout << "You entered:" << endl;
+    cout << "f         = " << f << endl;
+    cout << "g         = " << g << endl;
+
+    cout << "Some information on f:" << endl;
+    cout << "mvar(f)          = " << mvar( f ) << endl;
+    cout << "degree(f)        = " << degree( f ) << endl;
+    cout << "degree(f, x)     = " << degree( f, x ) << endl;
+    cout << "degree(f, y)     = " << degree( f, y ) << endl;
+    cout << "degree(f, z)     = " << degree( f, z ) << endl;
+    cout << "LC(f, x)         = " << LC( f, x ) << endl;
+    cout << "LC(f, y)         = " << LC( f, y ) << endl;
+    cout << "LC(f, z)         = " << LC( f, z ) << endl;
+
+    cout << "We test the arithmetic operators:" << endl;
+    cout << "f+g       = " << f + g << endl;
+    cout << "f-g       = " << f - g << endl;
+    cout << "f*g       = " << f * g << endl;
+    cout << "f/g       = " << f / g << endl;
+    cout << "f%g       = " << f % g << endl;
+    cout << "f(g, x)   = " << f(g, x) << endl;
+}
