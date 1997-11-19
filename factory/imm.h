07d8d413d240073deb88b3535db2bf4835c79712
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-11-19 11:37:29 +0000

    * imm.h (imm_sign): doc fix
    	* imm.h: functions reordered
    
    
    git-svn-id: file:///usr/local/Singular/svn/trunk@912 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/imm.h
+++ factory/imm.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: imm.h,v 1.6 1997-09-10 14:45:17 schmidt Exp $ */
+/* $Id: imm.h,v 1.7 1997-11-19 11:37:29 schmidt Exp $ */
 
 #ifndef INCL_IMM_H
 #define INCL_IMM_H
@@ -34,50 +34,7 @@ const long long int MINIMMEDIATELL = -268435454LL;
 const long long int MAXIMMEDIATELL = 268435454LL;
 #endif
 
-#ifdef HAS_ARITHMETIC_SHIFT
-
-inline int imm2int ( const InternalCF * const imm )
-{
-    return (int)imm >> 2;
-}
-
-inline InternalCF * int2imm ( int i )
-{
-    return (InternalCF*)((i << 2) | INTMARK );
-}
-
-#else
-
-inline int imm2int ( const InternalCF * const imm )
-{
-    // this could be better done by masking the sign bit
-    if ( (int)imm < 0 )
-	return -((-(int)imm) >> 2);
-    else
-	return (int)imm >> 2;
-}
-
-inline InternalCF * int2imm ( int i )
-{
-    if ( i < 0 )
-	return (InternalCF*)(-(((-i) << 2) | INTMARK));
-    else
-	return (InternalCF*)((i << 2) | INTMARK );
-}
-
-#endif
-
-
-inline InternalCF * int2imm_p ( int i )
-{
-    return (InternalCF*)((i << 2) | FFMARK );
-}
-
-inline InternalCF * int2imm_gf ( int i )
-{
-    return (InternalCF*)((i << 2) | GFMARK );
-}
-
+//{{{ predicates
 inline int is_imm ( const InternalCF * const ptr )
 {
     // returns 0 if ptr is not immediate
@@ -113,7 +70,111 @@ inline int imm_isone_gf ( const InternalCF * const ptr )
 {
     return gf_isone( imm2int( ptr ) );
 }
+//}}}
 
+//{{{ conversion functions
+#ifdef HAS_ARITHMETIC_SHIFT
+
+inline int imm2int ( const InternalCF * const imm )
+{
+    return (int)imm >> 2;
+}
+
+inline InternalCF * int2imm ( int i )
+{
+    return (InternalCF*)((i << 2) | INTMARK );
+}
+
+#else
+
+inline int imm2int ( const InternalCF * const imm )
+{
+    // this could be better done by masking the sign bit
+    if ( (int)imm < 0 )
+	return -((-(int)imm) >> 2);
+    else
+	return (int)imm >> 2;
+}
+
+inline InternalCF * int2imm ( int i )
+{
+    if ( i < 0 )
+	return (InternalCF*)(-(((-i) << 2) | INTMARK));
+    else
+	return (InternalCF*)((i << 2) | INTMARK );
+}
+
+#endif
+
+inline InternalCF * int2imm_p ( int i )
+{
+    return (InternalCF*)((i << 2) | FFMARK );
+}
+
+inline InternalCF * int2imm_gf ( int i )
+{
+    return (InternalCF*)((i << 2) | GFMARK );
+}
+
+inline int imm_intval ( const InternalCF* const op )
+{
+    if ( is_imm( op ) == FFMARK )
+	if ( cf_glob_switches.isOn( SW_SYMMETRIC_FF ) )
+	    return ff_symmetric( imm2int( op ) );
+	else
+	    return imm2int( op );
+    else  if ( is_imm( op ) == GFMARK ) {
+	ASSERT( gf_isff( imm2int( op ) ), "invalid conversion" );
+	if ( cf_glob_switches.isOn( SW_SYMMETRIC_FF ) )
+	    return ff_symmetric( gf_gf2ff( imm2int( op ) ) );
+	else
+	    return gf_gf2ff( imm2int( op ) );
+    }
+    else
+	return imm2int( op );
+}
+//}}}
+
+//{{{ inline int imm_sign ( const InternalCF * const op )
+//{{{ docu
+//
+// imm_sign() - return sign of immediate object.
+//
+// If CO is an immediate integer, the sign is defined as usual.
+// If CO is an element of a finite field and SW_SYMMETRIC_FF is
+// on the sign of CO is the sign of the symmetric representation
+// of CO.  If CO is in GF(q) or in a finite field and
+// SW_SYMMETRIC_FF is off, the sign of CO is zero iff CO is zero,
+// otherwise the sign is one.
+//
+// See alse: CanonicalForm::sign(), gf_sign()
+//
+//}}}
+inline int
+imm_sign ( const InternalCF * const op )
+{
+    if ( is_imm( op ) == FFMARK )
+	if ( imm2int( op ) == 0 )
+	    return 0;
+	else  if ( cf_glob_switches.isOn( SW_SYMMETRIC_FF ) )
+	    if ( ff_symmetric( imm2int( op ) ) > 0 )
+		return 1;
+	    else
+		return -1;
+	else
+	    return 1;
+    else  if ( is_imm( op ) == GFMARK )
+	return gf_sign( imm2int( op ) );
+    else  if ( imm2int( op ) == 0 )
+	return 0;
+    else  if ( imm2int( op ) > 0 )
+	return 1;
+    else
+	return -1;
+}
+//}}}
+
+//{{{ comparison operators
 inline int imm_cmp ( const InternalCF * const lhs, const InternalCF * const rhs )
 {
     if ( imm2int( lhs ) == imm2int( rhs ) )
@@ -123,9 +184,6 @@ inline int imm_cmp ( const InternalCF * const lhs, const InternalCF * const rhs 
     else
 	return -1;
 }
-
-// the following two functions are needed only for comparisons of
-// polynomials (gmpf)
 
 inline int imm_cmp_p ( const InternalCF * const lhs, const InternalCF * const rhs )
 {
@@ -142,7 +200,9 @@ inline int imm_cmp_gf ( const InternalCF * const lhs, const InternalCF * const r
     else
 	return 1;
 }
+//}}}
 
+//{{{ arithmetic operators
 inline InternalCF * imm_add ( const InternalCF * const lhs, const InternalCF * const rhs )
 {
     int result = imm2int( lhs ) + imm2int( rhs );
@@ -312,7 +372,9 @@ inline InternalCF * imm_neg_gf ( const InternalCF * const op )
 {
     return int2imm_gf( gf_neg( imm2int( op ) ) );
 }
+//}}}
 
+//{{{ input/output
 #ifndef NOSTREAMIO
 inline void imm_print ( ostream & os, const InternalCF * const op, const char * const str )
 {
@@ -329,49 +391,6 @@ inline void imm_print ( ostream & os, const InternalCF * const op, const char * 
 	os << imm2int( op ) << str;
 }
 #endif /* NOSTREAMIO */
-
-inline int imm_intval ( const InternalCF* const op )
-{
-    if ( is_imm( op ) == FFMARK )
-	if ( cf_glob_switches.isOn( SW_SYMMETRIC_FF ) )
-	    return ff_symmetric( imm2int( op ) );
-	else
-	    return imm2int( op );
-    else  if ( is_imm( op ) == GFMARK ) {
-	ASSERT( gf_isff( imm2int( op ) ), "invalid conversion" );
-	if ( cf_glob_switches.isOn( SW_SYMMETRIC_FF ) )
-	    return ff_symmetric( gf_gf2ff( imm2int( op ) ) );
-	else
-	    return gf_gf2ff( imm2int( op ) );
-    }
-    else
-	return imm2int( op );
-}
-
-//{{{ inline int imm_sign ( const InternalCF * const op )
-// docu: see CanonicalForm::sign()
-inline int
-imm_sign ( const InternalCF * const op )
-{
-    if ( is_imm( op ) == FFMARK )
-	if ( imm2int( op ) == 0 )
-	    return 0;
-	else  if ( cf_glob_switches.isOn( SW_SYMMETRIC_FF ) )
-	    if ( ff_symmetric( imm2int( op ) ) > 0 )
-		return 1;
-	    else
-		return -1;
-	else
-	    return 1;
-    else  if ( is_imm( op ) == GFMARK )
-	return gf_sign( imm2int( op ) );
-    else  if ( imm2int( op ) == 0 )
-	return 0;
-    else  if ( imm2int( op ) > 0 )
-	return 1;
-    else
-	return -1;
-}
 //}}}
 
 #endif /* ! INCL_IMM_H */
