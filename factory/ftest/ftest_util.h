7ba8269e7fdcaee2df4c555cf66dfa2084daae07
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1997-11-21 10:40:48 +0000

    * ftest_util.cc (ftestUsagePrint): new function.  Declaration added.
    	* ftest_util.h (ftestErrorT): new enumerator `CheckError'
    
    
    git-svn-id: file:///usr/local/Singular/svn/trunk@919 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/ftest/ftest_util.h
+++ factory/ftest/ftest_util.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: ftest_util.h,v 1.8 1997-11-13 08:37:06 schmidt Exp $ */
+/* $Id: ftest_util.h,v 1.9 1997-11-21 10:40:48 schmidt Exp $ */
 
 #ifndef INCL_FTEST_UTIL_H
 #define INCL_FTEST_UTIL_H
@@ -15,10 +15,24 @@ enum ftestStatusT {
 };
 
 //{{{ enum ftestErrorT
+//{{{ docu
+//
+// enum ftestErrorT - FTE error types.
+//
+// Ok: no error (rarely used :-)
+// CommandlineError: commandline syntax error
+// EnvSyntaxError: error in environment specification
+// FileError: error reading some file
+// CanFormSpecError: error in canonical form specification
+// CheckError: error in result checks
+// SignalError: process received a signal
+// TimeoutError: and this signal was due to our own call to `alarm()'
+//
+//}}}
 enum ftestErrorT
 {
     Ok, CommandlineError, EnvSyntaxError, FileError,
-    CanFormSpecError,
+    CanFormSpecError, CheckError,
     SignalError, TimeoutError = SignalError + 14
 };
 //}}}
@@ -31,6 +45,7 @@ extern int ftestPrintResultFlag;
 
 const char * ftestSkipBlancs ( const char * string );
 void ftestError ( const ftestErrorT errno, const char * format ... );
+void ftestUsagePrint ( const char * additionalUsage = 0 );
 void ftestPrint ( const char * longFormat, const char * shortFormat ... );
 
 void ftestSignalCatch ( bool block = false );
