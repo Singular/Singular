c30084002d9743a446fe5e4c99701fefa1f99aba
Olaf Bachmann <obachman@mathematik.uni-kl.de>
1997-12-03 17:40:50 +0000

    * forgot to add spSpolyLoop.h
    git-svn-id: file:///usr/local/Singular/svn/trunk@943 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ Singular/spSpolyLoop.h
@@ -0,0 +1,4 @@
+#ifdef COMP_FAST
+spSpolyLoopProc spSetSpolyLoop(ring r, int syzComp, int ak, BOOLEAN homog);
+extern void spPSpolyLoop_General(poly p1, poly p2, poly m, poly spNoether);
+#endif // COMP_FAST
