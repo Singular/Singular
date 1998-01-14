ab991095fd9265a47a3a99e5f268e866c82ab141
Olaf Bachmann <obachman@mathematik.uni-kl.de>
1998-01-14 14:42:17 +0000

    * binom.h: #undef TEST_MAC_ORDER as default
    git-svn-id: file:///usr/local/Singular/svn/trunk@1016 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/binom.h
+++ Singular/binom.h
@@ -3,11 +3,11 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: binom.h,v 1.4 1998-01-05 16:39:16 Singular Exp $ */
+/* $Id: binom.h,v 1.5 1998-01-14 14:42:17 obachman Exp $ */
 /*
 * ABSTRACT - set order (=number of monomial) for dp
 */
-#define TEST_MAC_ORDER
+// #define TEST_MAC_ORDER
 #ifdef TEST_MAC_ORDER
 extern int bHighdeg;
 extern BOOLEAN bNoAdd;
