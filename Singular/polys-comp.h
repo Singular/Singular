556acd683fd90074c0f666d70d62ce57c29260c9
Olaf Bachmann <obachman@mathematik.uni-kl.de>
1998-01-15 16:15:26 +0000

    * added COMP_NO_EXP_VECTOR_OPS for LITTLE ENDIAN
    git-svn-id: file:///usr/local/Singular/svn/trunk@1024 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/polys-comp.h
+++ Singular/polys-comp.h
@@ -3,7 +3,7 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: polys-comp.h,v 1.2 1997-12-16 18:23:58 obachman Exp $ */
+/* $Id: polys-comp.h,v 1.3 1998-01-15 16:15:26 obachman Exp $ */
 
 /***************************************************************
  *
@@ -119,12 +119,12 @@ do                                                          \
 }                                                           \
 while(0)
   
-#define _pMonCmp_2i_c(p1, p2, length, d, actionD, actionE)   \
+#define _pMonCmp_2i_c(p1, p2, length, d, actionD, actionE)  \
 do                                                          \
 {                                                           \
-  const long* s1 = (long*) &(p1->exp[0]);                         \
-  const long* s2 = (long*) &(p2->exp[0]);                         \
-  const long* const lb = s1 + length-1;                                 \
+  const long* s1 = (long*) &(p1->exp[0]);                   \
+  const long* s2 = (long*) &(p2->exp[0]);                   \
+  const long* const lb = s1 + length-1;                     \
                                                             \
   for (;;)                                                  \
   {                                                         \
@@ -150,35 +150,35 @@ do                                                          \
 }                                                           \
 while(0)
   
-#define _pMonCmp_2i_1_c(p1, p2, length, d, actionD, actionE) \
-do                                                          \
-{                                                           \
-  const long* s1 = (long*) &(p1->exp[0]);                         \
-  const long* s2 = (long*) &(p2->exp[0]);                         \
-  const long* const lb = s1 + length      -1;                           \
-                                                            \
-  for (;;)                                                  \
-  {                                                         \
-    d = *s1 - *s2;                                          \
-    if (d) actionD;                                         \
-    s1++;                                                   \
-    s2++;                                                   \
-    d = *s1 - *s2;                                          \
-    if (d) actionD;                                         \
-    s1++;                                                   \
-    if (s1 == lb) break;                                    \
-    s2++;                                                   \
-  }                                                         \
-                                                            \
-  d = *s1 - *(s2 + 1);                                      \
-  if (d)                                                    \
-  {                                                         \
-    if (((long) (pGetComp(p1) - pGetComp(p2))) == d)        \
-      d = -d;                                               \
-    actionD;                                                \
-  }                                                         \
-  actionE;                                                  \
-}                                                           \
+#define _pMonCmp_2i_1_c(p1, p2, length, d, actionD, actionE)    \
+do                                                              \
+{                                                               \
+  const long* s1 = (long*) &(p1->exp[0]);                       \
+  const long* s2 = (long*) &(p2->exp[0]);                       \
+  const long* const lb = s1 + length      -1;                   \
+                                                                \
+  for (;;)                                                      \
+  {                                                             \
+    d = *s1 - *s2;                                              \
+    if (d) actionD;                                             \
+    s1++;                                                       \
+    s2++;                                                       \
+    d = *s1 - *s2;                                              \
+    if (d) actionD;                                             \
+    s1++;                                                       \
+    if (s1 == lb) break;                                        \
+    s2++;                                                       \
+  }                                                             \
+                                                                \
+  d = *s1 - *(s2 + 1);                                          \
+  if (d)                                                        \
+  {                                                             \
+    if (((long) (pGetComp(p1) - pGetComp(p2))) == d)            \
+      d = -d;                                                   \
+    actionD;                                                    \
+  }                                                             \
+  actionE;                                                      \
+}                                                               \
 while(0)
 
 #else // COMP_NO_EXP_VECTOR_OPS
@@ -186,16 +186,21 @@ while(0)
 #define _pMonCmp(p1, p2, d, actionD, actionE)   \
 do                                              \
 {                                               \
-  Exponent_pt e1 = &(p1->exp[0]);               \
-  Exponent_pt e2 = &(p2->exp[0]);               \
-  Exponent_pt ub = e1 + pVariables + 1;         \
+  Exponent_pt e1 = &(p1->exp[0]);         \
+  Exponent_pt e2 = &(p2->exp[0]);         \
+  const Exponent_pt ub = e1 + pVariables; \
                                                 \
   for (;;)                                      \
   {                                             \
     d = *e1 - *e2;                              \
     if (d) actionD;                             \
     e1++;                                       \
-    if (e1 == ub) actionE;                      \
+    if (e1 == ub)                               \
+    {                                           \
+      d = pGetComp(p1) - pGetComp(p2);          \
+      if (d) actionD;                           \
+      actionE;                                  \
+    }                                           \
     e2++;                                       \
   }                                             \
 }                                               \
@@ -204,9 +209,9 @@ while(0)
 #define _pMonCmp_c(p1, p2, d, actionD, actionE) \
 do                                              \
 {                                               \
-  Exponent_pt e1 = &(p1->exp[0]);               \
-  Exponent_pt e2 = &(p2->exp[0]);               \
-  Exponent_pt ub = e1 + pVariables;             \
+  Exponent_pt e1 = &(p1->exp[0]);         \
+  Exponent_pt e2 = &(p2->exp[0]);         \
+  const Exponent_pt ub = e1 + pVariables; \
                                                 \
   for (;;)                                      \
   {                                             \
@@ -252,6 +257,7 @@ while(0)
 
 #else //  ! WORDS_BIGENDIAN
 
+#ifndef COMP_NO_EXP_VECTOR_OPS  
 #define _pMonCmp_1(p1, p2, d, actionD, actionE)                              \
 do                                                                          \
 {                                                                           \
@@ -413,8 +419,80 @@ do                                                          \
 }                                                           \
 while(0)
 
-// TBC
+#else // COMP_NO_EXP_VECTOR_OPS 
 
+#define _pMonCmp(p1, p2, d, actionD, actionE)   \
+do                                              \
+{                                               \
+  Exponent_pt e1 = &(p1->exp[pVarHighIndex]);   \
+  Exponent_pt e2 = &(p2->exp[pVarHighIndex]);   \
+  const Exponent_pt ub = e1 - pVariables;       \
+                                                \
+  for (;;)                                      \
+  {                                             \
+    d = *e1 - *e2;                              \
+    if (d) actionD;                             \
+    e1--;                                       \
+    if (e1 == ub)                               \
+    {                                           \
+      d = pGetComp(p1) - pGetComp(p2);          \
+      if (d) actionD;                           \
+      actionE;                                  \
+    }                                           \
+    e2--;                                       \
+  }                                             \
+}                                               \
+while(0)
+
+#define _pMonCmp_c(p1, p2, d, actionD, actionE) \
+do                                              \
+{                                               \
+  Exponent_pt e1 = &(p1->exp[pVarHighIndex]);   \
+  Exponent_pt e2 = &(p2->exp[pVarHighIndex]);   \
+  const Exponent_pt ub = e1 - pVariables;       \
+                                                \
+  for (;;)                                      \
+  {                                             \
+    d = *e1 - *e2;                              \
+    if (d) actionD;                             \
+    e1--;                                       \
+    if (e1 == ub)                               \
+    {                                           \
+      d = pGetComp(p2) - pGetComp(p1);          \
+      if (d) actionD;                           \
+      actionE;                                  \
+    }                                           \
+    e2--;                                       \
+  }                                             \
+}                                               \
+while(0)
+
+#define _pMonCmp_1(p1, p2, d, actionD, actionE)  \
+  _pMonCmp(p1, p2, d, actionD, actionE)
+
+#define _pMonCmp_1_c(p1, p2, d, actionD, actionE)  \
+  _pMonCmp_c(p1, p2, d, actionD, actionE)
+  
+#define _pMonCmp_2(p1, p2, d, actionD, actionE)  \
+  _pMonCmp(p1, p2, d, actionD, actionE)
+
+#define _pMonCmp_2_c(p1, p2, d, actionD, actionE)  \
+  _pMonCmp_c(p1, p2, d, actionD, actionE)
+  
+#define _pMonCmp_2i(p1, p2, length, d, actionD, actionE) \
+  _pMonCmp(p1, p2, d, actionD, actionE)
+
+#define _pMonCmp_2i_1(p1, p2, length, d, actionD, actionE) \
+  _pMonCmp(p1, p2, d, actionD, actionE)
+
+#define _pMonCmp_2i_c(p1, p2, length, d, actionD, actionE)   \
+  _pMonCmp_c(p1, p2, d, actionD, actionE)
+
+#define _pMonCmp_2i_1_c(p1, p2, length, d, actionD, actionE) \
+  _pMonCmp_c(p1, p2, d, actionD, actionE)
+  
+#endif // COMP_NO_EXP_VECTOR_OPS 
+  
 #endif // WORDS_BIGENDIAN
 
 #endif // COMP_FAST
