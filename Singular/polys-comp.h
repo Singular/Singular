24189c4982473a71947501ca9125a109ab40e937
Olaf Bachmann <obachman@mathematik.uni-kl.de>
1998-04-30 15:29:34 +0000

    * added target tar in Makefile * clean-up of some stuff
    git-svn-id: file:///usr/local/Singular/svn/trunk@1569 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/polys-comp.h
+++ Singular/polys-comp.h
@@ -3,7 +3,7 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: polys-comp.h,v 1.6 1998-03-17 10:59:56 obachman Exp $ */
+/* $Id: polys-comp.h,v 1.7 1998-04-30 15:29:34 obachman Exp $ */
 
 /***************************************************************
  *
@@ -176,6 +176,52 @@ do                                                                      \
 }                                                                       \
 while(0)
 
+#define _pMonComp_otEXP_nwGEN(p1, p2, length, d, actionD, actionE)  \
+do                                                                  \
+{                                                                   \
+  const long* s1 = (long*) &(p1->exp[0]);                           \
+  const long* s2 = (long*) &(p2->exp[0]);                           \
+  const long* const lb = s1 + length;                               \
+                                                                    \
+  for (;;)                                                          \
+  {                                                                 \
+    d = *s1 - *s2;                                                  \
+    if (d) actionD;                                                 \
+    s1++;                                                           \
+    if (s1 == lb) actionE;                                          \
+    s2++;                                                           \
+  }                                                                 \
+}                                                                   \
+while(0)
+
+#define _pMonComp_otEXPCOMP_nwGEN(p1, p2, length, d, actionD, actionE)  \
+do                                                                      \
+{                                                                       \
+  const long* s1 = (long*) &(p1->exp[0]);                               \
+  const long* s2 = (long*) &(p2->exp[0]);                               \
+  const long* const lb = s1 + length      -1;                           \
+                                                                        \
+  for (;;)                                                              \
+  {                                                                     \
+    d = *s1 - *s2;                                                      \
+    if (d) actionD;                                                     \
+    s1++;                                                               \
+    if (s1 == lb) break;                                                \
+    s2++;                                                               \
+  }                                                                     \
+                                                                        \
+  d = *s1 - *(s2 + 1);                                                  \
+  if (d)                                                                \
+  {                                                                     \
+    if (((long) (pGetComp(p1) - pGetComp(p2))) == d)                    \
+      d = -d;                                                           \
+    actionD;                                                            \
+  }                                                                     \
+  actionE;                                                              \
+}                                                                       \
+while(0)
+
+  
 #else //  ! WORDS_BIGENDIAN
 
 #define _pMonComp_otEXP_nwONE(p1, p2, d, actionD, actionE)                  \
@@ -321,6 +367,51 @@ do                                                                      \
     if (d) actionD;                                                     \
     s1--;                                                               \
     s2--;                                                               \
+    d = *s1 - *s2;                                                      \
+    if (d) actionD;                                                     \
+    s1--;                                                               \
+    if (s1 == lb) break;                                                \
+    s2--;                                                               \
+  }                                                                     \
+                                                                        \
+  d = *s1 - *(s2 - 1);                                                  \
+  if (d)                                                                \
+  {                                                                     \
+    if (((long) (pGetComp(p1) - pGetComp(p2))) == d)                    \
+      d = -d;                                                           \
+    actionD;                                                            \
+  }                                                                     \
+  actionE;                                                              \
+}                                                                       \
+while(0)
+
+#define _pMonComp_otEXP_nwGEN(p1, p2, length, d, actionD, actionE)  \
+do                                                                  \
+{                                                                   \
+  const long* s1 = ((long*) p1) + pMonomSizeW-1;                    \
+  const long* s2 = ((long*) p2)  + pMonomSizeW-1;                   \
+  const long* const lb = s1 - length;                               \
+                                                                    \
+  for (;;)                                                          \
+  {                                                                 \
+    d = *s1 - *s2;                                                  \
+    if (d) actionD;                                                 \
+    s1--;                                                           \
+    if (s1 == lb) actionE;                                          \
+    s2--;                                                           \
+  }                                                                 \
+}                                                                   \
+while(0)
+  
+#define _pMonComp_otEXPCOMP_nwGEN(p1, p2, length, d, actionD, actionE) \
+do                                                                      \
+{                                                                       \
+  const long* s1 = ((long*) p1) + pMonomSizeW-1;                        \
+  const long* s2 = ((long*) p2)  + pMonomSizeW-1;                       \
+  const long* const lb = s1 - length +1;                                \
+                                                                        \
+  for (;;)                                                              \
+  {                                                                     \
     d = *s1 - *s2;                                                      \
     if (d) actionD;                                                     \
     s1--;                                                               \
