babf5d391b327faf57b11790b4cb3a7c86f394e7
Jens Schmidt <schmidt@mathematik.uni-kl.de>
1998-05-11 10:01:56 +0000

    * imm.cc (imm_mul): new file for MetroWerks
    git-svn-id: file:///usr/local/Singular/svn/trunk@1680 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/imm.cc
+++ factory/imm.cc
@@ -1,40 +1,60 @@
-#ifdef __MWERKS__
+/* emacs edit mode for this file is -*- C++ -*- */
+/* $Id: imm.cc,v 1.2 1998-05-11 10:01:56 schmidt Exp $ */
 
-#include "imm.h"
+//{{{ docu
+//
+// imm.cc - some special implementations of immediate functions
+//   for some special platforms.
+//
+//}}}
+
+#include <config.h>
+
 #include <stuff_64.h>
 
-InternalCF * imm_mul (InternalCF * lhs, InternalCF * rhs )
+#include "imm.h"
+
+//{{{ InternalCF * imm_mul (InternalCF * lhs, InternalCF * rhs )
+//{{{ docu
+//
+// imm_mul() - multiplication of two immediates.
+//
+// Special implementation for MetroWerks.
+//
+//}}}
+InternalCF *
+imm_mul (InternalCF * lhs, InternalCF * rhs )
 {
     int li = imm2int( lhs ), ri = imm2int( rhs );    
     Int_64 pr;
     
     if (li<0)
     {
-      if (ri<0)
-        mul_64(&pr, -li, -ri);
-      else
-        mul_64(&pr, -li, ri);
+	if (ri<0)
+	    mul_64(&pr, -li, -ri);
+	else
+	    mul_64(&pr, -li, ri);
     }
     else
     {
-      if (ri<0)
-        mul_64(&pr, li, -ri);
-      else
-        mul_64(&pr, li, ri);
+	if (ri<0)
+	    mul_64(&pr, li, -ri);
+	else
+	    mul_64(&pr, li, ri);
     }
+
     if (pr.hi || (pr.lo>MAXIMMEDIATE))
     {
-	  InternalCF * res = CFFactory::basic( IntegerDomain, li, true );
-	  return res->mulcoeff( rhs );
+	InternalCF * res = CFFactory::basic( IntegerDomain, li, true );
+	return res->mulcoeff( rhs );
     }
     else
     {
-      int res = pr.lo;
-      if ((li>0) == (ri>0))
-        return int2imm(res);
-      else
-        return int2imm(-res);
-	}
+	int res = pr.lo;
+	if ((li>0) == (ri>0))
+	    return int2imm(res);
+	else
+	    return int2imm(-res);
+    }
 }
-
-#endif
+//}}}
