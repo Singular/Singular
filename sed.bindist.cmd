b911ac5ddf660513a9fa06698ab8ac4553c1ef65
Hans Schönemann <hannes@mathematik.uni-kl.de>
1998-05-27 15:25:40 +0000

    * hannes: changed default lib-dir
    git-svn-id: file:///usr/local/Singular/svn/trunk@2004 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- sed.bindist.cmd
+++ sed.bindist.cmd
@@ -1,4 +1,4 @@
 /SINGULAR_DATADIR/c\
-#define SINGULAR_DATADIR "/usr/local/share/Singular"
+#define SINGULAR_DATADIR "/usr/local/Singular/LIB"
 /SINGULAR_INFODIR/c\
 #define SINGULAR_INFODIR "/usr/local/info"
