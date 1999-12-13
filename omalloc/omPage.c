4178a1990c32db55ad365cafc027a44c2f25b0eb
Olaf Bachmann <obachman@mathematik.uni-kl.de>
1999-12-13 16:27:59 +0000

    * Page stuff
    git-svn-id: file:///usr/local/Singular/svn/trunk@4011 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- omalloc/omPage.c
+++ omalloc/omPage.c
@@ -3,14 +3,71 @@
  *  Purpose: implementation of routines for primitve page managment
  *  Author:  obachman (Olaf Bachmann)
  *  Created: 11/99
- *  Version: $Id: omPage.c,v 1.1.1.1 1999-11-18 17:45:53 obachman Exp $
+ *  Version: $Id: omPage.c,v 1.2 1999-12-13 16:27:58 obachman Exp $
  *******************************************************************/
 
+#include <limits.h>
 #include "omConfig.h"
 #include "omPage.h"
 #include "omPrivate.h"
 #include "omList.h"
 #include "omLocal.h"
+
+/***********************************************************************
+ *
+ * Addr identification
+ */
+
+unsigned long omMaxPageIndex = 0;
+unsigned long omMinPageIndex = ULONG_MAX;
+unsigned long *omPageIndicies = NULL;
+/* omPageIndicies has to be ReAlloced after OM_PAGE_INDEX_STEPWIDTH*BIT_SIZEOF_LONG
+   new pages were allocated */
+#define OM_PAGE_INDEX_STEPWIDTH 128
+
+void omPageIndexFault(unsigned long index)
+{
+  omAssume(index > omMaxPageIndex || index < omMinPageIndex);
+  
+  if (omPageIndicies == NULL)
+  {
+    omTypeAlloc0Block(omPageIndicies, unsigned long *, SIZEOF_LONG);
+    omMaxPageIndex = index;
+    omMinPageIndex = index;
+  }
+  else
+  {
+    if (index < omMinPageIndex)
+    {
+      unsigned long i;
+      unsigned long old_length = omMaxPageIndex - omMinPageIndex + 1;
+      unsigned long new_length = omMaxPageIndex - index + OM_PAGE_INDEX_STEPWIDTH;
+      unsigned long offset = old_length - new_length;
+      
+      omPageIndicies 
+        = omReallocBlock(omPageIndicies, old_length*SIZEOF_LONG, new_length*SIZEOF_LONG);
+      
+      for (i=old_length -1; i >= 0; i--)
+      {
+        omPageIndicies[i+offset] = omPageIndicies[i];
+      }
+      for (i=0; i<offset; i++)
+      {
+        omPageIndicies[i] = 0;
+      }
+      omMinPageIndex -= offset;
+    }
+    else
+    {
+      omPageIndicies 
+        = omRealloc0Block(omPageIndicies, 
+                          (omMaxPageIndex - omMinPageIndex + 1)*SIZEOF_LONG,
+                          (index - omMinPageIndex + OM_PAGE_INDEX_STEPWIDTH)*SIZEOF_LONG);
+      omMaxPageIndex =  index + OM_PAGE_INDEX_STEPWIDTH - omMaxPageIndex;
+    }
+  }
+}
+  
 
 static void* om_FreePages = NULL;
 static int om_NumberOfAllocatedPages = 0;
--- omalloc/omPage.h
+++ omalloc/omPage.h
@@ -3,10 +3,83 @@
  *  Purpose: declaration of routines for primitve page managment
  *  Author:  obachman (Olaf Bachmann)
  *  Created: 11/99
- *  Version: $Id: omPage.h,v 1.1.1.1 1999-11-18 17:45:53 obachman Exp $
+ *  Version: $Id: omPage.h,v 1.2 1999-12-13 16:27:59 obachman Exp $
  *******************************************************************/
 #ifndef OM_PAGE_H
 #define OM_PAGE_H
+
+/***********************************************************************
+ *
+ * Identifying whether an address is from a page requested with omGetPage()
+ * For this to work, omRegisterExternalAddr must be called with every address
+ * gotten from an external malloc 
+ */
+
+/* Here is how it works (assume SIZEOF_LONG == 4, SIZEOF_SYSTEM_PAGE = 2^12)
+   Let
+   Addr: |    15               |  5       |    12        |
+          PAGE_INDEX           PAGE_SHIFT  PAGE_OFFSET
+
+                                   PAGE_BASE
+
+   omPageIndicies is an array of bit-fields which is indexed by
+                  PAGE_INDEX - omMinPageIndex. I'ts maximal length
+                  is 2^15. PAGE_SHIFT is used as index into the bit-field.
+                  If it's value is 1, then addr is from omPage, else
+                  not.
+
+   omMinPageIndex is iminimal page index of registered addr
+
+   In other words: omIsPageAddr iff
+   omPageIndicies[PAGE_INDEX - omMinPageIndex] & (1 << PAGE_SHIFT) */
+
+
+#define OM_SIZEOF_INDEX_PAGE (SIZEOF_SYSTEM_PAGE << LOG_BIT_SIZEOF_LONG)
+
+#define omGetPageShiftOfAddr(addr) \
+  (((unsigned long) addr) & (OM_SIZEOF_INDEX_PAGE -1)) >> LOG_BIT_SIZEOF_SYSTEM_PAGE)
+
+#define omGetPageIndexOfAddr(addr) \
+  (((unsigned long) addr) >> (LOG_BIT_SIZEOF_LONG + LOG_BIT_SIZEOF_SYSTEM_PAGE))
+
+#define omIsPageAddr(addr) \
+   (omPageIndicies[omGetPageIndexOfAddr(addr) - omMinPageIndex] & \
+    (1 << omGetPageShiftOfAddr(addr)))
+
+extern void omPageIndexFault(unsigned long page_index);
+extern unsigned long omMaxPageIndex;
+extern unsigned long omMinPageIndex;
+extern unsigned long *omPageIndicies;
+
+#define omRegisterPageIndex(index)                      \
+do                                                      \
+{                                                       \
+  if (index > omMinPageIndex || index < omMaxPageIndex) \
+  {                                                     \
+    omPageIndexFault(index);                            \
+  }                                                     \
+}                                                       \
+while (0)
+
+#define omRegisterExternalAddr(addr)                        \
+do                                                          \
+{                                                           \
+  unsigned long _omPageIndex = omGetPageIndexOfAddr(addr);  \
+  omRegisterPageIndex(_omPageIndex);                        \
+  omPageIndicies[_omPageIndex - omMinPageIndex] &=          \
+    ~ (1 << omGetPageShiftOfAddr(addr));                    \
+}                                                           \
+while (0)
+
+#define omRegisterPageAddr(addr)                            \
+do                                                          \
+{                                                           \
+  unsigned long _omPageIndex = omGetPageIndexOfAddr(addr);  \
+  omRegisterPageIndex(_omPageIndex);                        \
+  omPageIndicies[_omPageIndex - omMinPageIndex] |=          \
+      (1 << omGetPageShiftOfAddr(addr));                    \
+}                                                           \
+while (0)
 
 /***********************************************************************
  *
