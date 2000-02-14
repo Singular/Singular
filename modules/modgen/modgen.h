af14cc9defcb2715205c9db691a9ced0ced09088
Kai Krüger <krueger@krueger-berg.de>
2000-02-14 21:43:32 +0000

    * modgen.h: extentions needed by other files
    git-svn-id: file:///usr/local/Singular/svn/trunk@4149 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/modgen/modgen.h
+++ modules/modgen/modgen.h
@@ -1,5 +1,5 @@
 /*
- *  $Id: modgen.h,v 1.5 2000-01-27 12:40:00 krueger Exp $
+ *  $Id: modgen.h,v 1.6 2000-02-14 21:43:32 krueger Exp $
  *
  */
 
@@ -42,22 +42,26 @@ class paramdef {
   char *name;
   char *typname;
   int  typ;
+  char *varname;
 };
 
 class procdef {
  public:
-  char *procname;
-  char *funcname;
-  int lineno;
-  int is_static;
-  paramdef  return_val;
-  paramdefv param;
-  int       paramcnt;
-  procflags flags;
-  char *c_code;
-  char *help_string;
-  long example_len;
-  char *example_string;
+  char       * procname;
+  char       * funcname;
+  language_defs language;
+  int          lineno;
+  int          is_static;
+  paramdef     return_val;
+  paramdefv    param;
+  int          paramcnt;
+  procflags    flags;
+  char       * c_code;
+  char       * help_string;
+  long         example_len;
+  char       * example_string;
+  long         sing_start;
+  long         sing_end;
 };
 
 class cfiles {
@@ -70,7 +74,9 @@ class moddef {
  public:
   FILE * modfp;           /* module file */
   FILE * modfp_h;         /* header file */
-  FILE * fmtfp;
+  FILE * fmtfp;           /* temporary file */
+  FILE * binfp;           /* include singular procedures are stored
+                             in an extra file */
   char * filename;        /* inputfile to parse */
   char * name;            /* name of the module */
   unsigned int major, minor, level;
@@ -89,7 +95,8 @@ typedef enum { VAR_UNKNOWN, VAR_BOOL, VAR_NUM, VAR_STRING,
                VAR_FILE, VAR_FILES
 } var_type;
 
-typedef enum { CMD_NONE, CMD_DECL, CMD_CHECK, CMD_RETURN
+typedef enum { CMD_NONE, CMD_DECL, CMD_CHECK, CMD_RETURN,
+               CMD_SINGULAR
 } cmd_token;
 
 typedef enum { VAR_NONE, VAR_MODULE, VAR_HELP, VAR_INFO, VAR_VERSION,
@@ -112,7 +119,7 @@ extern void generate_mod(moddefv module, int section);
 extern void mod_create_makefile(moddefv module);
 extern void Add2files(moddefv module, char *buff);
 
-extern void  mod_copy_tmp(FILE *fp_out, FILE *fp_in);
+extern void mod_copy_tmp(FILE *fp_out, FILE *fp_in);
 extern void mod_write_header(FILE *fp, char *module, char what);
 extern void generate_header(procdefv pi, FILE *fp);
 extern void write_header(FILE *fp, char *module, char *comment="");
@@ -120,16 +127,20 @@ extern void make_version(char *p, moddefv module);
 extern void write_procedure_text(moddefv module, int lineno);
 /*extern void write_procedure_header(moddefv module);*/
 
-extern int init_proc(procdefv p, char *procname, paramdefv ret, int line);
+extern int init_proc(procdefv p, char *procname, paramdefv ret, int line,
+                     language_defs language = LANG_C);
 extern void setup_proc(moddefv module, procdefv p);
 extern void proc_set_var(procdefv p, var_type type, var_token varid,
                          char *varname, void *varvalue);
 void proc_set_default_var(var_type type, var_token varid, char *varname,
                           void *varvalue);
 void write_finish_functions(moddefv module, procdefv proc);
-void AddParam(procdefv p, paramdefv vnew);
+void AddParam(procdefv p, paramdefv vnew, char *name = NULL);
 
 extern int create_tmpfile(moddefv module_def);
+
+/* from proc_setup.cc */
+extern int check_reseverd(char *name);
 
 /* from proc.cc */
 extern void write_example(moddefv module, procdefv proc);
@@ -139,7 +150,12 @@ extern void write_procedure_return(moddefv module, procdefv pi, FILE *fmtfp);
 extern void write_function_declaration(moddefv module, procdefv pi, void *arg = NULL);
 extern void write_function_typecheck(moddefv module, procdefv pi, void *arg = NULL);
 extern void write_function_return(moddefv module, procdefv pi, void *arg = NULL);
-extern void write_function_errorhandling(procdefv pi, FILE *fp);
+extern void write_function_errorhandling(moddefv module, procdefv pi);
+extern int  write_singular_procedures(moddefv module, procdefv proc);
+extern void write_singular_parameter(moddefv module,int lineno,
+                                     char *typname, char *varname);
+extern void write_codeline(moddefv module, procdefv proc,
+                           char *line, int lineno = -1);
 
 /* from misc.cc */
 extern cmd_token checkcmd(
