ab83208afbf15ecc4059a30826fbfcef2ef0e052
Olaf Bachmann <obachman@mathematik.uni-kl.de>
2000-08-16 11:23:37 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/Singular/svn/trunk@4532 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- omalloc/omGetBackTrace.h
+++ omalloc/omGetBackTrace.h
@@ -3,7 +3,7 @@
  *  Purpose: routines for getting Backtraces of stack
  *  Author:  obachman (Olaf Bachmann)
  *  Created: 11/99
- *  Version: $Id: omGetBackTrace.h,v 1.3 2000-08-14 12:26:45 obachman Exp $
+ *  Version: $Id: omGetBackTrace.h,v 1.4 2000-08-16 11:23:37 obachman Exp $
  *******************************************************************/
 #ifndef OM_GET_BACKTRACE_H
 #define OM_GET_BACKTRACE_H
@@ -13,7 +13,7 @@ void omInitGetBackTrace();
 int omGetBackTrace(void** bt, int start, int max);
 #else
 #define omInitGetBackTrace() ((void)0)
-#define omGetBackTrace(bt, s, max) ((void)0)
+#define omGetBackTrace(bt, s, max) (0)
 #endif
 
 #endif 
