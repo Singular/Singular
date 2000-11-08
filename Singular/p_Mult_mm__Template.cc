a3f7dfdce2de577c3894d03276b7bbf41384e113
Olaf Bachmann <obachman@mathematik.uni-kl.de>
2000-11-08 13:14:43 +0000

    * fixed memory leak
    git-svn-id: file:///usr/local/Singular/svn/trunk@4704 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/p_Mult_mm__Template.cc
+++ Singular/p_Mult_mm__Template.cc
@@ -6,7 +6,7 @@
  *  Purpose: template for p_Mult_n
  *  Author:  obachman (Olaf Bachmann)
  *  Created: 8/00
- *  Version: $Id: p_Mult_mm__Template.cc,v 1.6 2000-10-30 16:54:55 obachman Exp $
+ *  Version: $Id: p_Mult_mm__Template.cc,v 1.7 2000-11-08 13:14:43 obachman Exp $
  *******************************************************************/
 
 /***************************************************************
@@ -24,13 +24,16 @@ poly p_Mult_mm(poly p, const poly m, const ring ri)
   pAssume(m != NULL);
   poly q = p;
   number ln = pGetCoeff(m);
+  number pn;
   DECLARE_LENGTH(const unsigned long length = ri->ExpL_Size);
   const unsigned long* m_e = m->exp;
   pAssume(!n_IsZero(ln,ri));
 
   while (p != NULL)
   {
-    pSetCoeff0(p, n_Mult(ln, pGetCoeff(p), ri));
+    pn = pGetCoeff(p);
+    pSetCoeff0(p, n_Mult(ln, pn, ri));
+    n_Delete(&pn, ri);
     p_MemAdd(p->exp, m_e, length);
     p_MemAddAdjust(p, ri);
     p = pNext(p);
