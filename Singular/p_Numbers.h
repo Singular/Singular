d65eb89a8715a58a8921b48c08b598a0a1ded9ca
Hans Schönemann <hannes@mathematik.uni-kl.de>
2000-12-01 12:57:04 +0000

    *hannes: ring indep. n-operations
    git-svn-id: file:///usr/local/Singular/svn/trunk@4784 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/p_Numbers.h
+++ Singular/p_Numbers.h
@@ -6,29 +6,29 @@
  *  Purpose: macros/inline functions for number oerations
  *  Author:  obachman (Olaf Bachmann)
  *  Created: 8/00
- *  Version: $Id: p_Numbers.h,v 1.4 2000-09-20 13:25:41 obachman Exp $
+ *  Version: $Id: p_Numbers.h,v 1.5 2000-12-01 12:57:04 Singular Exp $
  *******************************************************************/
 #ifndef P_NUMBERS_H
 #define P_NUMBERS_H
 
 #include "numbers.h"
-#define n_Copy_FieldGeneral(n, r)           nCopy(n)
-#define n_Delete_FieldGeneral(n, r)         nDelete(n)
-#define n_Mult_FieldGeneral(n1, n2, r)      nMult(n1, n2)
-#define n_Add_FieldGeneral(n1, n2, r)       nAdd(n1, n2)
-#define n_IsZero_FieldGeneral(n, r)	        nIsZero(n)
-#define n_Equal_FieldGeneral(n1, n2, r)     nEqual(n1, n2)
-#define n_Neg_FieldGeneral(n, r)	        nNeg(n)
-#define n_Sub_FieldGeneral(n1, n2, r)       nSub(n1, n2)
+#define n_Copy_FieldGeneral(n, r)           (r)->cf->nCopy(n)
+#define n_Delete_FieldGeneral(n, r)         (r)->cf->nDelete(n)
+#define n_Mult_FieldGeneral(n1, n2, r)      (r)->cf->nMult(n1, n2)
+#define n_Add_FieldGeneral(n1, n2, r)       (r)->cf->nAdd(n1, n2)
+#define n_IsZero_FieldGeneral(n, r)         (r)->cf->nIsZero(n)
+#define n_Equal_FieldGeneral(n1, n2, r)     (r)->cf->nEqual(n1, n2)
+#define n_Neg_FieldGeneral(n, r)            (r)->cf->nNeg(n)
+#define n_Sub_FieldGeneral(n1, n2, r)       (r)->cf->nSub(n1, n2)
 
 #include "modulop.h"
 #define n_Copy_FieldZp(n, r)        n
 #define n_Delete_FieldZp(n, r)      ((void)0)
 #define n_Mult_FieldZp(n1, n2, r)   npMultM(n1,n2)
 #define n_Add_FieldZp(n1, n2, r)    npAddM(n1, n2)
-#define n_IsZero_FieldZp(n, r)	    npIsZeroM(n)
+#define n_IsZero_FieldZp(n, r)      npIsZeroM(n)
 #define n_Equal_FieldZp(n1, n2, r)  npEqualM(n1, n2)
-#define n_Neg_FieldZp(n, r)	        npNegM(n)
+#define n_Neg_FieldZp(n, r)         npNegM(n)
 #define n_Sub_FieldZp(n1, n2, r)    npSubM(n1, n2)
 
 #define DO_LINLINE
@@ -37,9 +37,8 @@
 #define n_Delete_FieldQ(n, r)      nlDelete(n)
 #define n_Mult_FieldQ(n1, n2, r)   nlMult(n1,n2)
 #define n_Add_FieldQ(n1, n2, r)    nlAdd(n1, n2)
-#define n_IsZero_FieldQ(n, r)	   nlIsZero(n)
+#define n_IsZero_FieldQ(n, r)      nlIsZero(n)
 #define n_Equal_FieldQ(n1, n2, r)  nlEqual(n1, n2)
-#define n_Neg_FieldQ(n, r)	       nlNeg(n)
+#define n_Neg_FieldQ(n, r)         nlNeg(n)
 #define n_Sub_FieldQ(n1, n2, r)    nlSub(n1, n2)
 #endif
-
