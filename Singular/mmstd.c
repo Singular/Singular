4b24dc87e783c30424d898c8d71f88b84cb61baf
Viktor Levandovskyy <levandov@math.rwth-aachen.de>
2001-01-24 16:54:04 +0000

    win adoption
    git-svn-id: file:///usr/local/Singular/svn/trunk@5125 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/mmstd.c
+++ Singular/mmstd.c
@@ -1,7 +1,7 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: mmstd.c,v 1.1 2000-11-20 10:18:56 hannes Exp $ */
+/* $Id: mmstd.c,v 1.2 2001-01-24 16:54:04 levandov Exp $ */
 /*
 * ABSTRACT: standard version of C-memory management alloc func
 */
@@ -13,5 +13,26 @@
 
 // define this so that all addr allocated there are marked 
 // as static, i.e. not metioned by omPrintUsedAddr
+#ifndef ix86_Win
 #define OM_MALLOC_MARK_AS_STATIC
 #include <omalloc.c>
+#else
+#include <stdlib.h>
+void freeSize(void* addr, size_t size)
+{
+  if (addr) free(addr);
+}
+
+void* reallocSize(void* old_addr, size_t old_size, size_t new_size)
+{
+  if (old_addr && new_size)
+  {
+   return realloc(old_addr, new_size);
+  }
+  else 
+  {
+    freeSize(old_addr, old_size);
+    return malloc(new_size);
+  }
+}
+#endif
