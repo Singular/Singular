747dceb56ea728087e141b835ab4e1d95b2faa67
Viktor Levandovskyy <levandov@math.rwth-aachen.de>
2001-02-15 12:58:17 +0000

    win adoption
    git-svn-id: file:///usr/local/Singular/svn/trunk@5236 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/mmstd.c
+++ Singular/mmstd.c
@@ -1,7 +1,7 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: mmstd.c,v 1.3 2001-01-27 17:04:59 obachman Exp $ */
+/* $Id: mmstd.c,v 1.4 2001-02-15 12:58:17 levandov Exp $ */
 /*
 * ABSTRACT: standard version of C-memory management alloc func 
 * i.e. (malloc/realloc/free)
@@ -10,6 +10,9 @@
 #include "mod2.h"
 
 #define OM_NO_MALLOC_MACROS
+#ifdef ix86_Win
+#define OMALLOC_USES_MALLOC
+#endif
 #include "omalloc.h"
 
 // we provide these functions, so that the settings of OM_CHECK
@@ -21,4 +24,24 @@
 // as static, i.e. not metioned by omPrintUsedAddr
 #define OM_MALLOC_MARK_AS_STATIC
 #include <omalloc.c>
+#else
+#include <stdlib.h>
+void freeSize(void* addr, size_t size)
+{
+  if (addr) free(addr);
+}
+
+void* reallocSize(void* old_addr, size_t old_size, size_t new_size)
+{
+  if (old_addr && new_size)
+  {
+   return realloc(old_addr, new_size);
+  }
+  else 
+  {
+    freeSize(old_addr, old_size);
+    return malloc(new_size);
+  }
+}
 #endif
+
