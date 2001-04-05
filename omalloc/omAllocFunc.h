421b9f3b7fae945ed2652cd04e50edfa1b8325e9
Hans Schönemann <hannes@mathematik.uni-kl.de>
2001-04-05 10:51:43 +0000

    *hannes: sgi-port
    git-svn-id: file:///usr/local/Singular/svn/trunk@5357 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- omalloc/omAllocFunc.h
+++ omalloc/omAllocFunc.h
@@ -5,7 +5,7 @@
  *           and end with the Func suffix.
  *  Author:  obachman@mathematik.uni-kl.de (Olaf Bachmann)
  *  Created: 11/99
- *  Version: $Id: omAllocFunc.h,v 1.2 2001-04-05 10:30:39 Singular Exp $
+ *  Version: $Id: omAllocFunc.h,v 1.3 2001-04-05 10:51:43 Singular Exp $
  *******************************************************************/
 
 void* omCallocFunc(size_t nmemb, size_t size);
@@ -13,9 +13,9 @@ void* omMallocFunc(size_t size);
 void omFreeFunc(void* addr);
 void* omVallocFunc(size_t size);
 #if defined(sgi)
-void* memalign(size_t size_1, size_t size_2);
+void* omMemalignFunc(size_t size_1, size_t size_2);
 #else
-void* memalign(void* alignment, size_t size);
+void* omMemalignFunc(void* alignment, size_t size);
 #endif
 void* omReallocFunc(void* old_addr, size_t new_size);
 char* omStrdupFunc(const char* addr);
