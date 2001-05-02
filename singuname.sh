fcc0c2c5c75175fab60c27d251a2e3a970603671
Hans Schönemann <hannes@mathematik.uni-kl.de>
2001-05-02 15:37:43 +0000

    *hannes: fixed typos
    git-svn-id: file:///usr/local/Singular/svn/trunk@5412 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- singuname.sh
+++ singuname.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# 
+#
 # FILE:    singuname
 # USAGE:   singuname
 # PURPOSE: prints to stdout a unique identifier of known uname(s)
-# AUTHOR:  obachman 
+# AUTHOR:  obachman
 # CREATED: 5/26/98
 #
 ###########################################################################
@@ -16,22 +16,22 @@ binary='/bin/ls'
 ldd='ldd'
 
 # HPUX ########################################################
-if (echo $uname_a | $egrep "HP-UX" > $devnull) 
+if (echo $uname_a | $egrep "HP-UX" > $devnull)
 then
     prefix=HPUX
     # HPUX-9
-    if (echo $uname_a | $egrep "\.09\." > $devnull) 
+    if (echo $uname_a | $egrep "\.09\." > $devnull)
     then
-	echo ${prefix}-9
-	exit 0
+        echo ${prefix}-9
+        exit 0
     # HPUX-10
     elif (echo $uname_a | $egrep "\.10\." > $devnull)
     then
-	echo ${prefix}-10
-	exit 0
+        echo ${prefix}-10
+        exit 0
     else
-	echo ${prefix}-Unknown
-	exit 1
+        echo ${prefix}-Unknown
+        exit 1
     fi
 # Intel ########################################################
 elif (echo $uname_a | $egrep "i[3,4,5,6]86" > $devnull)
@@ -40,33 +40,33 @@ then
     # Win ################
     if (echo $uname_a | $egrep "CYGWIN" > $devnull)
     then
-	echo ${prefix}-Win
-	exit 0
+        echo ${prefix}-Win
+        exit 0
     # Linux ###############
     elif (echo $uname_a | $egrep "Linux" > $devnull)
     then
-	prefix=${prefix}-Linux
-	if (test -x $file && -x $binary)
-	then
-	    # LinuxAOUT only if  file does not match ELF
-	    if ($file $binary | $egrep -v "ELF" > $devnull)
-	    then
-		echo ${prefix}AOUT
-		exit 0
-	    fi
-	fi
-	# everything else is assumed to be Linux ELF
+        prefix=${prefix}-Linux
+        if (test -x $file && -x $binary)
+        then
+            # LinuxAOUT only if  file does not match ELF
+            if ($file $binary | $egrep -v "ELF" > $devnull)
+            then
+                echo ${prefix}AOUT
+                exit 0
+            fi
+        fi
+        # everything else is assumed to be Linux ELF
         # check for libc5
         if (echo `$ldd $binary` | $egrep "libc.so.5" > $devnull)
-	then
-	    echo "${prefix}-libc5"
-	else
-	    echo ${prefix}
-	fi    
-	exit 0
+        then
+            echo "${prefix}-libc5"
+        else
+            echo ${prefix}
+        fi
+        exit 0
     else
-	echo ${prefix}-Unknown
-	exit 1
+        echo ${prefix}-Unknown
+        exit 1
     fi
 # SunOS ########################################################
 elif (echo $uname_a | $egrep "SunOS" > $devnull)
@@ -77,18 +77,18 @@ then
     else
       prefix=SunOS
     fi
-    if (echo $uname_a | $egrep "4\.[0-9]" > $devnull) 
+    if (echo $uname_a | $egrep "4\.[0-9]" > $devnull)
     then
-	echo ${prefix}-4
-	exit 0
-    # HPUX-10
+        echo ${prefix}-4
+        exit 0
+    # Solaris
     elif (echo $uname_a | $egrep "5\.[0-9]" > $devnull)
     then
-	echo ${prefix}-5
-	exit 0
+        echo ${prefix}-5
+        exit 0
     else
-	echo ${prefix}-Unknown
-	exit 1
+        echo ${prefix}-Unknown
+        exit 1
     fi
 # DecAlpha ########################################################
 elif (echo $uname_a | $egrep "alpha" > $devnull)
@@ -96,11 +96,11 @@ then
     prefix=DecAlpha
     if (echo $uname_a | $egrep "Linux" > $devnull)
     then
-	echo ${prefix}-Linux
-	exit 0 
+        echo ${prefix}-Linux
+        exit 0
     else
-	echo ${prefix}-Unknown
-	exit 1 
+        echo ${prefix}-Unknown
+        exit 1
     fi
 # IRIX ########################################################
 elif (echo $uname_a | $egrep "IRIX" > $devnull)
@@ -108,11 +108,11 @@ then
     prefix=IRIX
     if (echo $uname_a | $egrep "6\.[0-9]" > $devnull)
     then
-	echo ${prefix}-6
-	exit 0 
+        echo ${prefix}-6
+        exit 0
     else
-	echo ${prefix}-Unknown
-	exit 1 
+        echo ${prefix}-Unknown
+        exit 1
     fi
 # AIX ########################################################
 elif (echo $uname_a | $egrep "AIX" > $devnull)
@@ -120,19 +120,17 @@ then
     prefix=AIX
     if (uname -v |  $egrep "4" > $devnull)
     then
-	echo ${prefix}-4
-	exit 0
+        echo ${prefix}-4
+        exit 0
     elif (uname -v |  $egrep "3" > $devnull)
     then
-	echo ${prefix}-3
-	exit 0
+        echo ${prefix}-3
+        exit 0
     else
-	echo ${prefix}-Unknown
-	exit 1
+        echo ${prefix}-Unknown
+        exit 1
     fi
 else # Unknown ########################################################
     echo Unknown
     exit 2
 fi
-
-
