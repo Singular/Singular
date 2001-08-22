6434aa9a9973767ac883d051c5b3cee1a69278d1
Hans Schönemann <hannes@mathematik.uni-kl.de>
2001-08-22 14:36:37 +0000

    *hannes: version 0.3.3
    git-svn-id: file:///usr/local/Singular/svn/trunk@5599 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- libfac/factor/version.cc
+++ libfac/factor/version.cc
@@ -1,13 +1,13 @@
 /* Copyright 1997 Michael Messollen. All rights reserved. */
 ///////////////////////////////////////////////////////////////////////////////
 // emacs edit mode for this file is -*- C++ -*-
-// $Id: version.cc,v 1.5 2001-05-04 15:40:11 Singular Exp $
+// $Id: version.cc,v 1.6 2001-08-22 14:35:27 Singular Exp $
 static char * rcsid = "@(#) libfac version 0.2.4 [May 29 1997] (C) 1996-97 M. Messollen";
 ///////////////////////////////////////////////////////////////////////////////
 
 const char * libfac_name = "Factorization and characteristic sets library";
-const char * libfac_version = "0.3.2";
-const char * libfac_date = "Jan 31 2001";
+const char * libfac_version = "0.3.3";
+const char * libfac_date = "Aug 22 2001";
 const char * libfac_author = "M. Messollen";
 
 int    libfac_interruptflag=0;
