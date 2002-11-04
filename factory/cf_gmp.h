3ad8af19b12a63b428747d5bb18f6f7731b876d2
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2002-11-04 17:06:25 +0000

    + gmp workaround
    git-svn-id: file:///usr/local/Singular/svn/trunk@6304 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_gmp.h
+++ factory/cf_gmp.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_gmp.h,v 1.2 1997-06-19 12:24:27 schmidt Exp $ */
+/* $Id: cf_gmp.h,v 1.3 2002-11-04 17:06:24 bricken Exp $ */
 
 #ifndef INCL_CF_GMP_H
 #define INCL_CF_GMP_H
@@ -7,7 +7,13 @@
 #include <config.h>
 
 extern "C" {
+#ifdef __cplusplus
+#undef __cplusplus
 #include <gmp.h>
+#define __cplusplus
+#else
+#include <gmp.h>
+#endif
 }
 
 #endif /* ! INCL_CF_GMP_H */
