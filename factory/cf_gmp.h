46cabe8b835c1f035c815d020c47b2ca632600a9
Hans Schönemann <hannes@mathematik.uni-kl.de>
2002-11-05 11:00:12 +0000

    *hannes: gmp-4 adaption
    git-svn-id: file:///usr/local/Singular/svn/trunk@6307 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/cf_gmp.h
+++ factory/cf_gmp.h
@@ -1,19 +1,17 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: cf_gmp.h,v 1.3 2002-11-04 17:06:24 bricken Exp $ */
+/* $Id: cf_gmp.h,v 1.4 2002-11-05 11:00:12 Singular Exp $ */
 
 #ifndef INCL_CF_GMP_H
 #define INCL_CF_GMP_H
 
-#include <config.h>
-
-extern "C" {
 #ifdef __cplusplus
+extern "C" {
 #undef __cplusplus
 #include <gmp.h>
 #define __cplusplus
+}
 #else
 #include <gmp.h>
 #endif
-}
 
 #endif /* ! INCL_CF_GMP_H */
