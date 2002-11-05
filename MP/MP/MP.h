21a8befd0c11806b3654c34f88c08f7a750354cb
Hans Schönemann <hannes@mathematik.uni-kl.de>
2002-11-05 13:01:17 +0000

    *hannes: gmp-4 adaption
    git-svn-id: file:///usr/local/Singular/svn/trunk@6309 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- MP/MP/MP.h
+++ MP/MP/MP.h
@@ -1610,7 +1610,7 @@ EXTERN long IMP_GmpIntAsciiSize _ANSI_ARGS_((MP_ApInt_t mp_apint));
 #include "MP.h"
 
 #ifdef MP_HAVE_GMP_APREAL
-#include "gmp.h"
+#include "mp_gmp.h"
 
 EXTERN MP_BigRealOps_t imp_gmp_bigreal_ops;
 
@@ -1669,7 +1669,7 @@ EXTERN GEN (*IMP_AllocCgeti) _ANSI_ARGS_((long));
 EXTERN void (*IMP_FreeCgeti) _ANSI_ARGS_((GEN));
 
 #ifdef HAVE_GMP_PARI_CONVERSIONS
-#include "gmp.h"
+#include "mp_gmp.h"
 EXTERN mpz_ptr _pari_to_gmp(GEN pnum, mpz_ptr *gnum_ptr);
 EXTERN GEN     _gmp_to_pari(mpz_ptr gnum);
 #endif
@@ -1727,7 +1727,7 @@ typedef struct{
 
 #ifdef MP_HAVE_SAC
 #include "saclib.h"
-#include "gmp.h"
+#include "mp_gmp.h"
 
 EXTERN MP_BigIntOps_t sac_ops;
 
