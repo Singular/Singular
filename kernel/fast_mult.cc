cd518904e5a9af3cb906d82573eb15048b10fabf
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-02-08 15:00:05 +0000

    *bricken: end recursion removed
    git-svn-id: file:///usr/local/Singular/svn/trunk@7713 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/fast_mult.cc
+++ kernel/fast_mult.cc
@@ -1,10 +1,11 @@
 #include "fast_mult.h"
+#include "kbuckets.h"
 static void degsplit(poly p,int n,poly &p1,poly&p2){
   poly erg1_i, erg2_i;
   erg1_i=NULL;
   erg2_i=NULL;
   while(p){
-    if(pFDeg(p)>=n){
+    if(pGetExp(p,1)>=n){
       if (p1==NULL){
 	p1=p;
       } else{
@@ -31,17 +32,18 @@ static void degsplit(poly p,int n,poly &p1,poly&p2){
 }
 static void div_by_x_power_n(poly p, int n){
   while(p){
-    assume(pFDeg(p)>=n);
+    assume(pGetExp(p,1)>=n);
     int e=pGetExp(p,1);
     pSetExp(p,1,e-n);
     p=pNext(p);
   }
 }
-poly unifastmult(poly f,poly g){
+
+poly do_unifastmult(poly f,poly g){
   int n=1;
   if ((f==NULL)||(g==NULL)) return NULL;
-  int df=pFDeg(f);
-  int dg=pFDeg(g);
+  int df=pGetExp(f,1);//pFDeg(f);
+    int dg=pGetExp(g,1);//pFDeg(g);
 	
   int dm;
   if(df>dg){
@@ -76,7 +78,7 @@ poly unifastmult(poly f,poly g){
   
   poly pbig=unifastmult(pAdd(f0,f1),pAdd(g0,g1));
   
-  poly erg=p11;
+  poly erg=NULL;//=p11;
  
   poly factor=pOne();
   pSetExp(factor,1,n);
@@ -100,3 +102,107 @@ poly unifastmult(poly f,poly g){
   pDelete(&factor);
   return(erg);
 }
+
+poly do_unifastmult_buckets(poly f,poly g){
+   
+  
+  // BOOLEAN corr=lenS_correct(strat);
+
+  int n=1;
+  if ((f==NULL)||(g==NULL)) return NULL;
+  int df=pGetExp(f,1);//pFDeg(f);
+    int dg=pGetExp(g,1);//pFDeg(g);
+	
+  int dm;
+  if(df>dg){
+    dm=df;
+  }else{
+    dm=dg;
+  }
+  while(n<=dm)
+    {
+      n*=2;
+    }
+  int pseudo_len=0;
+  if(n==1){
+    return ppMult_qq(f,g);
+
+  }
+  kBucket_pt erg_bucket= kBucketCreate(currRing);
+  kBucketInit(erg_bucket,NULL,0 /*pLength(P.p)*/);
+  //poly erg_mult=pOne();
+  int pot=n/2;
+  assume(pot*2==n);
+  poly f1=NULL;
+  //f/(x^(pot));
+  poly f0=NULL;//f-(x^(pot)*F1);
+  degsplit(pCopy(f),pot,f1,f0);
+  div_by_x_power_n(f1,pot);
+  // poly G1=g/(x^(pot));
+  //poly G0=g-(x^(pot)*G1);
+  poly g1=NULL;
+  poly g0=NULL;
+  degsplit(pCopy(g),pot,g1,g0);
+  div_by_x_power_n(g1,pot);
+  
+  poly p00=unifastmult(f0,g0);
+    poly p11=unifastmult(f1,g1);
+  
+  poly pbig=unifastmult(pAdd(f0,f1),pAdd(g0,g1));
+  
+  //poly erg=p11;
+  
+  poly factor=pOne();//pCopy(erg_mult);
+  pSetExp(factor,1,n);
+  pseudo_len=0;
+  kBucket_Add_q(erg_bucket,ppMult_mm(p11,factor),&pseudo_len);
+  pseudo_len=0;
+  kBucket_Add_q(erg_bucket,pCopy(p00),&pseudo_len);
+  pSetExp(factor,1,pot);
+  //poly sum=pbig;
+  pseudo_len=0;
+  kBucket_Add_q(erg_bucket,pMult_mm(pbig,factor),&pseudo_len);
+  pseudo_len=0;
+  kBucket_Add_q(erg_bucket,pMult_mm(pNeg(p00),factor),&pseudo_len);
+  pseudo_len=0;
+  kBucket_Add_q(erg_bucket,pMult_mm(pNeg(p11),factor),&pseudo_len);
+  //  pSetExp(factor,1,pot);
+ 
+  // sum=pAdd(sum,pNeg(p00));
+ 
+
+  //sum=pAdd(sum,pNeg(p11));
+
+ 
+  //sum=pMult_mm(sum,factor);
+ 
+  pseudo_len=0;
+  //  kBucket_Add_q(erg_bucket,sum,&pseudo_len);
+  //  erg=pAdd(erg,p00);
+ 
+  pDelete(&factor);
+  //return(erg);
+  //  pDelete(&erg_mult);
+  int len=0;
+  poly erg=NULL;
+  kBucketClear(erg_bucket,&erg,&len);
+  kBucketDestroy(&erg_bucket);
+  //pNormalize(P.p);
+  return erg;
+}
+poly unifastmult(poly f,poly g){
+  if((f==NULL)||(g==NULL)) return NULL;
+  int df=pGetExp(f,1);
+  int dg=pGetExp(g,1);
+  if ((df==1)||(dg==1))
+    return ppMult_qq(f,g);
+  if (df*dg<100)
+    return ppMult_qq(f,g);
+  // if (df*dg>10000)
+  //  return
+  //    do_unifastmult_buckets(f,g);
+  //else
+  return do_unifastmult(f,g);
+
+}
+
