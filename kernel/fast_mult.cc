1903b7f98b7252fcf5bf02cbecaa4d79d4d87556
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-03-31 11:52:48 +0000

    *bricken: warning for polys of length shorter 2
    git-svn-id: file:///usr/local/Singular/svn/trunk@7804 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/fast_mult.cc
+++ kernel/fast_mult.cc
@@ -535,7 +535,9 @@ poly pFastPowerMC(poly f, int n, ring r){
   if(rChar(r)!=0)
     Werror("Char not 0, pFastPowerMC not implemented for this case");
   if (n<=1)
-    Werror("not implemented for so small n, recursion fails");
+    Werror("not implemented for so small n, recursion fails");//should be length(f)
+   if (pLength(f)<=1)
+    Werror("not implemented for so small lenght of f, recursion fails");
   //  number null_number=n_Init(0,r);
   number* facult=(number*) omalloc((n+1)*sizeof(number));
   facult[0]=n_Init(1,r);
