dee0c0bfc5091a87dbbbefd919c1b23da3276c0a
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-04-14 08:33:40 +0000

    *bricken: use si_gmp.h
    git-svn-id: file:///usr/local/Singular/svn/trunk@7821 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/misc.cc
+++ Singular/misc.cc
@@ -51,9 +51,10 @@
   extern const char * libfac_version;
   extern const char * libfac_date;
 #endif
-extern "C" {
-#include <gmp.h>
-}
+//extern "C" {
+//#include <gmp.h>
+//}
+#include <si_gmp.h>
 #ifdef HAVE_MPSR
 #include <MP_Config.h>
 #endif
