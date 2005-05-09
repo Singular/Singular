f4adfcbbf4e6a189938d7ffb03fbf5d42b6a0250
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-05-09 09:04:40 +0000

    *bricken: tgb.h now smaller
    git-svn-id: file:///usr/local/Singular/svn/trunk@8100 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/tgb.h
+++ kernel/tgb.h
@@ -3,21 +3,12 @@
 //#define OM_CHECK 3
 //#define OM_TRACK 5
 #include "mod2.h"
-#include <omalloc.h>
-#include "p_polys.h"
 
-#include "ideals.h"
 #include "ring.h"
-#include "febase.h"
+
 #include "structs.h"
-#include "polys.h"
-#include "stdlib.h"
 
 
-#include "kutil.h"
-#include "kInline.cc"
-#include "kstd1.h"
-#include "kbuckets.h"
 
 ideal t_rep_gb(ring r,ideal arg_I, BOOLEAN F4_mode=FALSE);
 #endif
