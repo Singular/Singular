0f7000a8ae34e15ab663ec770c361829783f9a11
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-05-25 15:34:54 +0000

    *bricken: even more ops
    git-svn-id: file:///usr/local/Singular/svn/trunk@8318 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/wrapper.h
+++ modules/wrapper.h
@@ -1,4 +1,4 @@
-//$Id: wrapper.h,v 1.4 2005-05-25 13:32:00 bricken Exp $
+//$Id: wrapper.h,v 1.5 2005-05-25 15:34:54 bricken Exp $
 #include "mod2.h"
 #include "numbers.h"
 #include "febase.h"
@@ -8,27 +8,37 @@
 using namespace boost::python;
 class Number{
   
-public:
-    friend Number operator+(const Number& n1, const Number& n2);
-    friend Number operator-(const Number& n1, const Number& n2);
-    friend Number operator/(const Number& n1, const Number& n2);
-    friend Number operator*(const Number& n1, const Number& n2);
-    friend bool operator==(const Number& n1, const Number& n2);
-    Number& operator=(const Number& n2){
-      //durch Reihenfolge Selbstzuweisungen berücksichtigt
-      number nc=n_Copy(n2.n,n2.r);
-      n_Delete(&n,r);
-      r=n2.r;
-      n=nc;
-      return *this;
-    }
-    Number operator-(){
-      Number t(*this);
-      t.n=n_Copy(n,r);
-      t.n=n_Neg(t.n,r);
-      return t;
-    }
-    Number& operator+=(const Number & n2){
+ public:
+  friend Number operator+(const Number& n1, const Number& n2);
+  friend Number operator-(const Number& n1, const Number& n2);
+  friend Number operator/(const Number& n1, const Number& n2);
+  friend Number operator*(const Number& n1, const Number& n2);
+  friend bool operator==(const Number& n1, const Number& n2);
+  friend Number operator+(const Number& n1, int n2);
+  friend Number operator-(const Number& n1, int n2);
+  friend Number operator/(const Number& n1, int n2);
+  friend Number operator*(const Number& n1, int n2);
+  friend bool operator==(const Number& n1, int n2);
+  friend Number operator+(int n1, const Number& n2);
+  friend Number operator-(int n1, const Number& n2);
+  friend Number operator/(int n1, const Number& n2);
+  friend Number operator*(int n1, const Number& n2);
+  friend bool operator==(int n1, const Number& n2);
+  Number& operator=(const Number& n2){
+    //durch Reihenfolge Selbstzuweisungen berücksichtigt
+    number nc=n_Copy(n2.n,n2.r);
+    n_Delete(&n,r);
+    r=n2.r;
+    n=nc;
+    return *this;
+  }
+  Number operator-(){
+    Number t(*this);
+    t.n=n_Copy(n,r);
+    t.n=n_Neg(t.n,r);
+    return t;
+  }
+  Number& operator+=(const Number & n2){
     if (r!=n2.r){
       Werror("not the same ring");
       return *this;
@@ -68,6 +78,58 @@ public:
     n=nv;
     return *this;
   }
+
+
+
+
+
+
+
+
+
+
+  Number& operator=(int n2){
+    n_Delete(&n,r);
+    n=n_Init(n2,r);
+    return *this;
+  }
+  
+  Number& operator+=(int n2){
+    number n2n=n_Init(n2,r);
+    number nv=n_Add(n,n2n,r);
+    n_Delete(&n,r);
+    n_Delete(&n2n,r);
+    n=nv;
+    return *this;
+  }
+  Number& operator*=(int n2){
+    number n2n=n_Init(n2,r);
+    number nv=n_Mult(n,n2n,r);
+    n_Delete(&n,r);
+    n_Delete(&n2n,r);
+    n=nv;
+    return *this;
+  }
+  Number& operator-=(int n2){
+
+    number n2n=n_Init(n2,r);
+    number nv=n_Sub(n,n2n,r);
+    n_Delete(&n,r);
+    n_Delete(&n2n,r);
+    n=nv;
+    return *this;
+  }
+  Number& operator/=(int n2){  
+    number n2n=n_Init(n2,r);
+    number nv=n_Div(n,n2n,r);
+    n_Delete(&n,r);
+    n_Delete(&n2n,r);
+    n=nv;
+    return *this;
+  }
+
+
+
   Number(){
     r=currRing;
     n=n_Init(0,r);
@@ -84,7 +146,7 @@ public:
     this->n=n_Init(n,r);
     this->r=r;
   }
-  Number(int n){
+  explicit Number(int n){
     r=currRing;
     this->n=n_Init(n,r);
   }
@@ -95,7 +157,7 @@ public:
     n_Delete(&n,r);
   }
 
-protected:
+ protected:
   number n;
   ring r;
 
@@ -133,14 +195,80 @@ bool operator==(const Number &n1, const Number& n2){
     return false;
   return n_Equal(n1.n,n2.n,n1.r);
 }
+
+
+Number operator+(const Number &n1, int n2){
+  Number erg(n1);
+  erg+=Number(n2,n1.r);
+  return erg;
+}
+Number operator*(const Number &n1, int n2){
+  Number erg(n1);
+  erg*=Number(n2,n1.r);
+  return erg;
+}
+Number operator-(const Number &n1, int n2){
+  Number erg(n1);
+  erg-=Number(n2,n1.r);
+  return erg;
+}
+Number operator/(const Number &n1, int n2){
+  Number erg(n1);
+  erg/=Number(n2,n1.r);
+  return erg;
+}
+bool operator==(const Number &n1, int n2){
+  return n_Equal(n1.n,Number(n2,n1.r).n,n1.r);
+}
+Number operator+(int n1, const Number& n2){
+  Number erg(n2);
+  return erg+=Number(n1,n2.r);
+}
+Number operator-(int n1, const Number& n2){
+
+  Number erg(n1,n2.r);
+  return erg-=n2;
+}
+Number operator/(int n1, const Number& n2){
+  Number erg(n1,n2.r);
+  return erg/=n2;
+}
+
+Number operator*(int n1, const Number& n2){
+  Number erg(n2);
+  return erg*=Number(n1,n2.r);
+}
+bool operator==(int n1, const Number& n2){
+  return n2==Number(n1,n2.r);
+}
+
+
 BOOST_PYTHON_MODULE(Singular){
   boost::python::class_<Number>("number")
     .def(boost::python::init <int>())
     .def("__str__", as_str)
     .def(-self)
+    .def(self*=self)
+    .def(self+=self)
+    .def(self-=self)
+    .def(self/=self)
     .def(self==self)
     .def(self+self)
     .def(self*self)
     .def(self/self)
-    .def(self-self);
+    .def(self-self)
+    .def(int()==self)
+    .def(int()+self)
+    .def(int()*self)
+    .def(int()/self)
+    .def(int()-self)
+    .def(self==int())
+    .def(self+int())
+    .def(self*int())
+    .def(self/int())
+    .def(self-int())
+    .def(self*=int())
+    .def(self+=int())
+    .def(self-=int())
+    .def(self/=int());
 }
