057e4d3ca8bf916daba09789459baef304afd8e2
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-06-23 15:24:01 +0000

    *bricken: beautified
    git-svn-id: file:///usr/local/Singular/svn/trunk@8390 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/openmathserver/binding.py
+++ modules/openmathserver/binding.py
@@ -1,51 +1,51 @@
 from objects import OMObjectBase, OMSymbol
-from cd import *
-from omexceptions import *
+from cd import OMCD
+from omexceptions import UnsupportedOperationError
 from itertools import izip
 from copy import copy
-cdFns1=OMCD("fns1")
-lambdasym=OMSymbol("lambda",cdFns1)
+cdFns1 = OMCD("fns1")
+lambdasym = OMSymbol("lambda", cdFns1)
 
 def islambda(sym):
-    return lambdasym==sym
+    return lambdasym == sym
     
 class OMBinding(OMObjectBase):
     """hopefully fixed possible problems: reevaluation writes new scope, if it isn't
        meant so, references do not work correctly because of scopes
        solve this by first evaluation to bounded OMBinding"""
-    def __init__(self, binder,variables,block):
-        super(OMBinding,self).__init__()
-        self.block=block
-        self.binder=binder
-        self.variables=variables
-        self.bounded=False
-    def evaluate(self,context):
+    def __init__(self, binder, variables, block):
+        super(OMBinding, self).__init__()
+        self.block = block
+        self.binder = binder
+        self.variables = variables
+        self.bounded = False
+    def evaluate(self, context):
         assert islambda(self.binder)
         if not self.bounded:
-            mycopy=copy(self)
-            mycopy.scope=context.scopeFromCurrentScope()
-            mycopy.bounded=True
+            mycopy = copy(self)
+            mycopy.scope = context.scopeFromCurrentScope()
+            mycopy.bounded = True
             return mycopy
         else:
             return self
     def bind(self, args):
         #print args, self.variables
-        assert len(args)==len(self.variables)
-        varBindings=dict(izip([i.name for i in self.variables],args))
+        assert len(args) == len(self.variables)
+        varBindings = dict(izip([i.name for i in self.variables], args))
         self.scope.push(varBindings)
     def unbind(self):
         self.scope.pop()
 
-    def calcErg(self,context):
-        return context.evaluateInScope(self.block,self.scope)
-    def __call__(self, context,*args):
+    def calcErg(self, context):
+        return context.evaluateInScope(self.block, self.scope)
+    def __call__(self, context, *args):
         self.bind(args)
-        erg=self.calcErg(context)
+        erg = self.calcErg(context)
         self.unbind()
         #print "__call__ erg is", erg
         return erg   
         
-    XMLtag="OMBIND"
+    XMLtag = "OMBIND"
     def getChildren(self):
         return [self.binder]+self.variables+[self.block]
     def setChildren(self):
