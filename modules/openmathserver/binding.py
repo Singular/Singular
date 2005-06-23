e25c5effa9b6fa015db14b7bde31ddca40b78786
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-06-23 15:29:01 +0000

    *bricken: beautified
    git-svn-id: file:///usr/local/Singular/svn/trunk@8391 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/openmathserver/binding.py
+++ modules/openmathserver/binding.py
@@ -1,3 +1,4 @@
+"""Definition of the OMBinding object"""
 from objects import OMObjectBase, OMSymbol
 from cd import OMCD
 from omexceptions import UnsupportedOperationError
@@ -7,10 +8,12 @@ cdFns1 = OMCD("fns1")
 lambdasym = OMSymbol("lambda", cdFns1)
 
 def islambda(sym):
+    "return True, iff sym is the lambda binder"
     return lambdasym == sym
     
 class OMBinding(OMObjectBase):
-    """hopefully fixed possible problems: reevaluation writes new scope, if it isn't
+    """hopefully fixed possible problems: reevaluation writes new scope, 
+       if it isn't
        meant so, references do not work correctly because of scopes
        solve this by first evaluation to bounded OMBinding"""
     def __init__(self, binder, variables, block):
@@ -20,6 +23,7 @@ class OMBinding(OMObjectBase):
         self.variables = variables
         self.bounded = False
     def evaluate(self, context):
+        "evaluate the OMbinding in context"
         assert islambda(self.binder)
         if not self.bounded:
             mycopy = copy(self)
@@ -29,14 +33,17 @@ class OMBinding(OMObjectBase):
         else:
             return self
     def bind(self, args):
+        "bind arguments to values"
         #print args, self.variables
         assert len(args) == len(self.variables)
         varBindings = dict(izip([i.name for i in self.variables], args))
         self.scope.push(varBindings)
     def unbind(self):
+        "unbind the arguments"
         self.scope.pop()
 
     def calcErg(self, context):
+        "do the actual computation"
         return context.evaluateInScope(self.block, self.scope)
     def __call__(self, context, *args):
         self.bind(args)
@@ -47,7 +54,6 @@ class OMBinding(OMObjectBase):
         
     XMLtag = "OMBIND"
     def getChildren(self):
+        "get children for (XML) representation"
         return [self.binder]+self.variables+[self.block]
-    def setChildren(self):
-        raise UnsupportedOperationError 
-        
+    
