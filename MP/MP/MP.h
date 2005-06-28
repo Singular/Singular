8cfae9e49d7d3f953224e84978f38bdc5d131bbd
Hans Schönemann <hannes@mathematik.uni-kl.de>
2005-06-28 15:12:08 +0000

    *hannes: fix of Mariano Suarez-Alvarez: gcc 4
    git-svn-id: file:///usr/local/Singular/svn/trunk@8407 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- MP/MP/MP.h
+++ MP/MP/MP.h
@@ -985,7 +985,7 @@ EXTERN void MP_LogEvent _ANSI_ARGS_((MP_Link_pt link, char *event, char *msg));
 EXTERN MP_Status_t MP_SetError _ANSI_ARGS_((MP_Link_pt link, 
                                             MP_Status_t the_err));
 
-#define MP_ClearError(link) ((MP_Status_t) (link)->MP_errno = MP_Success)
+#define MP_ClearError(link) ((link)->MP_errno = MP_Success)
 
 EXTERN void MP_PrintError _ANSI_ARGS_((MP_Link_pt link));
 
