10c308a4a31d93124e7cc9091cc9d37a435d17f9
Hans Schönemann <hannes@mathematik.uni-kl.de>
2005-07-28 14:37:16 +0000

    *hannes: perl and -a
    git-svn-id: file:///usr/local/Singular/svn/trunk@8467 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- omalloc/om_Alloc.h
+++ omalloc/om_Alloc.h
@@ -3,7 +3,7 @@
  *  Purpose: declaration of public routines for omalloc
  *  Author:  obachman@mathematik.uni-kl.de (Olaf Bachmann)
  *  Created: 11/99
- *  Version: $Id: om_Alloc.h,v 1.3 2001-04-30 09:02:12 Singular Exp $
+ *  Version: $Id: om_Alloc.h,v 1.4 2005-07-28 14:37:16 Singular Exp $
  *******************************************************************/
 #ifndef OM_ALLOC_H
 #define OM_ALLOC_H
@@ -15,26 +15,47 @@
 extern "C" {
 #endif
 
+/*MAKEHEADER*/
 #include "omConfig.h"
+/*MAKEHEADER*/
 #include "omDerivedConfig.h"
+/*MAKEHEADER*/
 #include "omError.h"
+/*MAKEHEADER*/
 #include "omStructs.h"
+/*MAKEHEADER*/
 #include "omAllocDecl.h"
+/*MAKEHEADER*/
 #include "omInlineDecl.h"
+/*MAKEHEADER*/
 #include "omBin.h"
+/*MAKEHEADER*/
 #include "omMemOps.h"
+/*MAKEHEADER*/
 #include "omList.h"
+/*MAKEHEADER*/
 #include "omFindExec.h"
+/*MAKEHEADER*/
 #include "omGetBackTrace.h"
+/*MAKEHEADER*/
 #include "omRet2Info.h"
+/*MAKEHEADER*/
 #include "omStats.h"
+/*MAKEHEADER*/
 #include "omOpts.h"
+/*MAKEHEADER*/
 #include "omBinPage.h"
+/*MAKEHEADER*/
 #include "omAllocSystem.h"
+/*MAKEHEADER*/
 #include "omTables.h"
+/*MAKEHEADER*/
 #include "omAllocPrivate.h"
+/*MAKEHEADER*/
 #include "omDebug.h"
+/*MAKEHEADER*/
 #include "omInline.h"
+/*MAKEHEADER*/
 #include "omAllocFunc.h"
 
 #ifdef __cplusplus
