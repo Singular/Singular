96b56400341f7d5a950ef51cf6ade27b7acc813a
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-08-19 12:49:57 +0000

    *bricken: now requires NOSTRINGIO not to be defined in factory/config.h, further work on factory
    git-svn-id: file:///usr/local/Singular/svn/trunk@8538 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/python/wrapper.h
+++ modules/python/wrapper.h
@@ -1,8 +1,9 @@
-//$Id: wrapper.h,v 1.10 2005-08-17 16:30:46 bricken Exp $
+//$Id: wrapper.h,v 1.11 2005-08-19 12:49:57 bricken Exp $
 #ifndef PYTHON_SINGULAR_WRAPPER_HEADER
 #define PYTHON_SINGULAR_WRAPPER_HEADER
 #include <Python.h>
 #include <boost/python.hpp>
+#include <sstream>
 #include "mod2.h"
 #include "numbers.h"
 #include "febase.h"
@@ -10,10 +11,14 @@
 #include "Number.h"
 #include "Poly.h"
 #include "PowerSeries.h"
+#include <factory.h>
+
 using namespace boost::python;
+typedef std::basic_stringstream<char>  mysstream;
 Vector unitVector0(int i){
   return unitVector(i,currRing);
 }
+
 static boost::python::object Number_as_str(Number n)
 {
   using boost::python::str;
@@ -22,7 +27,13 @@ static boost::python::object Number_as_str(Number n)
   char* out=StringAppendS("");
   return boost::python::str(out,strlen(out));
 }
-
+static boost::python::object CF_as_str(const CanonicalForm& f)
+{
+  using boost::python::str;
+  mysstream s;
+  s<<f;
+  return boost::python::str(s.str());
+}
 static boost::python::object Poly_as_str(Poly& p)
 {
   using boost::python::str;
@@ -41,6 +52,33 @@ static boost::python::object Vector_as_str(Vector& p)
 }
 
 BOOST_PYTHON_MODULE(Singular){
+  boost::python::class_<CanonicalForm>("canonical_form")
+    .def(boost::python::init <const int>())
+    .def("__str__", CF_as_str)
+    .def(-self)
+    .def(self*=self)
+    .def(self+=self)
+    .def(self-=self)
+    .def(self/=self)
+    .def(self==self)
+    .def(self+self)
+    .def(self*self)
+    .def(self/self)
+    .def(self-self)
+    .def(int()==self)
+    .def(int()+self)
+    .def(int()*self)
+    .def(int()/self)
+    .def(int()-self)
+    .def(self==int())
+    .def(self+int())
+    .def(self*int())
+    .def(self/int())
+    .def(self-int())
+    .def(self*=int())
+    .def(self+=int())
+    .def(self-=int())
+    .def(self/=int());
   boost::python::class_<Number>("number")
     .def(boost::python::init <int>())
     .def("__str__", Number_as_str)
@@ -78,8 +116,7 @@ BOOST_PYTHON_MODULE(Singular){
     //read monomials (only) from string
     .def(boost::python::init <const char* >())
     
-    //.def("__str__", as_str)
-    //.def(-self)
+    .def(-self)
     .def(self*=self)
     .def(self+=self)
     //    .def(self-=self)
@@ -93,13 +130,12 @@ BOOST_PYTHON_MODULE(Singular){
     .def(self*=Number())
     .def(self*self);
   boost::python::class_<Vector>("vector")
-    
     .def(boost::python::init <>())
     .def("__str__", Vector_as_str)
     
     //    .def("__str__", Poly_as_str)
     .def("__iter__", boost::python::iterator<Vector>())
-   
+    .def(-self)
     .def(self+=self)
    
     .def(self+self)
