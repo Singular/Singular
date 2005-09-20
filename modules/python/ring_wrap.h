8062aa63166d4d1d159c8cd2c00a2baf0be49271
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-09-20 12:22:59 +0000

    *bricken: refcounting bug fixed
    git-svn-id: file:///usr/local/Singular/svn/trunk@8632 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/python/ring_wrap.h
+++ modules/python/ring_wrap.h
@@ -1,4 +1,4 @@
-//$Id: ring_wrap.h,v 1.3 2005-09-20 08:39:44 bricken Exp $
+//$Id: ring_wrap.h,v 1.4 2005-09-20 12:22:59 bricken Exp $
 #ifndef RING_WRAP_HEADER
 #define RING_WRAP_HEADER
 class Ring{
@@ -6,11 +6,14 @@ class Ring{
     ring pimpl;
     Ring(ring r=currRing): pimpl(r){
       if (r!=NULL)
-        ++r->ref;
+        ++(r->ref);
+    }
+    Ring(const Ring& r2):pimpl(r2.pimpl){
+      pimpl->ref++;
     }
     ~Ring(){
       if (pimpl!=NULL)
-        --pimpl->ref;
+        --(pimpl->ref);
     }
 };
 void export_ring();
