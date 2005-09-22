586d0ca93d34f6fedd6326631becc50a30876375
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-09-22 18:28:57 +0000

    *bricken: assertions
    git-svn-id: file:///usr/local/Singular/svn/trunk@8647 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/openmathserver/singularom.py
+++ modules/openmathserver/singularom.py
@@ -50,16 +50,14 @@ def encodeRing(r):
     return OMApply(poly_ring_dsym,[f,OMint(nv)])   
 
 def ringFromOM(ring_desc, ordering="dp"):
-  if not isinstance(ring_desc, OMApply):
-    raise SingularException("ringFromOM expects instance of OMApply")
+  assert isinstance(ring_desc, OMApply)
   if (ring_desc.args[0]==Rationals):
     i=ring_desc.args[1].getValue()
     return create_ring(char=0, nvars=i, ordering=ordering)
   raise SingularException("ring not supported")
 def polyFromOM(poly_desc):
   """assumes the right ring is set"""
-  if not isinstance(poly_desc, OMApply):
-    raise SingularException("polyFromOM expects instance of OMApply")
+  assert isinstance(poly_desc, OMApply)
   terms=[termFromOM(t) for t in poly_desc.args]
   res=polynomial(number(0))
   for t in terms:
@@ -67,11 +65,14 @@ def polyFromOM(poly_desc):
     #res+=t
   return res
 def termFromOM(term_desc):
-  if not isinstance(term_desc, OMApply):
-    raise SingularException("polyFromOM expects instance of OMApply")
+  assert isinstance(term_desc, OMApply)
+  assert len(term_desc.args)==singular.nvars(ring())+1
+  assert isinstance(term_desc.args[0], OMint)
+  
   coef=number(term_desc.args[0].getValue())
   exp=intvec()
   for e in term_desc.args[1:]:
+    assert isinstance(e,OMint)
     exp.append(e.getValue())
   #print coef, polynomial(exp)
   return coef*polynomial(exp)
@@ -79,11 +80,13 @@ def termFromOM(term_desc):
 def idealFromDMPL(dmpl):
   """assumes that the right ring is set"""
   i=ideal()
+  assert len(dmpl.args)>=1
   ps=[polyFromOM(d) for d in dmpl.args[1:]]
   for p in ps:
     i.append(p)
   return i
 def ringFromDMPLOrd(dmpl,o):
+  assert len(dmpl.args)>=1
   print OrderingTableBack[o]
   return ringFromOM(dmpl.args[0], ordering=OrderingTableBack[o])
 def encodeTerm(t):
@@ -93,10 +96,7 @@ def encodeTerm(t):
   c=singular.leadcoef(t)
   exponents=[OMint(i) for i in exponents]
   return OMApply(termsym,[OMint(str(c))]+exponents)
-def groebnerfunc(context, *args):
-  assert len(args)==2
-  ordering=args[0]
-  dmpl=args[1]
+def groebnerfunc(context, ordering, dmpl):
   r=ringFromDMPLOrd(dmpl,ordering)
   r.set()
   i=idealFromDMPL(dmpl)
