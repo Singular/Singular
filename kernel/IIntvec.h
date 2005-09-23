f12ddcdde3136bd52030cdeef8ef6bf42ffd45f0
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2005-09-23 13:09:37 +0000

    *bricken: initial version
    git-svn-id: file:///usr/local/Singular/svn/trunk@8656 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ kernel/IIntvec.h
@@ -0,0 +1,30 @@
+#ifndef ITERATABLE_INTVEC_H
+#define ITERATABLE_INTVEC_H
+#include <vector>
+#include "mod2.h"
+#include "intvec.h"
+class Intvec: public std::vector<int>{
+public:
+  Intvec(iterator first, 
+	iterator last,
+	const allocator_type& __a = allocator_type()):
+    std::vector<int>(first,last,__a){
+  }
+  Intvec(int n=0):std::vector<int>(n){}
+  Intvec(intvec& iv):std::vector<int>(iv.length()){
+    int n=iv.length();
+    for(int i=0;i<n;i++){
+      (*this)[i]=iv[i];
+    }
+  }
+  intvec* allocate_legacy_intvec_copy() const{
+    int s=size();
+    intvec* iv=new intvec(s);
+    
+    for(int i=0;i<s;i++){
+      (*iv)[i]=(*this)[i];
+    }
+    return iv;
+  }
+};
+#endif
