66b5b8c82efc6d9d9508f5ee9f49a7644f2635fe
Hans Schönemann <hannes@mathematik.uni-kl.de>
2006-01-31 15:00:57 +0000

    *hannes: solaris port
    git-svn-id: file:///usr/local/Singular/svn/trunk@8916 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/fegetopt.h
+++ Singular/fegetopt.h
@@ -100,7 +100,7 @@ struct fe_option
 #define required_argument        1
 #define optional_argument        2
 
-#if __STDC__ || defined(PROTO) || defined(__cplusplus)
+#if defined(__STDC__) || defined(PROTO) || defined(__cplusplus)
 /* #if defined(__GNU_LIBRARY__)*/
 /* Many other libraries have conflicting prototypes for getopt, with
    differences in the consts, in stdlib.h.  To avoid compilation
