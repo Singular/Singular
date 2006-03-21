8f643b762d88af1f4cc168f81fce3f097d1a7d48
Michael Brickenstein <bricken@mathematik.uni-kl.de>
2006-03-21 08:21:38 +0000

    *bricken: improved turaev/viro
    git-svn-id: file:///usr/local/Singular/svn/trunk@9028 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- modules/python/symm.py
+++ modules/python/symm.py
@@ -1,26 +1,7 @@
 from probstat import Cartesian
-from itertools import repeat,imap
-class chainmap(object):
-  """this class represents an equivalent relation,
-    which is implemented by repeated evaluatation of a map
-    c[n]=m means that n is equivalent to m"""
-  def __init__(self):
-    self.mydict={}
-  def canonical(self,var):
-    while var in self.mydict:
-      var=self.mydict[var]
-    return var
-  def all_rewritings(self,var):
-    pass
-  def rewrite(self, a,b):
-    a=self.canonical(a)
-    b=self.canonical(b)    
-    if not a==b:
-      self.mydict[a]=b
-  def __getitem__(self,key):
-    return self.canonical(key)
-  def __setitem__(self, val, key):
-    self.rewrite(val,key)
+from itertools import repeat,imap, ifilter
+from util import EquivalenceRelation
+
 class modPNumber:
   """simple class for Turaev/Viro colors,
     each color has an inverse, but it doesn't have to be an algebraic
@@ -39,47 +20,32 @@ class modPNumber:
   def __str__(self):
     return str(self.val)
 
-
 colors=3
-def constr_value_range(n):
-  return [modPNumber(i,n) for i in xrange(n)]
-value_range=constr_value_range(colors)
+value_range=[modPNumber(i,colors) for i in xrange(colors)]
 
-chain=chainmap()
-l12=[1,2]
+wrel=EquivalenceRelation()
+for i in value_range:
+  wrel[i]=-i
+weights_canonical_to_index=dict((weight,i+1) \
+    for (i,weight) in enumerate(ifilter( \
+        wrel.isCanonical,value_range)))
+wtrans=dict((weight,weights_canonical_to_index[wrel.canonical(weight)]) for weight in \
+    value_range)
+weights=max([wtrans[i] for i in weights_canonical_to_index])
+
 
 def constr_variations():
   return imap(tuple,Cartesian(list(repeat(value_range,6))))
+relation=EquivalenceRelation()
 for (a,b,c,d,e,f) in constr_variations():
-  chain[(a,b,c,d,e,f)]=(b,c,a,f,-d,-e)
-  chain[(a,b,c,d,e,f)]=(a,-d,-e,-b,-c,-f)
-
-class chaintrans(object):
-  def __init__(self, chain,full):
-    self.table={}
-    self.i=1
-    full=list(full)
-    for c in full:
-      if not c in chain.mydict:
-        self.insert(c)
-    for c in full:
-      self.table[c]=self.table[chain[c]]
-  def __getitem__(self, key):
-    return self.table[key]
-  def insert(self,key):
-    self.table[key]=self.i
-    self.i=self.i+1
-  def __str__(self):
-    return str(self.table)
-
-
-
-vartrans=chaintrans(chain, constr_variations())
-#we only needed this class for construction, can replace it by a function
-vartrans=vartrans.table
-del chain
-
-#weights=2
+  relation[(a,b,c,d,e,f)]=(b,c,a,f,-d,-e)
+  relation[(a,b,c,d,e,f)]=(a,-d,-e,-b,-c,-f)
+canonical_to_index=dict((symbol,weights+i+1) for (i,symbol) in \
+    enumerate(ifilter( \
+       relation.isCanonical,constr_variations())))
+vartrans=dict((symbol,canonical_to_index[relation.canonical(symbol)]) for symbol in \
+    constr_variations())
+symbols=max([vartrans[i] for i in canonical_to_index])
 
 from interpreter import *
 singular=SingularGlobalsProxy()
@@ -87,22 +53,13 @@ from util import *
 from Singular import *
 singular_var=singular.var
 
-def singvar(v):
+def six_j_symbol(v):
   i=vartrans[v]+weights
-  return vartable[i-1]
+  return var_cache[i-1]
   
-wmap=chainmap()
-for i in value_range:
-  wmap[i]=-i
-wtrans=chaintrans(wmap,value_range)
-wtrans=wtrans.table
-symbols=max([vartrans[i] for i in vartrans])
-weights=max([wtrans[i] for i in wtrans])
-
-del wmap
 
 def w(i):
-  return vartable[wtrans[i]-1]
+  return var_cache[wtrans[i]-1]
 r=create_ring(char=0,nvars=weights+symbols)
 r.set()
 print r
@@ -110,29 +67,35 @@ myideal=Ideal()
 def polysum(l):
   acc=Polynomial(0)
   for p in l:
-    acc=acc+p
-
+    acc+=p
   return acc
 
 
-vartable=[singular_var(x+1) for x in range(singular.nvars(Ring()))]
+var_cache=[singular_var(x+1) for x in range(singular.nvars(Ring()))]
 for (j1,j2,j3,j4,j5,j6,j7,j8,j9) in Cartesian(list(repeat(value_range,9))):
   p=\
-    singvar((j1,j2,j3,j7,j8,j9))*\
-    singvar((j4,j5,j6,-j7,-j8,-j9))+\
+    six_j_symbol((j1,j2,j3,j7,j8,j9))*\
+    six_j_symbol((j4,j5,j6,-j7,-j8,-j9))+\
     Number(-1)*\
     polysum([\
-      w(j)*singvar((j,j1,j2,-j4,-j5,j7))*\
-      singvar((j,j2,j3,-j5,-j6,j9))*
-      singvar((j,j3,j1,-j6,-j4,-j8))\
+      w(j)*six_j_symbol((j,j1,j2,-j4,-j5,j7))*\
+      six_j_symbol((j,j2,j3,-j5,-j6,j9))*
+      six_j_symbol((j,j3,j1,-j6,-j4,-j8))\
       for j in value_range])
   myideal.append(p)
 
-print "now calculating"
 
+back_table_v=dict((canonical_to_index[s],s) for s\
+    in canonical_to_index)
+back_table_w=dict((weights_canonical_to_index[w],w) for w\
+    in weights_canonical_to_index)
+back_table_joint=dict(back_table_v)
+back_table_joint.update(back_table_w)
+print "back", back_table_joint
 print "original length",len(myideal)
+print "now calculating"
 myideal=singular.simplify(myideal,4)
 print "simplified", len(myideal)
 gb=singular.slimgb(myideal)
 print gb
-print len(gb), "GB Elemente"
+print len(gb), "GB elements"
--- modules/python/util.py
+++ modules/python/util.py
@@ -15,4 +15,50 @@ def create_ring(char=0, nvars=1, varNames=None, ordering="dp", degvec=None):
   modulweights.append(0)
   l=[char,varNames,[[ordering,degvec],["C",modulweights]], Ideal()]
   return singular.ring(l)
-  
+
+
+class EquivalenceRelation(object):
+  """this class represents an equivalent relation,
+    which is implemented by repeated evaluatation of a map
+    c[n]=m means that n is equivalent to m"""
+  def __init__(self):
+    self.mydict={}
+  def canonical(self,var):
+    while var in self.mydict:
+      var=self.mydict[var]
+    return var
+  def isCanonical(self,var):
+    if self.canonical(var)==var:
+        return True
+    else:
+        return False
+  def all_rewritings(self,var):
+    pass
+  def rewrite(self, a,b):
+    a=self.canonical(a)
+    b=self.canonical(b)    
+    if not a==b:
+      self.mydict[a]=b
+  def __getitem__(self,key):
+    return self.canonical(key)
+  def __setitem__(self, val, key):
+    self.rewrite(val,key)
+    
+    
+class chaintrans(object):
+  def __init__(self, chain,full):
+    self.table={}
+    self.i=1
+    full=list(full)
+    for c in full:
+      if not c in chain.mydict:
+        self.insert(c)
+    for c in full:
+      self.table[c]=self.table[chain[c]]
+  def __getitem__(self, key):
+    return self.table[key]
+  def insert(self,key):
+    self.table[key]=self.i
+    self.i=self.i+1
+  def __str__(self):
+    return str(self.table)
