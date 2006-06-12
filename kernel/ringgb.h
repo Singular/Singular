ada0d58fbabec18891e5bc616cfae251438277ce
Hans Schönemann <hannes@mathematik.uni-kl.de>
2006-06-12 13:04:26 +0000

    *hannes: mask HAVE_RING2TOM routines
    git-svn-id: file:///usr/local/Singular/svn/trunk@9200 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/ringgb.h
+++ kernel/ringgb.h
@@ -1,13 +1,14 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: ringgb.h,v 1.4 2006-06-12 00:07:12 wienand Exp $ */
+/* $Id: ringgb.h,v 1.5 2006-06-12 13:04:26 Singular Exp $ */
 /*
 * ABSTRACT: ringgb interface
 */
 #ifndef RINGGB_HEADER
 #define RINGGB_HEADER
 #include "mod2.h"
+#ifdef HAVE_RING2TOM
 #include "polys.h"
 
 poly reduce_poly_fct(poly p, ring r);
@@ -16,4 +17,5 @@ poly ringNF(poly f, ideal G, ring r);
 poly plain_spoly(poly f, poly g);
 int testGB(ideal I, ideal GI);
 
+#endif
 #endif
