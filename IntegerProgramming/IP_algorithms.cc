543931e9b94b51dfcb076b90134259291fd4bde5
Hans Schönemann <hannes@mathematik.uni-kl.de>
2006-06-17 11:43:35 +0000

    *hannes: sparc-cc
    git-svn-id: file:///usr/local/Singular/svn/trunk@9229 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- IntegerProgramming/IP_algorithms.cc
+++ IntegerProgramming/IP_algorithms.cc
@@ -3296,7 +3296,7 @@ int solve(INPUT_FILE PROBLEM, INPUT_FILE GROEBNER)
         return 0;
       }
 
-      Integer initial_solution[weighted_variables];
+      Integer *initial_solution=new Integer[weighted_variables];
 
       for(short k=0;k<instances;k++)
       {
@@ -3312,7 +3312,7 @@ int solve(INPUT_FILE PROBLEM, INPUT_FILE GROEBNER)
               "input failure when reading right hand vector "<<k+1<<" from "
               "first input \nfile, this and all following right hand vectors "
               "will be ignored"<<endl;
-            delete[] right_hand;
+            delete[] initial_solution;
             return 0;
           }
 
@@ -3351,9 +3351,9 @@ int solve(INPUT_FILE PROBLEM, INPUT_FILE GROEBNER)
         output<<"computation time"<<endl;
         output<<setw(6)<<setprecision(2)<<elapsed<<" sec"<<endl<<endl;
       }
+      delete[] initial_solution;
     }
 
-  delete[] right_hand;
   return 1;
 
 }
