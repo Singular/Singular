cbc372f1d6bba1dc057875ed3c0faeac5d926e88
Viktor Levandovskyy <levandov@math.rwth-aachen.de>
2007-03-04 18:18:17 +0000

    *levandov: rational noncomm GB's updated
    git-svn-id: file:///usr/local/Singular/svn/trunk@9917 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/ratgring.cc
+++ kernel/ratgring.cc
@@ -6,11 +6,12 @@
  *  Purpose: Ore-noncommutative kernel procedures
  *  Author:  levandov (Viktor Levandovsky)
  *  Created: 8/00 - 11/00
- *  Version: $Id: ratgring.cc,v 1.1 2007-01-14 22:12:33 levandov Exp $
+ *  Version: $Id: ratgring.cc,v 1.2 2007-03-04 18:18:17 levandov Exp $
  *******************************************************************/
 #include "mod2.h"
 #ifdef HAVE_PLURAL
 #include "gring.h"
+#include "ratgring.h"
 #include "febase.h"
 #include "ring.h"
 #include "polys.h"
@@ -36,36 +37,36 @@ void pLcmRat(poly a, poly b, poly m, int rat_shift)
   /* Don't do a pSetm here, otherwise hres/lres chockes */ 
 }
 
-void pLcmRat(poly a, poly b, poly m, poly pshift)
-{
-  /* shift is the exp of rational elements */
-  int i;
-  for (i=pVariables; i; i--)
-  {
-    if (!pGetExp(pshift,i))
-    {
-      pSetExp(m,i, si_max( pGetExp(a,i), pGetExp(b,i)));
-    }
-    else
-    {
-      /* do we really need it? */
-      pSetExp(m,i,0);
-    }
-  }
-  pSetComp(m, si_max(pGetComp(a), pGetComp(b)));
-  /* Don't do a pSetm here, otherwise hres/lres chockes */ 
-}
+// void pLcmRat(poly a, poly b, poly m, poly pshift)
+// {
+//   /* shift is the exp of rational elements */
+//   int i;
+//   for (i=pVariables; i; i--)
+//   {
+//     if (!pGetExp(pshift,i))
+//     {
+//       pSetExp(m,i, si_max( pGetExp(a,i), pGetExp(b,i)));
+//     }
+//     else
+//     {
+//       /* do we really need it? */
+//       pSetExp(m,i,0);
+//     }
+//   }
+//   pSetComp(m, si_max(pGetComp(a), pGetComp(b)));
+//   /* Don't do a pSetm here, otherwise hres/lres chockes */ 
+// }
 
 /* returns a subpoly of p, s.t. its monomials have the same D-part */
 
 poly p_HeadRat(poly p, int ishift, ring r)
 {
-  poly q   = p_Next(p,r);
+  poly q   = pNext(p);
   poly res = p_Head(p,r);
   while ( p_Comp_k_n(p, q, ishift, r) )
   {
-    res = p_Add_q(res,p_Head(q,r));
-    q   = p_Next(p,r);
+    res = p_Add_q(res,p_Head(q,r),r);
+    q   = pNext(p);
   }
   return res;
 }
@@ -75,26 +76,72 @@ have the same D-part */
 
 poly p_GetCoeffRat(poly p, int ishift, ring r)
 {
-  poly q   = p_Next(p,r);
+  poly q   = pNext(p);
   poly res = p_Head(p,r);
   poly s;
   while ( p_Comp_k_n(p, q, ishift, r) )
   {
     s   = p_GetExp_k_n(q, ishift, r->N, r);
-    res = p_Add_q(res,s);
-    q   = p_Next(p,r);
+    res = p_Add_q(res,s,r);
+    q   = pNext(p);
   }
   return res;
 }
 
-poly p_LmDeleteAndNextRat(poly p, int ishift, ring r)
+void p_LmDeleteAndNextRat(poly p, int ishift, ring r)
 {
+  /* modifies p*/
   p_LmCheckPolyRing2(p, r);
-  while ( p_Comp_k_n(p, p_Next(p,r), ishift, r) )
+  while ( p_Comp_k_n(p, pNext(p), ishift, r) )
   {
     p_LmDelete(&p,r);
   }
 }
+
+/* returns ideal (u,v) s.t. up + vq = 0 */
+
+ideal ncGCD(poly p, poly q, const ring r)
+{
+  intvec *w = NULL;
+  ideal h = idInit(2,1);
+  h->m[0] = p_Copy(p,r);
+  h->m[1] = p_Copy(q,r);
+#ifdef PDEBUG
+  Print("running syzygy comp. for nc_GCD:");
+#endif
+  ideal sh = idSyzygies(h, testHomog, &w);
+#ifdef PDEBUG
+  Print("done syzygy comp. for nc_GCD");
+#endif
+  /* in comm case, there is only 1 syzygy */
+  /*   singclap_gcd(); */
+  poly K, K1, K2;
+  K  = sh->m[0]; /* take just the first element - to be enhanced later */
+  K1 = pTakeOutComp(&K, 1); // 1st component is taken out from K
+  pShift(&K,-2); // 2nd component to 0th comp.
+  K2 = K;
+
+  /* checking signs before multiplying */    
+  number ck1 = p_GetCoeff(K1,r);
+  number ck2 = p_GetCoeff(K2,r);
+  BOOLEAN bck1, bck2;
+  bck1 = n_GreaterZero(ck1,r);
+  bck2 = n_GreaterZero(ck2,r);
+  /* K1 <0, K2 <0 (-K1,-K2)    */
+  if ( !(bck1 && bck2) ) /* - , - */
+  {
+    K1 = p_Neg(K1,r);
+    K2 = p_Neg(K2,r);
+  }
+  idDelete(&h);
+  h = idInit(2,1);
+  h->m[0] = K1;
+  h->m[1] = K2;
+  idDelete(&sh);
+  return(h);
+}
+
+
 
 /* PINLINE1 void p_ExpVectorDiff 
    remains as is -> BUT we can do memory shift on smaller number of exp's */
@@ -104,143 +151,231 @@ poly p_LmDeleteAndNextRat(poly p, int ishift, ring r)
 * creates the S-polynomial of p1 and p2
 * do not destroy p1 and p2
 */
-poly nc_rat_CreateSpoly(poly p1, poly p2, poly spNoether, int ishift, const ring r)
+// poly nc_rat_CreateSpoly(poly p1, poly p2, poly spNoether, int ishift, const ring r)
+// {
+//   if ((p_GetComp(p1,r)!=p_GetComp(p2,r))
+//   && (p_GetComp(p1,r)!=0)
+//   && (p_GetComp(p2,r)!=0))
+//   {
+// #ifdef PDEBUG
+//     Print("nc_CreateSpoly : different components!");
+// #endif
+//     return(NULL);
+//   }
+//   /* prod. crit does not apply yet */
+// //   if ((r->nc->type==nc_lie) && pHasNotCF(p1,p2)) /* prod crit */
+// //   {
+// //     return(nc_p_Bracket_qq(pCopy(p2),p1));
+// //   }
+//   poly pL=pOne();
+//   poly m1=pOne();
+//   poly m2=pOne();
+//   /* define shift */
+//   int is = ishift; /* TODO */
+//   pLcmRat(p1,p2,pL,is);
+//   p_Setm(pL,r);
+//   poly pr1 = p_GetExp_k_n(p1,1,ishift-1,r); /* rat D-exp of p1 */
+//   poly pr2 = p_GetExp_k_n(p2,1,ishift-1,r); /* rat D-exp of p2 */
+// #ifdef PDEBUG
+//   p_Test(pL,r);
+// #endif
+//   p_ExpVectorDiff(m1,pL,p1,r); /* purely in D part by construction */
+//   //p_SetComp(m1,0,r);
+//   //p_Setm(m1,r);
+// #ifdef PDEBUG
+//   p_Test(m1,r);
+// #endif
+//   p_ExpVectorDiff(m2,pL,p2,r); /* purely in D part by construction */
+//   //p_SetComp(m2,0,r);
+//   //p_Setm(m2,r);
+// #ifdef PDEBUG
+//   p_Test(m2,r);
+// #endif
+//   p_Delete(&pL,r);
+//   /* zero exponents ! */
+
+//   /* EXTRACT LEADCOEF */
+
+//   poly H1  = p_HeadRat(p1,is,r);
+//   poly M1  = r->nc->p_Procs.mm_Mult_p(m1,p_Copy(H1,r),r);
+
+//   /* POLY:  number C1  = n_Copy(p_GetCoeff(M1,r),r); */
+//   /* RAT: */
+
+//   poly C1  = p_GetCoeffRat(M1,ishift,r);
+
+//   poly H2  = p_HeadRat(p2,is,r);
+//   poly M2  = r->nc->p_Procs.mm_Mult_p(m2,p_Copy(H2,r),r);
+
+//   /* POLY:  number C2  = n_Copy(p_GetCoeff(M2,r),r); */
+//   /* RAT: */
+
+//   poly C2  = p_GetCoeffRat(M2,ishift,r);
+
+// /* we do not assume that X's commute */
+// /* we just run NC syzygies */
+
+// /* NEW IDEA: change the ring to K<X>, map things there
+//    and return the result back; seems to be a good optimization */
+// /* to be done later */
+// /* problem: map to subalgebra. contexts, induced (non-unique) orderings etc. */
+
+//   intvec *w = NULL;
+//   ideal h = idInit(2,1);
+//   h->m[0] = p_Copy(C1,r);
+//   h->m[1] = p_Copy(C2,r);
+// #ifdef PDEBUG
+//   Print("running syzygy comp. for coeffs");
+// #endif
+//   ideal sh = idSyzygies(h, testHomog, &w);
+//   /* in comm case, there is only 1 syzygy */
+//   /*   singclap_gcd(); */
+//   poly K,K1,K2;
+//   K  = sh->m[0];
+//   K1 = pTakeOutComp(&K, 1); // 1st component is taken out from K
+//   pShift(&K,-2); // 2nd component to 0th comp.
+//   K2 = K;
+
+//   /* checking signs before multiplying */    
+//   number ck1 = p_GetCoeff(K1,r);
+//   number ck2 = p_GetCoeff(K2,r);
+//   BOOLEAN bck1, bck2;
+//   bck1 = n_GreaterZero(ck1,r);
+//   bck2 = n_GreaterZero(ck2,r);
+//   /* K1 >0, K2 >0 (K1,-K2)    */
+//   /* K1 >0, K2 <0 (K1,-K2)    */
+//   /* K1 <0, K2 >0 (-K1,K2)    */
+//   /* K1 <0, K2 <0 (-K1,K2)    */
+//   if ( (bck1) && (bck2) ) /* +, + */
+//   {
+//     K2 = p_Neg(K2,r);
+//   }
+//   if ( (bck1) && (!bck2) ) /* + , - */
+//   {
+//     K2 = p_Neg(K2,r);
+//   }
+//   if ( (!bck1) && (bck2) ) /* - , + */
+//   {
+//     K1 = p_Neg(K1,r);
+//   }
+//   if ( !(bck1 && bck2) ) /* - , - */
+//   {
+//     K1 = p_Neg(K1,r);
+//   }
+
+//   poly P1,P2;
+
+//   //  p_LmDeleteRat(M1,ishift,r); // get tail(D^(gamma-alpha) * lm(p1)) = h_f
+//   P1 = p_Copy(p1,r);
+//   p_LmDeleteAndNextRat(P1,ishift,r); // get tail(p1) = t_f
+//   P1 = r->nc->p_Procs.mm_Mult_p(m1,P1,r);
+//   P1 = p_Add_q(P1,M1,r);
+
+//   //  p_LmDeleteRat(M2,ishift,r);
+//   P2 = p_Copy(p2,r);
+//   p_LmDeleteAndNextRat(P2,ishift,r);// get tail(p2)=t_g
+//   P2 = r->nc->p_Procs.mm_Mult_p(m2,P2,r);
+//   P2 = p_Add_q(P2,M2,r);
+
+//   /* coeff business */
+
+//   P1 = p_Mult_q(P1,K1,r);
+//   P2 = p_Mult_q(P2,K2,r);
+//   P1 = p_Add_q(P1,P2,r);
+
+//   /* cleaning up */
+
+// #ifdef PDEBUG
+//   p_Test(p1,r);
+// #endif
+//   /* questionable: */
+//   if (P1!=NULL) pCleardenom(P1);
+//   if (P1!=NULL) pContent(P1);
+//   return(P1);
+// }
+
+/*2
+* reduction of p2 with p1
+* do not destroy p1, but p2
+* p1 divides p2 -> for use in NF algorithm
+* works in an integer fashion
+*/
+
+poly nc_rat_ReduceSpolyNew(const poly p1, poly p2, int ishift, const ring r)
 {
-  if ((p_GetComp(p1,r)!=p_GetComp(p2,r))
-  && (p_GetComp(p1,r)!=0)
-  && (p_GetComp(p2,r)!=0))
+  const long lCompP1 = p_GetComp(p1,r);
+  const long lCompP2 = p_GetComp(p2,r);
+
+  if ((lCompP1!=lCompP2) && (lCompP1!=0) && (lCompP2!=0))
   {
 #ifdef PDEBUG
-    Print("nc_CreateSpoly : different components!");
+    Werror("nc_rat_ReduceSpolyNew: different non-zero components!");
 #endif
     return(NULL);
   }
-  /* prod. crit does not apply yet */
-//   if ((r->nc->type==nc_lie) && pHasNotCF(p1,p2)) /* prod crit */
-//   {
-//     return(nc_p_Bracket_qq(pCopy(p2),p1));
-//   }
-  poly pL=pOne();
-  poly m1=pOne();
-  poly m2=pOne();
-  /* define shift */
+
   int is = ishift; /* TODO */
-  pLcmRat(p1,p2,pL,is);
-  p_Setm(pL,r);
-  poly pr1 = p_GetExp_k_n(p1,1,ishift-1); /* rat D-exp of p1 */
-  poly pr2 = p_GetExp_k_n(p2,1,ishift-1); /* rat D-exp of p2 */
+
+  poly m  = pOne();
+  p_ExpVectorDiff(m, p2, p1, r); // includes X and D parts
+  //p_Setm(m,r);
+  m = p_GetExp_k_n(m,1,ishift-1,r); /* rat D-exp of m */
 #ifdef PDEBUG
-  p_Test(pL,r);
+  p_Test(m,r);
 #endif
-  p_ExpVectorDiff(m1,pL,p1,r); /* purely in D part by construction */
-  //p_SetComp(m1,0,r);
-  //p_Setm(m1,r);
-#ifdef PDEBUG
-  p_Test(m1,r);
-#endif
-  p_ExpVectorDiff(m2,pL,p2,r); /* purely in D part by construction */
-  //p_SetComp(m2,0,r);
-  //p_Setm(m2,r);
-#ifdef PDEBUG
-  p_Test(m2,r);
-#endif
-  p_Delete(&pL,r);
-  /* zero exponents ! */
 
-  /* EXTRACT LEADCOEF */
+  /* pSetComp(m,r)=0? */
+  poly HH, H;
+  HH = p_HeadRat(p1,is,r); // lm_D(g)
+  H  = r->nc->p_Procs.mm_Mult_p(m, p_Copy(HH, r), r); // d^aplha lm_D(g)
 
-  poly H1  = p_HeadRat(p1,r);
-  poly M1  = nc_mm_Mult_p(m1,p_Copy(H1,r),r);
+  poly K  = p_Copy( p_GetCoeffRat(H,  is, r), r);
+  poly P  = p_Copy( p_GetCoeffRat(p2, is, r), r);
 
-  /* POLY:  number C1  = n_Copy(p_GetCoeff(M1,r),r); */
-  /* RAT: */
+  HH = p_Neg(HH, r);
+  poly out = p_Add_q(p_Copy(p1,r), HH, r); // out == t_g
 
-  poly C1  = p_GetCoeffRat(M1,ishift,r);
+  // alt:
+  // out = p_Copy(p1,r);
+  // p_LmDeleteAndNextRat(out, is, r);
 
-  poly H2  = p_HeadRat(p2,r);
-  poly M2  = nc_mm_Mult_p(m2,p_Copy(H2,r),r);
+  ideal ncsyz = ncGCD(P,K,r);
+  poly KK = ncsyz->m[0]; // k'
+  poly PP = ncsyz->m[1]; // p'
+  
+  HH = p_HeadRat(p2,is,r);
+  HH = p_Neg(HH, r);
+  p2 = p_Add_q(p2, HH, r); // t_f
 
-  /* POLY:  number C2  = n_Copy(p_GetCoeff(M2,r),r); */
-  /* RAT: */
+  // alt:
+  // p_LmDeleteAndNextRat(p2, is, r);
 
-  poly C2  = p_GetCoeffRat(M2,ishift,r);
 
-/* we do not assume that X's commute */
-/* we just run NC syzygies */
+  HH = p_HeadRat(H,is,r);
+  HH = p_Neg(HH, r);
+  H  = p_Add_q(H, HH, r); // r_g
 
-/* NEW IDEA: change the ring to K<X>, map things there
-   and return the result back; seems to be a good optimization */
-/* to be done later */
+  // alt:
+  // p_LmDeleteAndNextRat(H, is, r);
 
-  intvec *w = NULL;
-  ideal h = idInit(2,1);
-  h->m[0] = p_Copy(C1,r);
-  h->m[1] = p_Copy(C2,r);
-#ifdef PDEBUG
-  Print("running syzygy comp. for coeffs");
-#endif
-  ideal sh = idSyzygies(h, testHomog, &w);
-  /* in comm case, there is only 1 syzygy */
-  /*   singclap_gcd(); */
-  poly K,K1,K2;
-  K  = sh->m[0];
-  K1 = pTakeOutComp(&K, 1); // 1st component is taken out from K
-  K2 = pShift(&K,-2); // 2nd component to 0th comp.
 
-  /* checking signs before multiplying */    
-  number ck1 = p_GetCoeff(K1,r);
-  number ck2 = p_GetCoeff(K2,r);
-  BOOLEAN bck1, bck2;
-  bck1 = n_GreaterZero(ck1);
-  bck2 = n_GreaterZero(ck2);
-  /* K1 >0, K2 >0 (K1,-K2)    */
-  /* K1 >0, K2 <0 (K1,-K2)    */
-  /* K1 <0, K2 >0 (-K1,K2)    */
-  /* K1 <0, K2 <0 (-K1,K2)    */
-  if ( (bck1) && (bck2) ) /* +, + */
-  {
-    K2 = p_Neg(K2,r);
-  }
-  if ( (bck1) && (!bck2) ) /* + , - */
-  {
-    K2 = p_Neg(K2,r);
-  }
-  if ( (!bck1) && (bck2) ) /* - , + */
-  {
-    K1 = p_Neg(K1,r);
-  }
-  if ( !(bck1 && bck2) ) /* - , - */
-  {
-    K1 = p_Neg(K1,r);
-  }
+  p2 = p_Mult_mm(p2, KK, r); // p2 = k' t_f
+  p_Test(p2,r);
+  p_Delete(&KK,r);
 
-  poly P1,P2;
+  out = r->nc->p_Procs.mm_Mult_p(m, out, r); // d^aplha t_g
+  p_Delete(&m,r);
 
-  p_LmDeleteRat(M1,ishift,r); // get tail(D^(gamma-alpha) * lm(p1)) = h_f
-  P1 = p_LmDeleteAndNextRat(p_Copy(p1,r),ishift,r); // get tail(p1) = t_f
-  P1 = nc_mm_Mult_p(m1,P1,r);
-  P1 = p_Add_q(P1,M1,r);
+  out = p_Add_q(H, out, r);
+  p_Test(out,r);
+  out  = p_Mult_q(PP, out, r);
 
-  p_LmDeleteRat(M2,ishift,r);
-  P2 = p_LmDeleteRat(p_Copy(p2,r),ishift,r);// get tail(p2)=t_g
-  P2 = nc_mm_Mult_p(m2,P2,r);
-  P2 = p_Add_q(P2,M2,r);
-
-  /* coeff business */
-
-  P1 = p_Mult_nn(P1,K1,r);
-  P2 = p_Mult_nn(P2,K2,r);
-  P1 = p_Add_q(P1,P2,r);
-
-  /* cleaning up */
-
-#ifdef PDEBUG
-  p_Test(p1,r);
-#endif
-  /* questionable: */
-  if (P1!=NULL) pCleardenom(P1);
-  if (P1!=NULL) pContent(P1);
-  return(P1);
+  out = p_Add_q(p2,out,r); // delete out, p2
+  p_Test(out,r);
+  if ( out!=NULL ) pContent(out);
+  return(out);
 }
+
+
 
 #endif
--- kernel/ratgring.h
+++ kernel/ratgring.h
@@ -1,9 +1,9 @@
-#ifndef GRING_H
-#define GRING_H
+#ifndef RATGRING_H
+#define RATGRING_H
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: ratgring.h,v 1.1 2007-01-14 22:12:33 levandov Exp $ */
+/* $Id: ratgring.h,v 1.2 2007-03-04 18:18:17 levandov Exp $ */
 /*
 * ABSTRACT additional defines etc for --with-plural
 */
@@ -15,67 +15,84 @@
 
 /* the part, related to the interface */ 
 
-ring nc_rCreateNCcomm(ring r);
+/* ring nc_rCreateNCcomm(ring r); */
+
+void pLcmRat(poly a, poly b, poly m, int rat_shift);
+
+//void pLcmRat(poly a, poly b, poly m, poly pshift);
+
+poly p_GetCoeffRat(poly p, int ishift, ring r);
+
+poly p_HeadRat(poly p, int ishift, ring r);
+
+void p_LmDeleteAndNextRat(poly p, int ishift, ring r);
+
+ideal ncGCD(poly p, poly q, ring r);
+
+//poly nc_rat_CreateSpoly(poly p1, poly p2, poly spNoether, int ishift, ring r);
+
+poly nc_rat_ReduceSpolyNew(poly p1, poly p2, int ishift, ring r);
+
 
 /* poly functions defined in p_Procs : */
-poly nc_pp_Mult_mm(poly p, poly m, const ring r, poly &last);
-poly nc_p_Mult_mm(poly p, const poly m, const ring r);
-poly nc_p_Minus_mm_Mult_qq(poly p, const poly m, poly q, const ring r);
-poly nc_p_Minus_mm_Mult_qq_ign(poly p, const poly m, poly q, int & d1, poly d2, const ring ri, poly &d3);
+// poly nc_pp_Mult_mm(poly p, poly m, const ring r, poly &last);
+// poly nc_p_Mult_mm(poly p, const poly m, const ring r);
+// poly nc_p_Minus_mm_Mult_qq(poly p, const poly m, poly q, const ring r);
+// poly nc_p_Minus_mm_Mult_qq_ign(poly p, const poly m, poly q, int & d1, poly d2, const ring ri, poly &d3);
 
 /* other routines we need in addition : */
-poly nc_p_Mult_mm_Common(poly p, const poly m, int side, const ring r);
-poly nc_mm_Mult_p(const poly m, poly p, const ring r);
-poly nc_mm_Mult_nn (int *F, int *G, const ring r); 
-poly nc_mm_Mult_uu (int *F,int jG,int bG, const ring r);
+// poly nc_p_Mult_mm_Common(poly p, const poly m, int side, const ring r);
+// poly nc_mm_Mult_p(const poly m, poly p, const ring r);
+// poly nc_mm_Mult_nn (int *F, int *G, const ring r); 
+// poly nc_mm_Mult_uu (int *F,int jG,int bG, const ring r);
 
-/* subst: */
-poly nc_pSubst(poly p, int n, poly e);
+// /* subst: */
+// poly nc_pSubst(poly p, int n, poly e);
 
-/* copy : */
-poly nc_p_CopyGet(poly a, const ring r);
-poly nc_p_CopyPut(poly a, const ring r);
+// /* copy : */
+// poly nc_p_CopyGet(poly a, const ring r);
+// poly nc_p_CopyPut(poly a, const ring r);
 
-/* syzygies : */
-/* former nc_spGSpolyCreate */
-poly nc_CreateSpoly(poly p1, poly p2, poly spNoether, const ring r);
-/* former nc_spGSpolyRed */
-poly nc_ReduceSpoly(poly p1, poly p2, poly spNoether, const ring r);
-/* former nc_spGSpolyRedNew */
-poly nc_ReduceSpolyNew(poly p1, poly p2, poly spNoether, const ring r);
-/* former nc_spGSpolyRedTail */
-void nc_ReduceSpolyTail(poly p1, poly q, poly q2, poly spNoether, const ring r);
-/* former nc_spShort */
-poly nc_CreateShortSpoly(poly p1, poly p2, const ring r=currRing);
+// /* syzygies : */
+// /* former nc_spGSpolyCreate */
+// poly nc_CreateSpoly(poly p1, poly p2, poly spNoether, const ring r);
+// /* former nc_spGSpolyRed */
+// poly nc_ReduceSpoly(poly p1, poly p2, poly spNoether, const ring r);
+// /* former nc_spGSpolyRedNew */
+// poly nc_ReduceSpolyNew(poly p1, poly p2, poly spNoether, const ring r);
+// /* former nc_spGSpolyRedTail */
+// void nc_ReduceSpolyTail(poly p1, poly q, poly q2, poly spNoether, const ring r);
+// /* former nc_spShort */
+// poly nc_CreateShortSpoly(poly p1, poly p2, const ring r=currRing);
 
-ideal gr_bba (ideal F, ideal Q,kStrategy strat);
+// ideal gr_bba (ideal F, ideal Q,kStrategy strat);
 
-/* brackets: */
-poly nc_p_Bracket_qq(poly p, poly q);
-poly nc_mm_Bracket_nn(poly m1, poly m2);
+// /* brackets: */
+// poly nc_p_Bracket_qq(poly p, poly q);
+// poly nc_mm_Bracket_nn(poly m1, poly m2);
 
-/* twostd: */
-ideal twostd(ideal I);
-/* Ann: */
-ideal Approx_Step(ideal L);
+// /* twostd: */
+// ideal twostd(ideal I);
+// /* Ann: */
+// ideal Approx_Step(ideal L);
 
-/* complete reduction routines */
+// /* complete reduction routines */
 
-/* void nc_kBucketPolyRed(kBucket_pt b, poly p); */
-void nc_kBucketPolyRed(kBucket_pt b, poly p, number *c);
-void nc_kBucketPolyRed_Z(kBucket_pt b, poly p, number *c);
-void nc_PolyPolyRed(poly &b, poly p, number *c);
+// /* void nc_kBucketPolyRed(kBucket_pt b, poly p); */
+// void nc_kBucketPolyRed(kBucket_pt b, poly p, number *c);
+// void nc_kBucketPolyRed_Z(kBucket_pt b, poly p, number *c);
+// void nc_PolyPolyRed(poly &b, poly p, number *c);
 
-matrix nc_PrintMat(int a, int b, ring r, int metric);
+// matrix nc_PrintMat(int a, int b, ring r, int metric);
 
-poly p_CopyEmbed(poly p, ring srcRing, int shift, int par_shift);
-poly pOppose(ring Rop, poly p);
-ideal idOppose(ring Rop, ideal I);
+// poly p_CopyEmbed(poly p, ring srcRing, int shift, int par_shift);
+// poly pOppose(ring Rop, poly p);
+// ideal idOppose(ring Rop, ideal I);
 
-#else
-/* dummy definition to make gcc happy */
-#define nc_kBucketPolyRed(A,B,C) 0
-#define nc_PolyPolyRed(A,B,C) 0
+// #else
+// /* dummy definition to make gcc happy */
+// #define nc_kBucketPolyRed(A,B,C) 0
+// #define nc_PolyPolyRed(A,B,C) 0
 
 #endif /* HAVE_PLURAL */
 #endif
