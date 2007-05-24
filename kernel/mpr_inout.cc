313600e78ee859f18896ff6c00d4ef505c29dcc0
Hans Schönemann <hannes@mathematik.uni-kl.de>
2007-05-24 16:53:05 +0000

    *hannes: fixed typo
    git-svn-id: file:///usr/local/Singular/svn/trunk@10093 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/mpr_inout.cc
+++ kernel/mpr_inout.cc
@@ -2,7 +2,7 @@
 *  Computer Algebra System SINGULAR     *
 ****************************************/
 
-/* $Id: mpr_inout.cc,v 1.4 2007-05-24 16:50:42 Singular Exp $ */
+/* $Id: mpr_inout.cc,v 1.5 2007-05-24 16:53:05 Singular Exp $ */
 
 /*
 * ABSTRACT - multipolynomial resultant
@@ -70,15 +70,15 @@ void mprPrintError( mprState state, const char * name )
     Werror("One element of the ideal %s is constant!",name);
     break;
   case mprInfNumOfVars:
-    Werror("Wrong number of elements in given ideal %s,should be %d resp. %d!",
+    Werror("Wrong number of elements in given ideal %s, should be %d resp. %d!",
            name,pVariables+1,pVariables);
     break;
   case mprNotZeroDim:
-    Werror("The given ideal %s must 0-dimensional!",name);
+    Werror("The given ideal %s must be 0-dimensional!",name);
     break;
   case mprNotHomog:
-    Werror("The given ideal %s has to be homogeneous in"
-           " the first ring variable!",name);
+    Werror("The given ideal %s has to be homogeneous in the first ring variable!",
+           name);
     break;
   case mprNotReduced:
     Werror("The given ideal %s has to reduced!",name);
