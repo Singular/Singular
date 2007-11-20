35667340eab502db9b8e8d2f4ce53b19967a4397
Hans Schönemann <hannes@mathematik.uni-kl.de>
2007-11-20 13:42:50 +0000

    *hannes: ld
    git-svn-id: file:///usr/local/Singular/svn/trunk@10431 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/ffreval.cc
+++ factory/ffreval.cc
@@ -14,6 +14,53 @@
 #include "ftmpl_functions.h"
 #include "ffreval.h"
 
+void FFREvaluation::init()
+{
+  int n = values.max();
+
+  for( int i=values.min(); i<=n; i++ )
+    values[i] = offset[i] = gen->generate(); // generate random point
+}
+
+bool FFREvaluation::step()
+{
+  // enumerates the points stored in values
+  int n = values.max();
+  int p = getCharacteristic();
+
+  int i;
+  for(i=values.min(); i<=n; i++ )
+  {
+    if( values[i] != p-1 )
+    {
+      values[i] += 1;
+      break;
+    }
+    values[i] += 1; // becomes 0
+  }
+  for(i=values.min();i<=n;i++)
+  {
+    if(values[i]!=offset[i]) return true;
+  }
+  return false;
+}
+
+FFREvaluation& FFREvaluation::operator= ( const FFREvaluation & e )
+{
+  if( this != &e )
+  {
+    if( gen != 0 )
+      delete gen;
+    values = e.values;
+    offset = e.offset;
+    if( e.gen == 0 )
+      gen = 0;
+    else
+      gen = e.gen->clone();
+  }
+  return *this;
+}    
+
 /* forward declarations:*/
 static CanonicalForm fin_ezgcd ( const CanonicalForm & FF, const CanonicalForm & GG, FFREvaluation & b, bool internal );
 static bool fin_findeval( const CanonicalForm & F, const CanonicalForm & G, CanonicalForm & Fb, CanonicalForm & Gb, CanonicalForm & Db, FFREvaluation & b, int delta, int degF, int degG );
--- factory/ffreval.h
+++ factory/ffreval.h
@@ -20,53 +20,6 @@ class FFREvaluation : public REvaluation
     FFREvaluation& operator= ( const FFREvaluation & e );
 };
 
-void FFREvaluation::init()
-{
-  int n = values.max();
-
-  for( int i=values.min(); i<=n; i++ )
-    values[i] = offset[i] = gen->generate(); // generate random point
-}
-
-bool FFREvaluation::step()
-{
-  // enumerates the points stored in values
-  int n = values.max();
-  int p = getCharacteristic();
-
-  int i;
-  for(i=values.min(); i<=n; i++ )
-  {
-    if( values[i] != p-1 )
-    {
-      values[i] += 1;
-      break;
-    }
-    values[i] += 1; // becomes 0
-  }
-  for(i=values.min();i<=n;i++)
-  {
-    if(values[i]!=offset[i]) return true;
-  }
-  return false;
-}
-
-FFREvaluation& FFREvaluation::operator= ( const FFREvaluation & e )
-{
-  if( this != &e )
-  {
-    if( gen != 0 )
-      delete gen;
-    values = e.values;
-    offset = e.offset;
-    if( e.gen == 0 )
-      gen = 0;
-    else
-      gen = e.gen->clone();
-  }
-  return *this;
-}    
-
 CanonicalForm fin_ezgcd ( const CanonicalForm & FF, const CanonicalForm & GG );
 
 #endif
