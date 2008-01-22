c6d3744af7f072c74673ab0aef31dfe80c1c9ae9
Hans Schönemann <hannes@mathematik.uni-kl.de>
2008-01-22 09:29:56 +0000

    *hannes: work in progress
    git-svn-id: file:///usr/local/Singular/svn/trunk@10517 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/fac_multivar.cc
+++ factory/fac_multivar.cc
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: fac_multivar.cc,v 1.12 2005-08-22 17:24:02 Singular Exp $ */
+/* $Id: fac_multivar.cc,v 1.13 2008-01-22 09:29:56 Singular Exp $ */
 
 #include <config.h>
 
@@ -20,6 +20,7 @@
 #include "fac_distrib.h"
 
 void out_cf(char *s1,const CanonicalForm &f,char *s2);
+void out_cff(CFFList &L);
 
 TIMING_DEFINE_PRINT(fac_content);
 TIMING_DEFINE_PRINT(fac_findeval);
@@ -117,19 +118,23 @@ findEvaluation ( const CanonicalForm & U, const CanonicalForm & V, const Canonic
     CFArray FF = CFArray( 1, F.length() );
     if ( r > 0 )
         A.nextpoint();
-    while ( ! found ) {
+    while ( ! found )
+    {
         Vn = A( V );
-        if ( Vn != 0 ) {
+        if ( Vn != 0 )
+        {
             U0 = A( U );
             DEBOUTLN( cerr, "U0 = " << U0 );
-            if ( isSqrFree( U0 ) ) {
+            if ( isSqrFree( U0 ) )
+            {
                 delta = content( U0 );
                 DEBOUTLN( cerr, "content( U0 ) = " << delta );
                 for ( I = F, j = 1; I.hasItem(); I++, j++ )
                     FF[j] = A( I.getItem().factor() );
                 found = nonDivisors( omega, delta, FF, D );
             }
-            else {
+            else
+            {
                 DEBOUTLN( cerr, "not sqrfree : " << sqrFree( U0 ) );
             }
         }
@@ -190,8 +195,7 @@ void find_good_prime(const CanonicalForm &f, int &start)
 }
 #endif
 
-static CFArray
-ZFactorizeMulti ( const CanonicalForm & arg )
+static CFArray ZFactorizeMulti ( const CanonicalForm & arg )
 {
     DEBINCLEVEL( cerr, "ZFactorizeMulti" );
     CFMap M;
@@ -216,15 +220,18 @@ ZFactorizeMulti ( const CanonicalForm & arg )
     DEBOUTLN( cerr, "which is factorized as " << F );
 
     maxdeg = 0;
-    for ( i = 2; i <= t; i++ ) {
+    for ( i = 2; i <= t; i++ )
+    {
         j = U.degree( Variable( i ) );
         if ( j > maxdeg ) maxdeg = j;
     }
 
-    if ( F.getFirst().factor().inCoeffDomain() ) {
+    if ( F.getFirst().factor().inCoeffDomain() )
+    {
         omega = F.getFirst().factor();
         F.removeFirst();
-        if ( omega < 0 ) {
+        if ( omega < 0 )
+        {
             negate = true;
             omega = -omega;
             U = -U;
@@ -239,7 +246,8 @@ ZFactorizeMulti ( const CanonicalForm & arg )
 //    for ( i = 0; i < 10*t; i++ )
 //        A.nextpoint();
 
-    while ( ! goodeval ) {
+    while ( ! goodeval )
+    {
         TIMING_START(fac_findeval);
         findEvaluation( U, V, omega, F, A, U0, delta, D, r );
         TIMING_END(fac_findeval);
@@ -253,27 +261,27 @@ ZFactorizeMulti ( const CanonicalForm & arg )
         #ifdef HAVE_NTL
         {
           int i=prime_number;
-	  find_good_prime(arg,i);
-	  find_good_prime(U0,i);
-	  find_good_prime(U,i);
-	  int p=cf_getSmallPrime(i);
-	  //printf("found:p=%d (%d)\n",p,i);
-	  if ((i==0)||(i!=prime_number))
-	  {  
+          find_good_prime(arg,i);
+          find_good_prime(U0,i);
+          find_good_prime(U,i);
+          int p=cf_getSmallPrime(i);
+          //printf("found:p=%d (%d)\n",p,i);
+          if ((i==0)||(i!=prime_number))
+          {
             b = coeffBound( U, p );
-	    prime_number=i;
-	  }  
-	  modpk bb=coeffBound(U0,p);
-	  if (bb.getk() > b.getk() ) b=bb;
-	  bb=coeffBound(arg,p);
-	  if (bb.getk() > b.getk() ) b=bb;
+            prime_number=i;
+          }
+          modpk bb=coeffBound(U0,p);
+          if (bb.getk() > b.getk() ) b=bb;
+          bb=coeffBound(arg,p);
+          if (bb.getk() > b.getk() ) b=bb;
         }
         #else
         b = coeffBound( U, getZFacModulus().getp() );
         if ( getZFacModulus().getpk() > b.getpk() )
             b = getZFacModulus();
         #endif
-	//printf("p=%d, k=%d\n",b.getp(),b.getk());
+        //printf("p=%d, k=%d\n",b.getp(),b.getk());
         DEBOUTLN( cerr, "the coefficient bound of the factors of U is " << b.getpk() );
 
         r = G.size();
@@ -284,13 +292,15 @@ ZFactorizeMulti ( const CanonicalForm & arg )
         goodeval = distributeLeadingCoeffs( UU, G, lcG, F, D, delta, omega, A, r );
         TIMING_END(fac_distrib);
 #ifdef DEBUGOUTPUT
-        if ( goodeval ) {
+        if ( goodeval )
+        {
             DEBOUTLN( cerr, "the univariate factors after distribution are " << G );
             DEBOUTLN( cerr, "the distributed leading coeffs are " << lcG );
             DEBOUTLN( cerr, "U may have changed and is now " << UU );
             DEBOUTLN( cerr, "which has leading coefficient " << LC( UU, Variable(1) ) );
 
-            if ( LC( UU, Variable(1) ) != prod( lcG ) || A(UU) != prod( G ) ) {
+            if ( LC( UU, Variable(1) ) != prod( lcG ) || A(UU) != prod( G ) )
+            {
                 DEBOUTLN( cerr, "!!! distribution was not correct !!!" );
                 DEBOUTLN( cerr, "product of leading coeffs is " << prod( lcG ) );
                 DEBOUTLN( cerr, "product of univariate factors is " << prod( G ) );
@@ -299,17 +309,20 @@ ZFactorizeMulti ( const CanonicalForm & arg )
             else
                 DEBOUTLN( cerr, "leading coeffs correct" );
         }
-        else {
+        else
+        {
             DEBOUTLN( cerr, "we have found a bad evaluation point" );
         }
 #endif
-        if ( goodeval ) {
+        if ( goodeval )
+        {
             TIMING_START(fac_hensel);
             goodeval = Hensel( UU, G, lcG, A, b, Variable(1) );
             TIMING_END(fac_hensel);
         }
     }
-    for ( i = 1; i <= r; i++ ) {
+    for ( i = 1; i <= r; i++ )
+    {
         G[i] /= icontent( G[i] );
         G[i] = M(G[i]);
     }
@@ -320,8 +333,7 @@ ZFactorizeMulti ( const CanonicalForm & arg )
     return G;
 }
 
-CFFList
-ZFactorizeMultivariate ( const CanonicalForm & f, bool issqrfree )
+CFFList ZFactorizeMultivariate ( const CanonicalForm & f, bool issqrfree )
 {
     CFFList G, F, R;
     CFArray GG;
@@ -335,14 +347,17 @@ ZFactorizeMultivariate ( const CanonicalForm & f, bool issqrfree )
     if ( issqrfree )
         F = CFFactor( f, 1 );
     else
-        F = sqrFree( f );
+        F = sqrFree( f, 0, false );
 
-    for ( i = F; i.hasItem(); i++ ) {
-        if ( i.getItem().factor().inCoeffDomain() ) {
+    for ( i = F; i.hasItem(); i++ )
+    {
+        if ( i.getItem().factor().inCoeffDomain() )
+        {
             if ( ! i.getItem().factor().isOne() )
                 R.append( CFFactor( i.getItem().factor(), i.getItem().exp() ) );
         }
-        else {
+        else
+        {
             TIMING_START(fac_content);
             g = compress( i.getItem().factor(), M );
             // now after compress g contains Variable(1)
@@ -354,13 +369,15 @@ ZFactorizeMultivariate ( const CanonicalForm & f, bool issqrfree )
             n = i.getItem().exp();
             TIMING_END(fac_content);
             DEBOUTLN( cerr, "now after content ..." );
-            if ( g.isUnivariate() ) {
+            if ( g.isUnivariate() )
+            {
                 G = factorize( g, true );
                 for ( j = G; j.hasItem(); j++ )
                     if ( ! j.getItem().factor().isOne() )
                         R.append( CFFactor( M( j.getItem().factor() ), n ) );
             }
-            else {
+            else
+            {
                 GG = ZFactorizeMulti( g );
                 m = GG.max();
                 for ( k = GG.min(); k <= m; k++ )
@@ -368,6 +385,161 @@ ZFactorizeMultivariate ( const CanonicalForm & f, bool issqrfree )
                         R.append( CFFactor( M( GG[k] ), n ) );
             }
             G = factorize( cont, true );
+            for ( j = G; j.hasItem(); j++ )
+                if ( ! j.getItem().factor().isOne() )
+                    R.append( CFFactor( M( j.getItem().factor() ), n ) );
+        }
+    }
+    return R;
+}
+
+static CFArray FpFactorizeMulti ( const CanonicalForm & arg )
+{
+    out_cf("FpFactorizeMulti:",arg,"\n");
+    DEBINCLEVEL( cerr, "FpFactorizeMulti" );
+    CFMap M;
+    CanonicalForm UU, U = compress( arg, M );
+    CanonicalForm delta, omega, V = LC( U, 1 );
+    int t = U.level();
+    CFFList F = factorize( V );
+    CFFListIterator I, J;
+    CFArray G, lcG, D;
+    int i, j, k, m, r, maxdeg, h;
+    REvaluation A( 2, t, FFRandom() );
+    CanonicalForm U0;
+    CanonicalForm ft, ut, gt, d;
+    modpk b;
+    bool negate = false;
+
+    DEBOUTLN( cerr, "-----------------------------------------------------" );
+    DEBOUTLN( cerr, "trying to factorize U = " << U );
+    DEBOUTLN( cerr, "U is a polynomial of level = " << arg.level() );
+    DEBOUTLN( cerr, "U will be factorized with respect to variable " << Variable(1) );
+    DEBOUTLN( cerr, "the leading coefficient of U with respect to that variable is " << V );
+    DEBOUTLN( cerr, "which is factorized as " << F );
+
+    maxdeg = 0;
+    out_cf("try:",U,"\n");
+    for ( i = 2; i <= t; i++ )
+    {
+        j = U.degree( Variable( i ) );
+        if ( j > maxdeg ) maxdeg = j;
+    }
+
+    if ( F.getFirst().factor().inCoeffDomain() )
+    {
+        omega = F.getFirst().factor();
+        F.removeFirst();
+        if ( omega < 0 )
+        {
+            negate = true;
+            omega = -omega;
+            U = -U;
+        }
+    }
+    else
+        omega = 1;
+
+    bool goodeval = false;
+    r = 0;
+
+//    for ( i = 0; i < 10*t; i++ )
+//        A.nextpoint();
+
+    while ( ! goodeval )
+    {
+        TIMING_START(fac_findeval);
+        findEvaluation( U, V, omega, F, A, U0, delta, D, r );
+        out_cf("univ.U0:",U0,"\n");
+        TIMING_END(fac_findeval);
+        DEBOUTLN( cerr, "the evaluation point to reduce to an univariate problem is " << A );
+        DEBOUTLN( cerr, "corresponding delta = " << delta );
+        DEBOUTLN( cerr, "              omega = " << omega );
+        DEBOUTLN( cerr, "              D     = " << D );
+        DEBOUTLN( cerr, "now factorize the univariate polynomial " << U0 );
+        G = conv_to_factor_array( factorize( U0, false ) );
+        printf("conv_to_factor_array\n");
+        DEBOUTLN( cerr, "which factorizes into " << G );
+
+        r = G.size();
+        lcG = CFArray( 1, r );
+        UU = U;
+        //if ( goodeval )
+        {
+            printf("start hensel\n");
+            TIMING_START(fac_hensel);
+            goodeval = Hensel( UU, G, lcG, A, b, Variable(1) );
+            TIMING_END(fac_hensel);
+        }
+    }
+    for ( i = 1; i <= r; i++ )
+    {
+        G[i] /= icontent( G[i] );
+        G[i] = M(G[i]);
+    }
+    // negate noch beachten !
+    if ( negate )
+        G[1] = -G[1];
+    DEBDECLEVEL( cerr, "ZFactorMulti" );
+    return G;
+}
+CFFList FpFactorizeMultivariate ( const CanonicalForm & f, bool issqrfree )
+{
+    CFFList G, F, R;
+    CFArray GG;
+    CFFListIterator i, j;
+    CFMap M;
+    CanonicalForm g, cont;
+    Variable v1, vm;
+    int k, m, n;
+
+    v1 = Variable(1);
+    if ( issqrfree )
+        F = CFFactor( f, 1 );
+    else
+        F = sqrFree( f, 0, false );
+
+    printf("nach sqrFree:\n");
+    out_cff(F);
+    for ( i = F; i.hasItem(); i++ )
+    {
+        out_cf("consider:",i.getItem().factor(),"\n");
+        if ( i.getItem().factor().inCoeffDomain() )
+        {
+            if ( ! i.getItem().factor().isOne() )
+                R.append( CFFactor( i.getItem().factor(), i.getItem().exp() ) );
+        }
+        else
+        {
+            TIMING_START(fac_content);
+            g = compress( i.getItem().factor(), M );
+            // now after compress g contains Variable(1)
+            vm = g.mvar();
+            g = swapvar( g, v1, vm );
+            cont = content( g );
+            g = swapvar( g / cont, v1, vm );
+            cont = swapvar( cont, v1, vm );
+            n = i.getItem().exp();
+            TIMING_END(fac_content);
+            DEBOUTLN( cerr, "now after content ..." );
+            if ( g.isUnivariate() )
+            {
+                G = factorize( g, true );
+                for ( j = G; j.hasItem(); j++ )
+                    if ( ! j.getItem().factor().isOne() )
+                        R.append( CFFactor( M( j.getItem().factor() ), n ) );
+            }
+            else
+            {
+                GG = FpFactorizeMulti( g );
+                m = GG.max();
+                for ( k = GG.min(); k <= m; k++ )
+                    if ( ! GG[k].isOne() )
+                        R.append( CFFactor( M( GG[k] ), n ) );
+            }
+            out_cf("try cont:",cont,"\n");
+            G = factorize( cont, true );
+            out_cff(G);
             for ( j = G; j.hasItem(); j++ )
                 if ( ! j.getItem().factor().isOne() )
                     R.append( CFFactor( M( j.getItem().factor() ), n ) );
