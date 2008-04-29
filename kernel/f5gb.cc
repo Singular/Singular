19a37887121b18bdf1a55cb76c315d5dbabf71a7
Christian Eder <ederc@mathematik.uni-kl.de>
2008-04-29 18:34:06 +0000

    first test
    git-svn-id: file:///usr/local/Singular/svn/trunk@10693 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/f5gb.cc
+++ kernel/f5gb.cc
@@ -1,11 +1,10 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: f5gb.cc,v 1.1 2008-04-17 12:32:13 wienand Exp $ */
+/* $Id: f5gb.cc,v 1.2 2008-04-29 18:34:06 ederc Exp $ */
 /*
 * ABSTRACT: f5gb interface
 */
-
 #include "mod2.h"
 #include "kutil.h"
 #include "structs.h"
@@ -24,9 +23,22 @@
 #include "f5gb.h"
 
 #ifdef HAVE_F5
-ideal computeF5(ideal I, ring r)
+ideal computeF5(ideal i, ring r)
 {
+  ideal res;
+        
+  Print("Hallo\n");
+  res = idInit(IDELEMS(i),i->rank);
+  for(int j=0; j<IDELEMS(i); j++)
+  {
+    if(NULL != i->m[j])
+    {
+      res->m[j] = i->m[j];
+    }
+  }
+  Print("Ideal i: %d \n", IDELEMS(i));mflush();
+  Print("Ideal res: %d", IDELEMS(res));mflush();
+
   return NULL;
 }
-
 #endif
