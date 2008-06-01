9a6e9ffb1c630a9c3f859a243a888ba80724038d
Christian Eder <ederc@mathematik.uni-kl.de>
2008-06-01 12:49:42 +0000

    quicksort, label poly class
    git-svn-id: file:///usr/local/Singular/svn/trunk@10734 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/f5gb.cc
+++ kernel/f5gb.cc
@@ -1,10 +1,11 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: f5gb.cc,v 1.4 2008-05-24 07:39:22 ederc Exp $ */
+/* $Id: f5gb.cc,v 1.5 2008-06-01 12:49:42 ederc Exp $ */
 /*
 * ABSTRACT: f5gb interface
 */
+
 #include "mod2.h"
 #include "kutil.h"
 #include "structs.h"
@@ -19,28 +20,85 @@
 #include "weight.h"
 #include "intvec.h"
 #include "pInline1.h"
-
 #include "f5gb.h"
-
+#include "lpoly.h"
 #ifdef HAVE_F5
+
+
+
+
+/*2
+* sorting ideals by decreasing total degree 
+* "left" and "right" are the pointer of the first
+* and last polynomial in the considered ideal
+*/
+void qsort_degree(poly* left, poly* right)
+{
+        poly* ptr1 = left;
+        poly* ptr2 = right;
+        poly p1,p2;
+
+        p2 = *(left + (right - left >> 1));
+        do{
+                while(pTotaldegree(*ptr1, currRing) < pTotaldegree(p2, currRing)){
+                        ptr1++;
+                }
+                while(pTotaldegree(*ptr2, currRing) > pTotaldegree(p2,currRing)){
+                        ptr2--;
+                }
+                if(ptr1 > ptr2){
+                        break;
+                }
+                p1    = *ptr1;
+                *ptr1 = *ptr2;
+                *ptr2 = p1;
+        } while(++ptr1 <= --ptr2);
+
+        if(left < ptr2){
+                qsort_degree(left,ptr2);
+        }
+        if(ptr1 < right){
+                qsort_degree(ptr1,right);
+        }
+}
+
+
+/*2
+* computes a gb of the ideal i in the ring r with our f5 
+* implementation 
+*/
 ideal F5main(ideal i, ring r)
 {
-  ideal iTmp, g;
-  int j;      
-  iTmp = idInit(IDELEMS(i),i->rank);
+      ideal iTmp, g;
+      int j;      
+      lpoly* lp;
+      intvec* sort;
+      iTmp = idInit(IDELEMS(i),i->rank);
   
-  for(j=0; j<IDELEMS(i); j++)
-  {
-    if(NULL != i->m[j])
-    {
-      iTmp->m[j] = i->m[j];
-    }
-  }
-   
-  iTmp = kInterRed(i,0);  
-  idShow(i);
-  idShow(iTmp);
-  idShow(iTmp);
-  return iTmp;
+      for(j=0; j<IDELEMS(i); j++)
+      {
+              if(NULL != i->m[j])
+              {
+                      iTmp->m[j] = i->m[j];
+              }
+      }
+
+      iTmp = kInterRed(i,0);  
+      qsort_degree(&iTmp->m[0],&iTmp->m[IDELEMS(iTmp)-1]);
+      idShow(iTmp);
+      
+      lp = new lpoly[IDELEMS(iTmp)];
+      for(j=0; j <IDELEMS(iTmp); j++){
+                lp[j].setPoly(&iTmp->m[j]);
+                pWrite(*(lp[j].getPoly()));
+      }
+                
+
+            
+
+
+        return iTmp;
 }
+
+
 #endif
