d0f98e2b6e781c62af72431742193116d71b7f19
Christian Eder <ederc@mathematik.uni-kl.de>
2008-06-01 12:50:03 +0000

    quicksort
    git-svn-id: file:///usr/local/Singular/svn/trunk@10735 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/f5gb.h
+++ kernel/f5gb.h
@@ -1,15 +1,38 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: f5gb.h,v 1.3 2008-05-23 19:47:39 ederc Exp $ */
+/* $Id: f5gb.h,v 1.4 2008-06-01 12:50:03 ederc Exp $ */
 /*
-* ABSTRACT: ringgb interface
+* ABSTRACT: f5gb interface
 */
 #ifndef F5_HEADER
 #define F5_HEADER
 #include "mod2.h"
 
 #ifdef HAVE_F5
+
+
+class lpoly {
+        private:
+                poly* t_ptr;
+                long* i_ptr;
+                poly* p_ptr;
+
+        public:
+                void setPoly(poly* p);
+                poly* getPoly();
+                void setTerm(poly* t);
+                poly* getTerm();
+                void setIndex(long* i);
+                long* getIndex();
+};
+
+
+
+// sort polynomials in ideal i by decreasing total degree
+void qsort_degree(poly* left, poly* right);
+
+// main function of our f5 implementation
 ideal F5main(ideal i, ring r);
 #endif
 #endif
