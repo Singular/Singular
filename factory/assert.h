dacc2a4a98f8e5f07c186bf31f81a571582ff5c1
Hans Schönemann <hannes@mathematik.uni-kl.de>
2008-08-20 12:39:00 +0000

    *hannes: stdlib & c++
    git-svn-id: file:///usr/local/Singular/svn/trunk@11009 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/assert.h
+++ factory/assert.h
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C -*- */
-/* $Id: assert.h,v 1.7 2006-11-13 14:15:04 Singular Exp $ */
+/* $Id: assert.h,v 1.8 2008-08-20 12:39:00 Singular Exp $ */
 
 /* This is for compatibility with standard assert.h */
 #if defined (NDEBUG) && ! defined (NOASSERT)
@@ -33,7 +33,6 @@ extern "C" {
 #endif
 
 #include <stdio.h>
-#include <stdlib.h>
 
 #ifdef __cplusplus
 }
