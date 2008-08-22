ed0d3c884cb3d290762c61e585627ce1f0c18f60
Hans Schönemann <hannes@mathematik.uni-kl.de>
2008-08-22 09:03:21 +0000

    *hannes: new cf_gmp.h
    git-svn-id: file:///usr/local/Singular/svn/trunk@11024 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/si_gmp.h
+++ kernel/si_gmp.h
@@ -1,9 +1,10 @@
-/* $Id: si_gmp.h,v 1.6 2006-06-13 16:57:12 Singular Exp $ */
-/* $Id: si_gmp.h,v 1.6 2006-06-13 16:57:12 Singular Exp $ */
+/* $Id: si_gmp.h,v 1.7 2008-08-22 09:03:21 Singular Exp $ */
 
 #ifndef INCL_CF_GMP_H
 #define INCL_CF_GMP_H
-
+#ifdef HAVE_FACTORY
+#include <cf_gmp.h>
+#else
 #if defined(__cplusplus) && defined(__GNUC__)
 extern "C" {
 #undef __cplusplus
@@ -14,4 +15,5 @@ extern "C" {
 #include <gmp.h>
 #endif
 
+#endif 
 #endif /* ! INCL_CF_GMP_H */
