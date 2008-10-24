98055283adc51401f1bb9edb034d2a86af64deb8
Hans Schönemann <hannes@mathematik.uni-kl.de>
2008-10-24 13:04:26 +0000

    *hannes: date/version by configure
    git-svn-id: file:///usr/local/Singular/svn/trunk@11157 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- libfac/factor/version.cc
+++ libfac/factor/version.cc
@@ -1,11 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 // emacs edit mode for this file is -*- C++ -*-
-// $Id: version.cc,v 1.11 2008-10-24 12:46:40 Singular Exp $
+// $Id: version.cc,v 1.12 2008-10-24 13:04:26 Singular Exp $
 ///////////////////////////////////////////////////////////////////////////////
+#include "version.h"
 
 const char * libfac_name = "Factorization and characteristic sets library";
-const char * libfac_version = "3.1.0";
-const char * libfac_date = "Oct 2008";
+const char * libfac_version = LIBFAC_VERSION;
+const char * libfac_date = LIBFAC_DATE;
 const char * libfac_author = "M. Messollen";
 
 int    libfac_interruptflag=0;
