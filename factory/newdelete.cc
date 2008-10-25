ad83e4fe7101dbe0763a36ab0bf774daa8c9e7db
Hans Schönemann <hannes@mathematik.uni-kl.de>
2008-10-25 11:05:51 +0000

    *hannes: new-nothrow
    git-svn-id: file:///usr/local/Singular/svn/trunk@11159 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/newdelete.cc
+++ factory/newdelete.cc
@@ -1,5 +1,5 @@
 /* emacs edit mode for this file is -*- C++ -*- */
-/* $Id: newdelete.cc,v 1.2 1997-06-19 12:22:03 schmidt Exp $ */
+/* $Id: newdelete.cc,v 1.3 2008-10-25 11:04:06 Singular Exp $ */
 
 #include <config.h>
 
@@ -11,6 +11,25 @@
 #include "memman.h"
 #endif
 
+// The C++ standard has ratified a change to the new operator.
+//
+//  T *p = new T;
+//
+// Previously, if the call to new above failed, a null pointer would've been returned.
+// Under the ISO C++ Standard, an exception of type std::bad_alloc is thrown.
+// It is possible to suppress this behaviour in favour of the old style
+// by using the nothrow version.
+//
+//  T *p = new (std::nothrow) T;
+//
+// So we have to overload this new also, just to be sure.
+//
+// A further interesting question is, if you don't have enough resources
+// to allocate a request for memory,
+// do you expect to have enough to be able to deal with it?
+// Most operating systems will have slowed to be unusable
+// long before the exception gets thrown.
+
 #ifdef USE_OLD_MEMMAN
 
 void * operator new ( size_t size )
@@ -33,6 +52,15 @@ void operator delete[] ( void * block )
     freeBlock( block, 0 );
 }
 
+void * operator new(size_t size, const std::nothrow_t &) throw()
+{
+    return getBlock( size );
+}
+void * operator new[](size_t size, const std::nothrow_t &) throw()
+{
+    return getBlock( size );
+}
+
 #else
 
 void * operator new ( size_t size )
@@ -53,6 +81,15 @@ void * operator new[] ( size_t size )
 void operator delete[] ( void * block )
 {
     mmFree( block );
+}
+
+void * operator new(size_t size, const std::nothrow_t &) throw()
+{
+    return mmAlloc( size );
+}
+void * operator new[](size_t size, const std::nothrow_t &) throw()
+{
+    return mmAlloc( size );
 }
 
 #endif /* USE_OLD_MEMMAN */
--- kernel/mmalloc.cc
+++ kernel/mmalloc.cc
@@ -1,7 +1,7 @@
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
-/* $Id: mmalloc.cc,v 1.5 2006-07-27 09:09:37 Singular Exp $ */
+/* $Id: mmalloc.cc,v 1.6 2008-10-25 11:05:51 Singular Exp $ */
 /*
 * ABSTRACT: standard version of C++-memory management alloc func
 */
@@ -10,6 +10,7 @@
 #include "mod2.h"
 
 #include <omalloc.h>
+#include <new>
 
 /* We define those, so that our values of
    OM_TRACK and OM_CHECK are used  */
@@ -17,6 +18,33 @@ void* operator new ( size_t size )
 {
   void* addr;
   if (!size) size = 1;
+  omTypeAlloc(void*, addr, size);
+  return addr;
+}
+
+// The C++ standard has ratified a change to the new operator.
+//
+//  T *p = new T;
+//
+// Previously, if the call to new above failed, a null pointer would've been returned.
+// Under the ISO C++ Standard, an exception of type std::bad_alloc is thrown.
+// It is possible to suppress this behaviour in favour of the old style
+// by using the nothrow version.
+//
+//  T *p = new (std::nothrow) T;
+//
+// So we have to overload this new also, just to be sure.
+//
+// A further interesting question is, if you don't have enough resources
+// to allocate a request for memory,
+// do you expect to have enough to be able to deal with it?
+// Most operating systems will have slowed to be unusable
+// long before the exception gets thrown.
+
+void * operator new(size_t size, const std::nothrow_t &) throw()
+{
+  void* addr;
+  if (size==(size_t)0) size = (size_t)1;
   omTypeAlloc(void*, addr, size);
   return addr;
 }
@@ -29,10 +57,19 @@ void operator delete ( void* block )
 void* operator new[] ( size_t size )
 {
   void* addr;
-  if (! size) size = 1;
+  if (size==(size_t)0) size = (size_t)1;
   omTypeAlloc(void*, addr, size);
   return addr;
 }
+
+void * operator new[](size_t size, const std::nothrow_t &) throw()
+{
+  void* addr;
+  if (size==(size_t)0) size = (size_t)1;
+  omTypeAlloc(void*, addr, size);
+  return addr;
+}
+
 
 void operator delete[] ( void* block )
 {
