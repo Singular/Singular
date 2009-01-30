01e82c892793a44b6fafcfe6e6b076492ad7337d
Christian Eder <ederc@mathematik.uni-kl.de>
2009-01-30 17:27:20 +0000

    these files replace all lpoly stuff in the f5 implementation
    git-svn-id: file:///usr/local/Singular/svn/trunk@11345 2c84dea3-7e68-4137-9b89-c4e89433aadc

---
+++ kernel/f5data.cc
@@ -0,0 +1,149 @@
+/****************************************
+*  Computer Algebra System SINGULAR     *
+****************************************/
+/* $Id: f5data.cc,v 1.1 2009-01-30 17:27:20 ederc Exp $ */
+/*
+* ABSTRACT: lpolynomial definition 
+*/
+#include "mod2.h"
+
+#ifdef HAVE_F5
+#include "kutil.h"
+#include "structs.h"
+#include "omalloc.h"
+#include "polys.h"
+#include "p_polys.h"
+#include "ideals.h"
+#include "febase.h"
+#include "kstd1.h"
+#include "khstd.h"
+#include "kbuckets.h"
+#include "weight.h"
+#include "intvec.h"
+#include "pInline1.h"
+#include "f5gb.h"
+#include "f5data.h"
+#include "f5lists.h"
+/*
+================================================================
+all functions working on the class LPoly for labeled polynomials
+================================================================
+*/
+LPoly::LPoly(poly* t,int* i,poly* p) {
+    set(t,i,p);
+}
+
+void LPoly::setPoly(poly* p)  {
+    polynomial = *p;
+}
+
+void LPoly::setTerm(poly* t) {
+    term = *t;
+}
+
+void LPoly::setIndex(int* i) {
+    index = *i;
+}
+
+void LPoly::setDel(bool b) {
+    del = b;
+}
+
+poly LPoly::getPoly() {
+    return polynomial;
+}
+
+poly LPoly::getTerm() {
+    return term;
+}
+
+int LPoly::getIndex() {
+    return index;
+}
+
+bool LPoly::getDel() const {
+    return del;
+}
+
+void LPoly::set(poly* t, int* i, poly* p) {
+    this->setTerm(t);
+    this->setIndex(i);
+    this->setPoly(p);
+}
+
+LPoly* LPoly::get() {
+    return this;
+}
+
+/*
+====================================
+functions working on the class CPair
+====================================
+*/
+CPair::CPair(long degree, poly term1, LPoly* lpoly1, poly term2, LPoly* lpoly2) {
+   deg              =   degree;
+   t1               =   term1;
+   lp1              =   lpoly1;
+   t2               =   term2;
+   lp2              =   lpoly2;
+   lastRuleTested   =   NULL;
+}
+
+long CPair::getDeg() {
+    return deg;
+}
+
+poly CPair::getT1() {
+    return t1;
+}
+
+poly CPair::getT2() {
+    return t2;
+}
+
+poly CPair::getLp1Poly() {
+    return lp1->getPoly();
+}
+
+poly CPair::getLp2Poly() {
+    return lp2->getPoly();
+}
+
+poly CPair::getLp1Term() {
+    return lp1->getTerm();
+}
+
+poly CPair::getLp2Term() {
+    return lp2->getTerm();
+}
+
+int CPair::getLp1Index() {
+    return lp1->getIndex();
+}
+
+int CPair::getLp2Index() {
+    return lp2->getIndex();
+}
+
+Rule* CPair::getLastRuleTested() {
+    return lastRuleTested;
+}
+
+/*
+===================================
+functions working on the class Rule
+===================================
+*/
+Rule::Rule(int* i, poly* t) {
+    index   =   i;
+    term    =   t;
+}
+
+int Rule::getIndex() {
+    return *index;
+}
+
+poly Rule::getTerm() {
+    return *term;
+}
+#endif
---
+++ kernel/f5data.h
@@ -0,0 +1,92 @@
+/****************************************
+*  Computer Algebra System SINGULAR     *
+****************************************/
+/* $Id: f5data.h,v 1.1 2009-01-30 17:27:20 ederc Exp $ */
+/*
+* ABSTRACT: labeled polynomial interface
+*/
+#ifndef F5DATA_HEADER
+#define F5DATA_HEADER
+#ifdef HAVE_F5
+/*
+=========================================================
+=========================================================
+classes for labeled polynomials/pairs/S-polynomials in F5
+=========================================================
+=========================================================
+*/
+class LPoly;
+class CPair;
+class Rule;
+
+
+/*
+============================
+class of labeled polynomials
+============================
+*/
+class LPoly {
+    private:
+        poly    term; //term of signature
+        int     index; //index of signature
+        poly    polynomial; //standard polynomial data
+        bool    del; //for deletion in TopReduction Subalgorithm
+    public:
+                LPoly(poly*t,int* i,poly* p); 
+        void    setPoly(poly* p);
+        poly    getPoly();
+        void    setTerm(poly* t);
+        poly    getTerm();
+        void    setIndex(int* i);
+        int     getIndex();
+        void    setDel(bool b);
+        bool    getDel() const;
+        void    set(poly* t, int* i, poly* p);
+        LPoly*  get();
+};
+
+/*
+========================================================
+structure of rules(i.e. already computed / known labels)
+========================================================
+*/
+class Rule {
+    private:
+        int*     index;    // index of the labeled polynomial the rule comes from 
+        poly*    term;     // term of the labeled polynomial the rule comes from
+    public:
+                Rule(int* i, poly* term);
+        int     getIndex();
+        poly    getTerm();
+};
+
+
+/*
+===================================
+structure of labeled critical pairs
+===================================
+*/
+class CPair {
+    private:
+        long    deg;            // total degree of the critical pair
+        poly    t1;             // first term for label
+        LPoly*  lp1;            // first labeled poly
+        poly    t2;             // second term for label
+        LPoly*  lp2;            // second labeled poly
+        Rule*   lastRuleTested; // already tested by rules up to lastRuleTested
+    public:
+                CPair(long degree, poly term1, LPoly* lpoly1, poly term2, LPoly* lpoly2);
+        long    getDeg();
+        poly    getT1();
+        poly    getLp1Poly();
+        poly    getLp1Term();
+        int     getLp1Index();
+        poly    getT2();
+        poly    getLp2Poly();
+        poly    getLp2Term();
+        int     getLp2Index();
+        Rule*   getLastRuleTested();
+};
+
+#endif
+#endif
