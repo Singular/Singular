2c6535a8648691161a07a546e5c73a0b41b9ac06
Martin Monerjan <monerjan@mathematik.uni-kl.de>
2009-02-09 15:32:07 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/Singular/svn/trunk@11356 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/gfan.cc
+++ kernel/gfan.cc
@@ -5,27 +5,73 @@ Compute the Gröbner fan of an ideal
 #include "mod2.h"
 #include "kstd1.h"
 #include "intvec.h"
+#include "polys.h"
+#include "ideals.h"
 
 #ifndef gfan_DEBUG
 #define gfan_DEBUG
 #endif
 
-ideal getGB(ideal inputIdeal){
-  	ideal gb;
-	gb=kStd(inputIdeal,NULL,testHomog,NULL); //Possible to call without testHomog/isHomog?
-						// Why are 64 "results" printed?
+ideal getGB(ideal inputIdeal)
+{
 	#ifdef gfan_DEBUG
 	printf("Now in getGB\n");
 	#endif
+
+  	ideal gb;
+	gb=kStd(inputIdeal,NULL,testHomog,NULL); //Possible to call without testHomog/isHomog?
+	idSkipZeroes(gb); //Get rid of zero entries
+
 	return gb;
 }
 
-ideal gfan(ideal inputIdeal){
+/****** getWallIneq computes the inequalities ***/
+/*INPUT_TYPE: ideal                             */
+/*RETURN_TYPE: matrix                           */
+/************************************************/
+void getWallIneq(ideal I)
+{
+	#ifdef gfan_DEBUG
+	printf("Computing Inequalities...\n");
+	#endif
+
+	// Exponentenvektor
+	Exponent_t leadexp,aktexp,diffexp;
+	poly aktpoly;
+
+	int lengthGB=IDELEMS(I);
+	printf("The Gröbner basis has %i elements\n",lengthGB);
+
+	// We loop through each g\in GB
+	for (int i=0; i<IDELEMS(I); i++)
+	{
+		aktpoly=(poly)I->m[i];
+		leadexp = pGetExp(aktpoly,1); //get the exp.vect of leading monomial
+		for (int j=2;j<=pLength(aktpoly);j++)
+		{
+			aktexp=pGetExp(aktpoly,j);
+			//diffexp=pSubExp(aktpoly, leadexp,aktexp); //Dang! => runtime error
+			//printf("Exponentenvektor=%i\n",expmark);
+			//printf("Diff=%i\n",expmark-pGetExp(aktpoly,j));
+		}
+		int pCompCount;
+		pCompCount=pLength(aktpoly);
+		printf("Poly No. %i has %i components\n",i,pCompCount);
+	} //for
+	//res=(ideal)aktpoly;
+	//return res;
+}
+
+ideal gfan(ideal inputIdeal)
+{
 	#ifdef gfan_DEBUG
 	printf("Now in subroutine gfan\n");
 	#endif
 	ideal res;
+	matrix ineq; //Matrix containing the boundary inequalities
+
 	res=getGB(inputIdeal);
+	getWallIneq(res);
 	return res;
 }
 
