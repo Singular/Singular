fc4f9a6dc1aa8816f99642c699541cb71324666f
Martin Monerjan <monerjan@mathematik.uni-kl.de>
2009-03-23 16:59:56 +0000

    -no more printf -various ways of #including cddlib -more on class facet -tried to get facet::f->printFacet() to work -removed some no longer useful cddlib stuff
    git-svn-id: file:///usr/local/Singular/svn/trunk@11585 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/gfan.cc
+++ kernel/gfan.cc
@@ -1,9 +1,9 @@
 /*
 Compute the Groebner fan of an ideal
-Author: $Author: monerjan $
-Date: $Date: 2009-03-02 14:44:49 $
-Header: $Header: /exports/cvsroot-2/cvsroot/kernel/gfan.cc,v 1.17 2009-03-02 14:44:49 monerjan Exp $
-Id: $Id: gfan.cc,v 1.17 2009-03-02 14:44:49 monerjan Exp $
+$Author: monerjan $
+$Date: 2009-03-23 16:59:56 $
+$Header: /exports/cvsroot-2/cvsroot/kernel/gfan.cc,v 1.18 2009-03-23 16:59:56 monerjan Exp $
+$Id: gfan.cc,v 1.18 2009-03-23 16:59:56 monerjan Exp $
 */
 
 #include "mod2.h"
@@ -15,8 +15,23 @@ Id: $Id: gfan.cc,v 1.17 2009-03-02 14:44:49 monerjan Exp $
 #include "polys.h"
 #include "ideals.h"
 #include "kmatrix.h"
+#include "iostream.h"
+
+//Hacks for different working places
+#define HOME
+
+#ifdef UNI
 #include "/users/urmel/alggeom/monerjan/cddlib/include/setoper.h" //Support for cddlib. Dirty hack
 #include "/users/urmel/alggeom/monerjan/cddlib/include/cdd.h"
+#endif
+
+#ifdef HOME
+#include "/home/momo/studium/diplomarbeit/cddlib/include/setoper.h"
+#include "/home/momo/studium/diplomarbeit/cddlib/include/cdd.h"
+#endif
+
+#ifdef ITWM
+#endif
 
 #ifndef gfan_DEBUG
 #define gfan_DEBUG
@@ -27,7 +42,17 @@ class facet
 	private:
 		intvec fNormal;		//inner normal, describing the facet uniquely
 	public:
-		facet();		//constructor
+		facet(intvec FN)		//constructor
+		{
+			fNormal = FN;
+		}
+		
+		~facet();		//destructor
+		void printNormal(){
+			std::cout << "The normal is = " << "fNormal" << endl;
+			return;
+		}
+		
 		bool isFlippable;	//flippable facet? Want to have cone->isflippable.facet[i]
 		bool isIncoming;	//Is the facet incoming or outgoing?
 };
@@ -42,12 +67,15 @@ class gcone
 
 	public:
 		gcone(int);		//constructor with dimension
-		poly gcMarkedTerm; 	//marked terms of the cone's Gröbner basis
+		~gcone();		//destructor
+		poly gcMarkedTerm; 	//marked terms of the cone's Grï¿½bner basis
 		ideal gcBasis;		//GB of the cone
 		gcone *next;		//Pointer to *previous* cone in search tree
 	
 		void flip();		//Compute "the other side"
-		void remRedFacets();	//Remove redundant facets of the cone
+		void remRedFacets();	//Remove redundant facets of the cone NOT NEEDED since this is already done by cddlib while compunting the normals
+		
+		ideal GenGrbWlk(ideal, ideal);	//Implementation of the Generic Groebner Walk. Needed for a) Computing the sink and b) finding search facets
 		
 
 };//class gcone
@@ -55,7 +83,7 @@ class gcone
 ideal getGB(ideal inputIdeal)
 {
 	#ifdef gfan_DEBUG
-	printf("Computing a groebner basis...\n");
+	cout << "Computing a groebner basis..." << endl;
 	#endif
 
   	ideal gb;
@@ -65,14 +93,14 @@ ideal getGB(ideal inputIdeal)
 	return gb;
 }
 
-/****** getWallIneq computes the inequalities ***/
+/****** getConeNormals computes the inequalities ***/
 /*INPUT_TYPE: ideal                             */
 /*RETURN_TYPE: matrix                           */
 /************************************************/
-void getWallIneq(ideal I)
+void getConeNormals(ideal I)
 {
 	#ifdef gfan_DEBUG
-	printf("*** Computing Inequalities... ***\n");
+	cout << "*** Computing Inequalities... ***" << endl;
 	#endif
 
 	//All variables go here - except ineq matrix and *v, see below
@@ -92,8 +120,8 @@ void getWallIneq(ideal I)
 	dd_ErrorType dderr=dd_NoError;	//
 	// End of var declaration
 
-	printf("The Groebner basis has %i elements\n",lengthGB);
-	printf("The current ring has %i variables\n",numvar);
+	cout << "The Groebner basis has " << lengthGB << " elements" << endl;
+	cout << "The current ring has " << numvar << " variables" << endl;
 	cols = numvar;
 
 	//Compute the # inequalities i.e. rows of the matrix
@@ -103,8 +131,8 @@ void getWallIneq(ideal I)
 		aktpoly=(poly)I->m[ii];
 		rows=rows+pLength(aktpoly)-1;
 	}
-	printf("rows=%i\n",rows);
-	printf("Will create a %i x %i - matrix to store inequalities\n",rows,cols);
+	cout << "rows=" << rows << endl;
+	cout << "Will create a " << rows << " x " << cols << " matrix to store inequalities" << endl;
 
 	dd_rowrange aktmatrixrow=0;	// needed to store the diffs of the expvects in the rows of ddineq
 	dd_set_global_constants();
@@ -118,7 +146,7 @@ void getWallIneq(ideal I)
 	{
 		aktpoly=(poly)I->m[i];		//get aktpoly as i-th component of I
 		pCompCount=pLength(aktpoly);	//How many terms does aktpoly consist of?
-		printf("Poly No. %i has %i components\n",i,pCompCount);
+		cout << "Poly No. " << i << " has " << pCompCount << " components" << endl;
 
 		int *v=(int *)omAlloc((numvar+1)*sizeof(int));
 		pGetExpV(aktpoly,v);	//find the exp.vect in v[1],...,v[n], use pNext(p)
@@ -154,7 +182,7 @@ void getWallIneq(ideal I)
 	//Maybe add another row to contain the constraints of the standard simplex?
 
 	#ifdef gfan_DEBUG
-	printf("The inequality matrix is:\n");
+	cout << "The inequality matrix is" << endl;
 	dd_WriteMatrix(stdout, ddineq);
 	#endif
 
@@ -166,24 +194,37 @@ void getWallIneq(ideal I)
 		 dd_WriteErrorMessages(stderr,dderr);	//if so tell us
 	} else
 	{
-		printf("Redundant rows: ");
+		cout << "Redundant rows: ";
 		set_fwrite(stdout, ddredrows);		//otherwise print the redundant rows
 	}//if dd_Error
 
 	//Remove reduntant rows here!
 	dd_MatrixCanonicalize(&ddineq, &ddlinset, &ddredrows, &ddnewpos, &dderr);
 	#ifdef gfan_DEBUG
-	printf("Having removed redundant rows, the inequalities now read:\n");
+	cout << "Having removed redundancies, the normals now read:" << endl;
 	dd_WriteMatrix(stdout,ddineq);
 	#endif
 
-	dd_PolyhedraPtr ddpolyh;
+	/*dd_PolyhedraPtr ddpolyh;
 	dd_MatrixPtr G;
 	ddpolyh=dd_DDMatrix2Poly(ddineq, &dderr);
 	G=dd_CopyGenerators(ddpolyh);
 	printf("\nSpanning vectors = rows:\n");
 	dd_WriteMatrix(stdout, G);
-
+	*/
+	
+	
+	/*Write the normals into class facet
+		How do I get the #rows in ddineq? \exists s.th. like dd_NumRows?
+	Strange enough: facet *f=new facet(intvec(2,3)) does work for the constructor facet(intvec FN){fNormal = FN;}
+	*/
+	for (int kk = 1; kk<=2; kk++){
+		facet *f = new facet(intvec(2,3));
+		//facet *fPtr = &f;
+		cout << &f << endl;
+ 		f->printNormal();
+	}
+	
 	//ddineq->representation=dd_Inequality;		//We want our LP to be Ax>=0
 
 	//Clean up but don't delete the return value! (Whatever it will turn out to be)
@@ -204,13 +245,17 @@ void getWallIneq(ideal I)
 ideal gfan(ideal inputIdeal)
 {
 	#ifdef gfan_DEBUG
-	printf("Now in subroutine gfan\n");
+	cout << "Now in subroutine gfan" << endl;
 	#endif
 	ideal res;
 	matrix ineq; //Matrix containing the boundary inequalities
-
+	/*
+	1. Select target order
+	2. Compute GB of inputIdeal wrt target order
+	3. getConeNormals
+	*/
 	res=getGB(inputIdeal);
-	getWallIneq(res);
+	getConeNormals(res);
 	return res;
 }
 #endif
