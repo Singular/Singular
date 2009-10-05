3e96c5dc8f5e261b746d32bb4d985ec8def80154
Hans Schönemann <hannes@mathematik.uni-kl.de>
2009-10-05 17:26:51 +0000

    *hannes: syntax fix: deine all non-pure virtual functions
    git-svn-id: file:///usr/local/Singular/svn/trunk@12159 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/Minor.h
+++ Singular/Minor.h
@@ -458,7 +458,7 @@ class MinorValue {
              * @return the weight of a given instance of MinorValue
              * @see Cache::getWeight () const
              */
-             virtual long getWeight () const;
+             virtual long getWeight () const {};
 
              /**
              * A method for accessing the number of retrievals of this minor. Multiple retrievals will
@@ -563,7 +563,7 @@ class MinorValue {
              * A method for providing a printable version of the represented MinorValue.
              * @return a printable version of the given instance as instance of class string
              */
-             virtual string toString () const;
+             virtual string toString () const {};
 
              /**
              * A method for printing a string representation of the given MinorValue to std::cout.
