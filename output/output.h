63c3a8df898418cfb9484633a8d9eb78c299a95c
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2010-05-05 14:26:32 +0200

    syntax fix

--- output/output.h
+++ output/output.h
@@ -114,7 +114,6 @@ extern "C"
 extern int dReportError(const char* fmt, ...);
 #define dReportBug(s) \
   dReportError("Bug reported: %s\n occured at %s,%d\n", s, __FILE__, __LINE__)
-#endif
 
 // this is just a dummy procedure which is called after the error
 // has been reported. Within the debugger, set a breakpoint on this
