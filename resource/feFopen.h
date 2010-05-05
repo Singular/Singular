0c3199ca29aa7250bd56827a0abaeda1f10da326
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2010-05-05 17:00:41 +0200

    directory resources

---
+++ resource/feFopen.h
@@ -0,0 +1,26 @@
+#ifndef FEFOPEN_H
+#define FEFOPEN_H
+/*****************************************************************
+ *
+ * File Stuff
+ *
+ *****************************************************************/
+FILE *  feFopen(const char *path, const char *mode, char *where=NULL,
+                int useWerror=FALSE, int path_only=FALSE);
+
+/*
+// These are our versions of fopen and fread They are very similar to
+// the usual fopen and fread, except that on reading, they always
+// convert "\r\n" into " \n" and "\r" into "\n".
+//
+// IMPORTANT: do only use myfopen and myfread when reading text,
+// do never use fopen and fread
+*/
+#ifdef ix86_Win
+FILE *myfopen(const char *path, const char *mode);
+#else
+#define myfopen fopen
+#endif
+
+size_t myfread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+#endif
