9056ea919222afe0abe7d535be9a0315f3273359
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2010-05-19 14:36:44 +0200

    Templates: initial version

---
+++ templates/template.c
@@ -0,0 +1,33 @@
+// -*- c++ -*-
+/***************************************************************************//**
+ * @file template.c
+ * 
+ * This file implements the class TEMPLATE. AAA. BBB.
+ * CCC.
+ *
+ * DDDD.
+ *
+ * @author Oleksandr Motsak
+ *
+ * @par Copyright:
+ *   (C) by The SINGULAR Team, see COPYING file
+ * EEE.
+ *
+ * FFFFFFFF.
+ * 
+ * FFFFFFFFFFFFFFFFFFFFFFFFF.
+ * @internal 
+ * @version \$Id$
+ *
+ ******************************************************************************/
+
+// include header file
+#include "template.h"
+
+BEGIN_NAMESPACE_SINGULARXX
+
+// compiled stuff of the class TEMPLATE
+
+
+
+END_NAMESPACE_SINGULARXX
