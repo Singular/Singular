09330b5be651d0b1781ddd8c92bbce0af1817795
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2010-11-09 12:47:13 +0000

    more singular stuff removed
    git-svn-id: file:///usr/local/Singular/svn/trunk@13623 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- factory/int_int.h
+++ factory/int_int.h
@@ -22,12 +22,6 @@
 #include "cf_gmp.h"
 #include "gmpext.h"
 
-#ifdef SINGULAR
-#ifndef OM_NDEBUG
-#define OM_NDEBUG
-#endif
-#endif
-
 #ifdef HAVE_OMALLOC
 #  include <omalloc/omalloc.h>
 #endif
@@ -125,10 +119,8 @@ public:
     int ilog2();
 
     friend class InternalRational;
-#ifdef SINGULAR
     friend void gmp_numerator ( const CanonicalForm & f, mpz_ptr result);
     friend void gmp_denominator ( const CanonicalForm & f, mpz_ptr result );
-#endif /* SINGULAR */
     friend MP_INT getmpi ( InternalCF * value, bool symmetric );
 };
 
