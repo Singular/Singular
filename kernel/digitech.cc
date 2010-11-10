fda70904f94761d72bf7d66535b4a93646e471c3
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2010-11-10 16:07:34 +0000

    code simplification
    git-svn-id: file:///usr/local/Singular/svn/trunk@13628 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- kernel/digitech.cc
+++ kernel/digitech.cc
@@ -9,7 +9,7 @@
 #include <kernel/kbuckets.h>
 #include <kernel/ideals.h>
 static ideal zero_ideal;
-#if 1
+
 void bit_reduce(poly & f,ring r)
 {
   poly p=f;
@@ -41,173 +41,6 @@ void bit_reduce(poly & f,ring r)
   kBucketDestroy(&erg_bucket);
   f=erg;
 }
-#else
-
-
-
-void do_bit_reduce(poly f, kBucket_pt bucket)
-{
-    ring r=bucket->bucket_ring;
-    int p=rChar(r);
-    int max=rVar(r);
-    while (f!=NULL)
-        {
-            // int len=0;
-//             poly erg;
-//             kBucketClear(bucket,&erg, &len);
-//             kBucketDestroy(&bucket);
-//             return erg;
-            //return;
-        //}
-
-    BOOLEAN changed=FALSE;
-    poly next=pNext(f);
-    pNext(f)=NULL;
-    int i;
-    for(i=1;i<=max;i++)
-    {
-      unsigned long exp;
-      while((exp=p_GetExp(f,i,r))>=p){
-               p_SetExp(f,i,exp-p+1,r);
-               changed=TRUE;
-      }
-    }
-
-    if (changed)
-    {
-        p_Setm(f,r);
-
-        int pseudo_len=0;
-        kBucket_Add_q(bucket,f,&pseudo_len);
-        //do_bit_reduce(next,bucket);
-    }
-    else
-    {
-        //do_bit_reduce(next,bucket);
-        int pseudo_len=0;
-        kBucket_Add_q(bucket,f,&pseudo_len);
-
-    }
-    f=next;
-    }
-}
-poly do_bitreduce(poly f, ring r)
-{
-  poly erg=NULL;
-  poly *append_to=&erg;
-  int p=rChar(r);
-  int max=rVar(r);
-  kBucket_pt bucket= kBucketCreate(r);
-  kBucketInit(bucket,NULL,0 /*pLength(P.p)*/);
-  while(f!=NULL)
-  {
-    BOOLEAN changed=FALSE;
-    poly next=pNext(f);
-    pNext(f)=NULL;
-    assume(pNext(f)==NULL);
-    int i;
-    for(i=1;i<=max;i++)
-    {
-      unsigned long exp;
-      while((exp=p_GetExp(f,i,r))>=p)
-      {
-               p_SetExp(f,i,exp-p+1,r);
-               changed=TRUE;
-      }
-    }
-    if (changed)
-    {
-        p_Setm(f,r);
-        int pseudo_len=0;
-        kBucket_Add_q(bucket,f,&pseudo_len);
-    }
-    else
-    {
-        (*append_to)=f;
-        append_to=&(pNext(f));
-    }
-    f=next;
-  }
-  {
-    int pseudo_len=0;
-    kBucket_Add_q(bucket,erg,&pseudo_len);
-    pseudo_len=0;
-    kBucketClear(bucket,&erg,&pseudo_len);
-  }
-    return erg;
-}
-void bit_reduce2(poly & f,ring r)
-{
-  if (f==NULL) return;
-  if (pNext(f)==NULL){
-    int p=rChar(r);
-    int max=rVar(r);
-    BOOLEAN changed=FALSE;
-    poly next=pNext(f);
-    assume(pNext(f)==NULL);
-    int i;
-    for(i=1;i<=max;i++)
-    {
-      unsigned long exp;
-      while((exp=p_GetExp(f,i,r))>=p)
-      {
-               p_SetExp(f,i,exp-p+1,r);
-               changed=TRUE;
-      }
-    }
-    if (changed)
-        p_Setm(f,r);
-    return;
-  }
-  // kBucket_pt bucket= kBucketCreate(r);
-//   kBucketInit(bucket,NULL,0 /*pLength(P.p)*/);
-//   do_bit_reduce(f,bucket);
-//   int len=0;
-//   kBucketClear(bucket,&f, &len);
-//   kBucketDestroy(&bucket);
-    f=do_bitreduce(f,r);
-
-}
-void bit_reduce1(poly & f,ring r)
-{
-  if (f==NULL) return;
-
-
-  if (pNext(f)==NULL)
-  {
-    int p=rChar(r);
-    int max=rVar(r);
-    BOOLEAN changed=FALSE;
-    poly next=pNext(f);
-    assume(pNext(f)==NULL);
-    int i;
-    for(i=1;i<=max;i++)
-    {
-      unsigned long exp;
-      while((exp=p_GetExp(f,i,r))>=p)
-      {
-               p_SetExp(f,i,exp-p+1,r);
-               changed=TRUE;
-      }
-    }
-    if (changed)
-        p_Setm(f,r);
-    return;
-  }
-  kBucket_pt bucket= kBucketCreate(r);
-  kBucketInit(bucket,NULL,0 /*pLength(P.p)*/);
-  do_bit_reduce(f,bucket);
-  int len=0;
-  kBucketClear(bucket,&f, &len);
-  kBucketDestroy(&bucket);
-
-}
-// void bit_reduce_arg(poly & f,ring r){
-//   kBucket_pt bucket= kBucketCreate(r);
-//   kBucketInit(bucket,NULL,0 /*pLength(P.p)*/);
-//   f=do_bit_reduce(f,bucket);
-// }
-#endif
 
 poly uni_subst_bits(poly outer_uni, poly inner_multi, ring r)
 {
@@ -237,9 +70,6 @@ poly uni_subst_bits(poly outer_uni, poly inner_multi, ring r)
     potences[d]=NULL;
     p=pNext(p);
   }
-
-
-
 
   //free potences
   for(i=0;i<=d_max;i++)
