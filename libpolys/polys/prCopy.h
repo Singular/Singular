351a5f98b6806d2385238578e737147580dc23de
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2011-01-13 13:50:41 +0100

    syntax fixes for libpoly, part 3: ring.cc

--- libpolys/polys/prCopy.h
+++ libpolys/polys/prCopy.h
@@ -1,3 +1,5 @@
+#ifndef PRCOPY_H
+#define PRCOPY_H
 /****************************************
 *  Computer Algebra System SINGULAR     *
 ****************************************/
@@ -18,27 +20,23 @@
  * Copy:        monomials and coeffs are copied
  *
  ************************************************************************/
-poly prMoveR_NoSort(poly &p, ring r, ring dest_r = currRing);
-poly prMoveR(poly &p, ring r, ring dest_r = currRing);
-poly prCopyR_NoSort(poly p, ring r, ring dest_r = currRing);
-poly prCopyR(poly p, ring r, ring dest_r = currRing);
-poly prShallowCopyR_NoSort(poly p, ring r, ring dest_t = currRing);
-poly prShallowCopyR(poly p, ring r, ring dest_t = currRing);
-poly prHeadR(poly p, ring r, ring dest_r = currRing);
+poly prMoveR_NoSort(poly &p, ring r, ring dest_r);
+poly prMoveR(poly &p, ring r, ring dest_r);
+poly prCopyR_NoSort(poly p, ring r, ring dest_r);
+poly prCopyR(poly p, ring r, ring dest_r);
+poly prShallowCopyR_NoSort(poly p, ring r, ring dest_t);
+poly prShallowCopyR(poly p, ring r, ring dest_t);
+poly prHeadR(poly p, ring r, ring dest_rg);
 
-ideal idrMoveR_NoSort(ideal &id, ring r, ring dest_r = currRing);
-ideal idrMoveR(ideal &id, ring r, ring dest_r = currRing);
+ideal idrMoveR_NoSort(ideal &id, ring r, ring dest_r );
+ideal idrMoveR(ideal &id, ring r, ring dest_r );
 
-ideal idrCopyR_NoSort(ideal id, ring r, ring dest_r = currRing);
-ideal idrCopyR(ideal id, ring r, ring dest_r = currRing);
+ideal idrCopyR_NoSort(ideal id, ring r, ring dest_r );
+ideal idrCopyR(ideal id, ring r, ring dest_r );
 
-ideal idrShallowCopyR_NoSort(ideal id, ring r, ring dest_r = currRing);
-ideal idrShallowCopyR(ideal id, ring r, ring dest_r = currRing);
-
+ideal idrShallowCopyR_NoSort(ideal id, ring r, ring dest_r );
+ideal idrShallowCopyR(ideal id, ring r, ring dest_r );
 
 /// Copy leading terms of id[i] via prHeeadR into dest_r
-ideal idrHeadR(ideal id, ring r, ring dest_r = currRing);
-
-
-
-
+ideal idrHeadR(ideal id, ring r, ring dest_r );
+#endif
