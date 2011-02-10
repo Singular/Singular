76b37cc99585766333fcee968b6b6dad3541da2c
Alexander Dreyer <alexander.dreyer@itwm.fraunhofer.de>
2011-02-10 18:15:30 +0000

    Merge commit 'origin/master' into pyobject
    From: Alexander Dreyer <alexander.dreyer@itwm.fraunhofer.de>
    
    git-svn-id: file:///usr/local/Singular/svn/trunk@13837 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/pyobject_setup.cc
+++ Singular/pyobject_setup.cc
@@ -16,20 +16,28 @@
 #include <kernel/febase.h>
 #include "static.h"
 
-/* whether development version of python is linked statically or dynamically */
-#ifdef HAVE_STATIC
-  #define HAVE_STATIC_PYTHON 1
+/* whether pyobject module is linked statically or dynamically */
+#ifdef HAVE_PYTHON
+
+  #if defined(HAVE_STATIC) 
+    #ifdef HAVE_STATIC_PYTHON
+      #define HAVE_STATIC_PYOBJECT
+    #endif
+  #else
+    #ifdef EMBED_PYTHON
+      #define HAVE_STATIC_PYOBJECT
+    #else
+      #define HAVE_DYNAMIC_PYOBJECT
+    #endif
+  #endif 
 #endif
 
-#ifdef HAVE_PYTHON  // Case: python available (prefered variant) 
-
-
-# ifdef HAVE_STATIC_PYTHON // Case: link pyobject interface statically
+# ifdef HAVE_STATIC_PYOBJECT // Case: link pyobject interface statically
 #include "pyobject.cc"
 void pyobject_setup() { pyobject_init(); }
 
 
-# else                     // Case: pyobject is dynamic module (prefered variant)
+# elif defined(HAVE_DYNAMIC_PYOBJECT) // Case: pyobject is dynamic module (prefered variant)
 
 // forward declaration for Singular/iplib.cc
 void* binary_module_function(const char* lib, const char* func); 
@@ -37,10 +45,8 @@ void pyobject_setup()
 {
   void* fktn = binary_module_function("pyobject", "mod_init");
   if (fktn) (* reinterpret_cast<void (*)()>(fktn) )();
-  else Werror("python related functions are not avialable");
+  else Werror("python related functions are not available");
 }
-
-# endif
 
 #else                // Case: no python
 void pyobject_setup() { }
