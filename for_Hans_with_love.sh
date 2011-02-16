af8c79e316e617d102755c9e4c24252fd4a518af
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2011-02-16 15:54:13 +0100

    fixed typo in reporter/Makefile.am

--- for_Hans_with_love.sh
+++ for_Hans_with_love.sh
@@ -9,7 +9,19 @@ automake -a
 
 TOP_DIR=$PWD
 
-for d in . omalloc libpolys/ libpolys/misc libpolys/resources libpolys/reporter libpolys/coeffs libpolys/polys; do
+for d in . libpolys; do
+  echo "==============="
+  echo "run aclocal, autoconf in $d"
+  echo "---------------"
+  cd $d;
+  aclocal
+  autoconf
+  automake -a
+  cd $TOP_DIR
+  echo
+done
+
+for d in omalloc libpolys/misc libpolys/resources libpolys/reporter libpolys/coeffs libpolys/polys; do
   echo "==============="
   echo "run aclocal, autoheader, autoconf in $d"
   echo "---------------"
