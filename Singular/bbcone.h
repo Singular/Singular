0dc3de36e49849c906bfedf4c8d4ebf9a5e9a532
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2011-02-26 12:06:07 +0000

    conditional compile: HAVE_FANS
    git-svn-id: file:///usr/local/Singular/svn/trunk@13901 2c84dea3-7e68-4137-9b89-c4e89433aadc

--- Singular/bbcone.h
+++ Singular/bbcone.h
@@ -1,6 +1,7 @@
 #ifndef BBCONE_H
 #define BBCONE_H
 
+#ifdef HAVE_FANS
 #include <gfanlib/gfanlib.h>
 
 extern int coneID;
@@ -12,4 +13,5 @@ gfan::ZMatrix intmat2ZMatrix(const intvec* iMat);
 gfan::ZVector intvec2ZVector(const intvec* iVec);
 int integerToInt(gfan::Integer const &V, bool &ok);
 
+#endif
 #endif
