494884581702ad06eefdf632a5e1abb42bd90f4d
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2011-03-22 11:16:48 +0100

    fixed weight.cc

--- libpolys/polys/weight.h
+++ libpolys/polys/weight.h
@@ -10,25 +10,25 @@
 
 #include <polys/monomials/ring.h>
 
-//extern short * ecartWeights;
+extern short * ecartWeights;
 //extern pFDegProc pFDegOld;
 //extern pLDegProc pLDegOld;
 
-void kEcartWeights(poly* s, int sl, short *eweight);
+void kEcartWeights(poly* s, int sl, short *eweight, const ring R);
 //BOOLEAN kWeight(leftv res,leftv id);
 //BOOLEAN kQHWeight(leftv res,leftv v);
 long maxdegreeWecart(poly p,int *l, ring r);
 long totaldegreeWecart(poly p, ring r);
 long totaldegreeWecart_IV(poly p, ring r, const short *w);
 
-short * iv2array(intvec * iv);
+short * iv2array(intvec * iv, const ring R);
 
 // internal:
 extern "C" double (*wFunctional)(int *degw, int *lpol, int npol,
        double *rel, double wx, double wNsqr);
 extern "C" double wFunctionalBuch(int *degw, int *lpol, int npol,
        double *rel, double wx, double wNsqr);
-void wCall(poly* s, int sl, int *x, double wNsqr);
+void wCall(poly* s, int sl, int *x, double wNsqr, const ring R);
 
 #endif
 
