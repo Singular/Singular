68fbf33af19152bb1238c4a645902d03ebcb4d04
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2011-04-13 13:09:09 +0200

    created libpolys/tests and set up the beginning of a test-suite using cxxtest

--- for_Hans_with_love.sh
+++ for_Hans_with_love.sh
@@ -18,30 +18,19 @@ for d in . libpolys; do
   echo "run aclocal, autoconf in $d"
   echo "---------------"
   cd $d;
-  aclocal
+  aclocal -I $TOP_DIR/m4
   autoconf
   automake -a
   cd $TOP_DIR
   echo
 done
 
-for d in omalloc libpolys/misc libpolys/resources libpolys/reporter; do
+for d in omalloc libpolys/misc libpolys/resources libpolys/reporter libpolys/coeffs; do
   echo "==============="
-  echo "run aclocal, autoheader, autoconf in $d"
+  echo "run aclocal -I $TOP_DIR/m4, autoheader, autoconf in $d"
   echo "---------------"
   cd $d;
-  aclocal
-  autotools
-  cd $TOP_DIR
-  echo
-done
-
-for d in libpolys/coeffs; do
-  echo "==============="
-  echo "run aclocal -I ../../m4, autoheader, autoconf in $d"
-  echo "---------------"
-  cd $d;
-  aclocal -I ../../m4
+  aclocal -I $TOP_DIR/m4
   autotools
   cd $TOP_DIR
   echo
@@ -49,10 +38,10 @@ done
 
 for d in libpolys/polys; do
   echo "==============="
-  echo "run aclocal -I ../../m4 -I m4, autoheader, autoconf in $d"
+  echo "run aclocal -I $TOP_DIR/m4 -I m4, autoheader, autoconf in $d"
   echo "---------------"
   cd $d;
-  aclocal -I ../../m4 -I m4
+  aclocal -I $TOP_DIR/m4 -I m4
   autotools
   cd $TOP_DIR
   echo
