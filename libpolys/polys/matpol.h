0a3a629f65533731dfccd53a13103d906918010b
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-04-13 19:17:16 +0200

    FIX: fixed the matrix-related inteface: e.g. mpCopy(matrix) -> mp_Copy(matrix, const ring)

--- libpolys/polys/matpol.h
+++ libpolys/polys/matpol.h
@@ -31,54 +31,60 @@ class ip_smatrix
 };
 
 matrix mpNew(int r, int c);
+static inline matrix mp_New(int r, int c){ return mpNew(r,c); }
+
 // matrix mpCopy(matrix a);
-void   mpDelete(matrix* a, const ring r);
-matrix mpCopy(const matrix a, const ring rSrc, const ring rDst);
+void   mp_Delete(matrix* a, const ring r);
+matrix mp_Copy(const matrix a, const ring rSrc, const ring rDst);
 
-static inline matrix mp_Copy(matrix a, ring r){ return mpCopy(a, r, r); }
+matrix mp_Copy(matrix a, const ring r);
+// static inline matrix mp_Copy(matrix a, const ring r){ return mp_Copy(a, r, r); }
 
-matrix mpInitP(int r, int c, poly p);
-matrix mpInitI(int r, int c, int v);
-matrix mpMultI(matrix a, int f);
-matrix mpMultP(matrix a, poly p);
-matrix pMultMp(poly p, matrix a);
-matrix mpAdd(matrix a, matrix b);
-matrix mpSub(matrix a, matrix b);
-matrix mpMult(matrix a, matrix b);
-matrix mpTransp(matrix a);
-BOOLEAN mpEqual(matrix a, matrix b);
-poly mpTrace ( matrix a);
-poly TraceOfProd ( matrix a, matrix b, int n);
+matrix mp_InitP(int r, int c, poly p, const ring R);
+matrix mp_InitI(int r, int c, int v, const ring R);
+matrix mp_MultI(matrix a, int f, const ring r);
+matrix mp_MultP(matrix a, poly p, const ring r);
+matrix pMultMp(poly p, matrix a, const ring r);
+matrix mp_Add(matrix a, matrix b, const ring r);
+matrix mp_Sub(matrix a, matrix b, const ring r);
+matrix mp_Mult(matrix a, matrix b, const ring r);
+matrix mp_Transp(matrix a, const ring r);
+BOOLEAN mp_Equal(matrix a, matrix b, const ring r);
+poly mp_Trace ( matrix a, const ring r);
+poly TraceOfProd ( matrix a, matrix b, int n, const ring r);
 
-poly mpDet (matrix m);
-matrix mpWedge(matrix a, int ar);
+poly mp_Det (matrix m, const ring r);
+matrix mp_Wedge(matrix a, int ar, const ring r);
+
 // BOOLEAN mpJacobi(leftv res,leftv a);
 // BOOLEAN mpKoszul(leftv res,leftv b/*in*/, leftv c/*ip*/, leftv id=NULL);
-poly mpDetBareiss (matrix a);
+poly mp_DetBareiss (matrix a, const ring r);
 
-//matrix mpHomogen(matrix a, int v);
+//matrix mp_Homogen(matrix a, int v, const ring r);
 
-void   mpMonomials(matrix c, int r, int var, matrix m);
-matrix mpCoeffs(ideal I, int var);
-/* corresponds to Maple's coeffs:
-*var has to be the number of a variable
-*/
+void   mp_Monomials(matrix c, int r, int var, matrix m, const ring R);
 
-matrix mpCoeffProc (poly f, poly vars);
-/* corresponds to Macauley's coef:
-the exponent vector of vars has to contain the variables, eg 'xy';
-then the poly f is searched for monomials in x and y, these monimials
-are written to the first row of the matrix co.
-the second row of co contains the respective factors in f.
-Thus f = sum co[1,i]*co[2,i], i = 1..cols, rows equals 2. */
-void mpCoef2(poly v, poly vars, matrix *c, matrix *m);
-/* for minors with Bareiss */
-void mpRecMin(int, ideal, int &, matrix, int, int, poly, ideal);
-void mpMinorToResult(ideal, int &, matrix, int, int, ideal);
+/// corresponds to Maple's coeffs:
+/// var has to be the number of a variable
+matrix mp_Coeffs(ideal I, int var, const ring r);
 
-BOOLEAN mpIsDiagUnit(matrix U);
-void iiWriteMatrix(matrix im, const char *n, int dim,int spaces=0);
-char * iiStringMatrix(matrix im, int dim,char ch=',');
+matrix mp_CoeffProc (poly f, poly vars, const ring r);
+/// corresponds to Macauley's coef:
+/// the exponent vector of vars has to contain the variables, eg 'xy';
+/// then the poly f is searched for monomials in x and y, these monimials
+/// are written to the first row of the matrix co.
+/// the second row of co contains the respective factors in f.
+/// Thus f = sum co[1,i]*co[2,i], i = 1..cols, rows equals 2.
+void mp_Coef2(poly v, poly vars, matrix *c, matrix *m, const ring r);
+
+/// for minors with Bareiss
+void mp_RecMin(int, ideal, int &, matrix, int, int, poly, ideal, const ring r);
+void mp_MinorToResult(ideal, int &, matrix, int, int, ideal, const ring r);
+
+BOOLEAN mp_IsDiagUnit(matrix U, const ring r);
+void iiWriteMatrix(matrix im, const char *n, int dim, const ring r, int spaces=0);
+char * iiStringMatrix(matrix im, int dim, const ring r, char ch=',');
 
 extern omBin ip_smatrix_bin;
-#endif
+
+#endif/* MATPOL_H */
