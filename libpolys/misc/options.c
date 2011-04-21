cf56e578dc467ae99c0158075503ffb5ae672052
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-04-21 23:59:58 +0200

    FIX: fixed linkage under Mac OS X: one has to initialize global variables

--- libpolys/misc/options.c
+++ libpolys/misc/options.c
@@ -1,3 +1,5 @@
-unsigned test;
-unsigned verbose;
+// the following initialization is needed for linkage on Mac OS X, 
+// since initialized variables will be "D" (uninit. go to "C") segments
+unsigned test = 0;
+unsigned verbose = 0;
 
