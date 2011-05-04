211146554165c4e6c498c633261b86cf0969119d
Martin Lee <martinlee84@web.de>
2011-05-04 19:02:52 +0200

    switched on HAVE_FACTORY in auxiliary.h.in deleted include of cf_gmp.h in si_gmp.h

--- libpolys/coeffs/si_gmp.h
+++ libpolys/coeffs/si_gmp.h
@@ -2,9 +2,7 @@
 
 #ifndef INCL_CF_GMP_H
 #define INCL_CF_GMP_H
-#ifdef HAVE_FACTORY
-#include <factory/cf_gmp.h>
-#else
+
 #if defined(__cplusplus) && defined(__GNUC__)
 extern "C" {
 #undef __cplusplus
@@ -15,5 +13,4 @@ extern "C" {
 #include <gmp.h>
 #endif
 
-#endif 
 #endif /* ! INCL_CF_GMP_H */
