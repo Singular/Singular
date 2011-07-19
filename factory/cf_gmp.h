9d53d0ba60a48685fd21921665282099d9253c1e
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2011-07-19 10:09:01 +0200

    fix includes for C-files

--- factory/cf_gmp.h
+++ factory/cf_gmp.h
@@ -2,12 +2,16 @@
 #define CF_GMP_H
 
 
-#define __cplusplus_backup __cplusplus
-#undef __cplusplus
-extern "C" {
-#include <gmp.h>
-}
-#define __cplusplus __cplusplus_backup
+#ifdef _cplusplus
+  #define __cplusplus_backup __cplusplus
+  #undef __cplusplus
+  extern "C" {
+  #include <gmp.h>
+  }
+  #define __cplusplus __cplusplus_backup
+#else
+  #include <gmp.h>
+#endif
 
 
 #endif /* CF_GMP_H */
