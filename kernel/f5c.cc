d24881483b861af455933b94bdd90bd3f5b9d74a
Christian Eder <ederc@mathematik.uni-kl.de>
2011-07-20 09:57:57 +0200

    1. deletes HAVE_F5C 2. updates F5 files in kernel

--- kernel/f5c.cc
+++ kernel/f5c.cc
@@ -1,5 +1,5 @@
 #include <kernel/mod2.h>
-#ifdef HAVE_F5C
+#ifdef HAVE_F5
 #include <unistd.h>
 #include <kernel/structs.h>
 #include <kernel/kutil.h>
@@ -16,8 +16,6 @@
 #include <misc/intvec.h>
 #include <libpolys/polys/polys.h>
 #include <kernel/f5c.h>
-#include <kernel/F5cData.h>
-#include <kernel/F5cLists.h>
 #include <kernel/timer.h>
 /*
 ==========================================================================
@@ -25,24 +23,9 @@ MAIN:computes a gb of the ideal i in the ring r with our F5 implementation
 ==========================================================================
 */
 ideal f5cMain(ideal id, ring r) {
-  Print("SHORT EXP VECTOR 1:  %ld\n", pGetShortExpVector(id->m[0]));
-  int* expVec   = new int[(r->N)+1];
-  pGetExpV(id->m[0],expVec);
-  Print("EXP VECTOR 1: %d\n",expVec[1]);
-  Label* label  = new Label(expVec);
-  Print("EXP VECTOR 2: %d\n", label->getExpVec()[1]);
-  Print("SHORT EXP VECTOR 2:  %ld\n", label->getShortExpVec());
-  //Print("%ld\n", label->computeShortExpVec(expVec)); 
-  Print("SHORT EXP VECTOR 1:  %ld\n", pGetShortExpVector(id->m[1]));
-  //int* expVec   = new int[(r->N)+1];
-  pGetExpV(id->m[1],expVec);
-  Print("EXP VECTOR 1: %d\n",expVec[1]);
-  Label* label2  = new Label(expVec);
-  Print("EXP VECTOR 2: %d\n", label2->getExpVec()[1]);
-  Print("SHORT EXP VECTOR 2:  %ld\n", label2->getShortExpVec());
 
   return id;
 }
 
 #endif
-// HAVE_F5C
+// HAVE_F5
