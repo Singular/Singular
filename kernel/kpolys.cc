11fe052c40e708824ad3ca7806ddd8b1b4d6ffe6
Burcin Erocal <burcin@erocal.org>
2011-07-20 13:34:49 +0200

    Convert kernel/kpolys.cc to use libpolys, some pGetComp calls remaining.

--- kernel/kpolys.cc
+++ kernel/kpolys.cc
@@ -7,50 +7,50 @@
          * LE(p, j)  != LE(lcm, j)
          * LE(p2, j) != LE(lcm, j)   ==> LCM(p2, p) != lcm
 */
-BOOLEAN pCompareChain (poly p,poly p1,poly p2,poly lcm)
+BOOLEAN pCompareChain (poly p,poly p1,poly p2,poly lcm, const ring R = currRing)
 {
   int k, j;
 
   if (lcm==NULL) return FALSE;
 
-  for (j=(currRing->N); j; j--)
-    if ( pGetExp(p,j) >  pGetExp(lcm,j)) return FALSE;
+  for (j=(R->N); j; j--)
+    if ( p_GetExp(p,j, R) >  p_GetExp(lcm,j, R)) return FALSE;
   if ( pGetComp(p) !=  pGetComp(lcm)) return FALSE;
-  for (j=(currRing->N); j; j--)
+  for (j=(R->N); j; j--)
   {
-    if (pGetExp(p1,j)!=pGetExp(lcm,j))
+    if (p_GetExp(p1,j, R)!=p_GetExp(lcm,j, R))
     {
-      if (pGetExp(p,j)!=pGetExp(lcm,j))
+      if (p_GetExp(p,j, R)!=p_GetExp(lcm,j, R))
       {
-        for (k=(currRing->N); k>j; k--)
+        for (k=(R->N); k>j; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p2,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p2,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
         for (k=j-1; k; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p2,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p2,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
         return FALSE;
       }
     }
-    else if (pGetExp(p2,j)!=pGetExp(lcm,j))
+    else if (p_GetExp(p2,j, R)!=p_GetExp(lcm,j, R))
     {
-      if (pGetExp(p,j)!=pGetExp(lcm,j))
+      if (p_GetExp(p,j, R)!=p_GetExp(lcm,j, R))
       {
-        for (k=(currRing->N); k>j; k--)
+        for (k=(R->N); k>j; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p1,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p1,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
         for (k=j-1; k!=0 ; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p1,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p1,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
         return FALSE;
@@ -60,51 +60,52 @@ BOOLEAN pCompareChain (poly p,poly p1,poly p2,poly lcm)
   return FALSE;
 }
 #ifdef HAVE_RATGRING
-BOOLEAN pCompareChainPart (poly p,poly p1,poly p2,poly lcm)
+BOOLEAN pCompareChainPart (poly p,poly p1,poly p2,poly lcm,
+		const ring R = currRing)
 {
   int k, j;
 
   if (lcm==NULL) return FALSE;
 
-  for (j=currRing->real_var_end; j>=currRing->real_var_start; j--)
-    if ( pGetExp(p,j) >  pGetExp(lcm,j)) return FALSE;
+  for (j=R->real_var_end; j>=R->real_var_start; j--)
+    if ( p_GetExp(p,j, R) >  p_GetExp(lcm,j, R)) return FALSE;
   if ( pGetComp(p) !=  pGetComp(lcm)) return FALSE;
-  for (j=currRing->real_var_end; j>=currRing->real_var_start; j--)
+  for (j=R->real_var_end; j>=R->real_var_start; j--)
   {
-    if (pGetExp(p1,j)!=pGetExp(lcm,j))
+    if (p_GetExp(p1,j, R)!=p_GetExp(lcm,j, R))
     {
-      if (pGetExp(p,j)!=pGetExp(lcm,j))
+      if (p_GetExp(p,j, R)!=p_GetExp(lcm,j, R))
       {
-        for (k=(currRing->N); k>j; k--)
-        for (k=currRing->real_var_end; k>j; k--)
+        for (k=(R->N); k>j; k--)
+        for (k=R->real_var_end; k>j; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p2,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p2,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
-        for (k=j-1; k>=currRing->real_var_start; k--)
+        for (k=j-1; k>=R->real_var_start; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p2,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p2,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
         return FALSE;
       }
     }
-    else if (pGetExp(p2,j)!=pGetExp(lcm,j))
+    else if (p_GetExp(p2,j, R)!=p_GetExp(lcm,j, R))
     {
-      if (pGetExp(p,j)!=pGetExp(lcm,j))
+      if (p_GetExp(p,j, R)!=p_GetExp(lcm,j, R))
       {
-        for (k=currRing->real_var_end; k>j; k--)
+        for (k=R->real_var_end; k>j; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p1,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p1,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
-        for (k=j-1; k>=currRing->real_var_start; k--)
+        for (k=j-1; k>=R->real_var_start; k--)
         {
-          if ((pGetExp(p,k)!=pGetExp(lcm,k))
-          && (pGetExp(p1,k)!=pGetExp(lcm,k)))
+          if ((p_GetExp(p,k, R)!=p_GetExp(lcm,k, R))
+          && (p_GetExp(p1,k, R)!=p_GetExp(lcm,k, R)))
             return TRUE;
         }
         return FALSE;
