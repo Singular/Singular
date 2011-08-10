4a0124b7d889e444b5fa3d7ea1350a452f8f5d77
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2011-08-10 14:36:25 +0200

    currRingHdl deklared for all in ipid.h

--- Singular/subexpr.cc
+++ Singular/subexpr.cc
@@ -145,7 +145,7 @@ void sleftv::Print(leftv store, int spaces)
         case RESOLUTION_CMD:
         {
           syStrategy tmp=(syStrategy)d;
-          syPrint(tmp);
+          syPrint(tmp,IDID(currRingHdl));
           break;
         }
         case STRING_CMD:
