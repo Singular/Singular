96dc0d38972e4288bab5af2595c3c5174db8cb62
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-08-11 15:21:35 +0200

    ADD: expose feStringAppendBrowsers (moved to Singular/fehelp.*)

--- Singular/fehelp.h
+++ Singular/fehelp.h
@@ -1,7 +1,24 @@
 #ifndef FEHELP_H
 #define FEHELP_H
 
+
+/*****************************************************************
+ *
+ * help system (fehelp.cc)
+ *
+ *****************************************************************/
+// if str != NULL display help for str
+// display general help, otherwise
 void feHelp(char* str = NULL);
-const char* feHelpBrowser(char* which=NULL, int warn=-1);
+// if browser != NULL or feOpt("browser") != NULL
+//    set HelpBrowser to browser
+// otherwise, if browser was already set, leave as is,
+//            if not, choose first available browser
+// return string identifying current browser
+// keeps feOpt("browser") up-to-date
+// Optional warn argument is as in feResource
+const char* feHelpBrowser(char* browser = NULL, int warn = -1);
+
+void feStringAppendBrowsers(int warn = -1);
 
 #endif /*  FEHELP_H */
--- kernel/old.febase.h
+++ kernel/old.febase.h
@@ -211,23 +211,6 @@ enum   feBufferInputs
   BI_file
 };
 
-/*****************************************************************
- *
- * help system (fehelp.cc)
- *
- *****************************************************************/
-// if str != NULL display help for str
-// display general help, otherwise
-void feHelp(char* str = NULL);
-// if browser != NULL or feOpt("browser") != NULL
-//    set HelpBrowser to browser
-// otherwise, if browser was already set, leave as is,
-//            if not, choose first available browser
-// return string identifying current browser
-// keeps feOpt("browser") up-to-date
-// Optional warn argument is as in feResource
-const char* feHelpBrowser(char* browser = NULL, int warn = -1);
-void feStringAppendBrowsers(int warn = -1);
 
 /*****************************************************************
  *
