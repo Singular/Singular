f75197d1eabae631a5a4c9ee5f8e55a9c3448e05
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-08-16 19:36:36 +0200

    ADD: build test/testg out of test.cc and run them during 'make check' ADD: libSingular*.a ADD: Singular/Singular_g need corresp. testgh.o/.og and libSingular.a/_g.a CHG: better makefile

--- Singular/test.cc
+++ Singular/test.cc
@@ -1,3 +1,191 @@
+#include <misc/auxiliary.h>
+
+#ifdef HAVE_FACTORY
+#include <factory/factory.h> // :(
+#endif
+
+#include <omalloc/omalloc.h>
+
+#include <misc/intvec.h>
+#include <misc/int64vec.h>
+#include <misc/mylimits.h>
+#include <misc/options.h>
+
+#include <reporter/reporter.h>
+
+#include <resources/feFopen.h>
+#include <resources/feResource.h>
+
+#include <coeffs/coeffs.h>
+
+#include <coeffs/si_gmp.h>
+
+#include <polys/kbuckets.h>
+#include <polys/matpol.h>
+#include <polys/mod_raw.h>
+#include <polys/prCopy.h>
+#include <polys/sbuckets.h>
+#include <polys/simpleideals.h>
+#include <polys/weight.h>
+
+#include <polys/monomials/maps.h>
+#include <polys/monomials/monomials.h>
+#include <polys/monomials/p_polys.h>
+#include <polys/monomials/ring.h>
+
+#include <polys/nc/nc.h>
+#include <polys/nc/ncSACache.h>
+#include <polys/nc/ncSAFormula.h>
+#include <polys/nc/ncSAMult.h>
+#include <polys/nc/sca.h>
+#include <polys/nc/summator.h>
+
+
+#include <polys/templates/p_MemAdd.h>
+#include <polys/templates/p_Procs.h>
+
+#include <polys/operations/pShallowCopyDelete.h>
+
+#include <polys/clapsing.h>
+
+
+#ifdef HAVE_FACTORY
+int initializeGMP(){ return 1; } // NEEDED FOR MAIN APP. LINKING!!!
+#endif
+
+
+#include <coeffs/numbers.h>
+#include <polys/polys.h>
+
+
+#include <kernel/mod2.h>
+
+#include <kernel/structs.h>
+
+
+// HEADERS:
+#include <kernel/hutil.h>
+//#include "idrec.h" // moved to Singular
+#include <kernel/stairc.h>
+#include <kernel/ideals.h>
+#include <kernel/syz.h>
+#include <kernel/fast_maps.h>
+#include <kernel/febase.h>
+#include <kernel/walkProc.h>
+#include <kernel/walkMain.h>
+#include <kernel/walkSupport.h>
+#include <kernel/khstd.h>
+/// #include <kernel/sparsmat.h> // TODO: install polys/this!
+//+
+
+#include <kernel/fglm.h>
+#include <kernel/kstd1.h>
+#include <kernel/fglmgauss.h>
+#include <kernel/fglmvec.h>
+#include <kernel/kstdfac.h>
+#include <kernel/kmatrix.h>
+#include <kernel/GMPrat.h>
+#include <kernel/multicnt.h>
+#include <kernel/npolygon.h>
+#include <kernel/semic.h>
+#include <kernel/spectrum.h>
+#include <kernel/splist.h>
+#include <kernel/multicnt.h>
+#include <kernel/eigenval.h>
+#include <kernel/units.h>
+#include <kernel/fegetopt.h>
+#include <kernel/ratgring.h>
+#include <kernel/shiftgb.h>
+#include <kernel/mmalloc.h>
+#include <kernel/gfan.h>
+
+#include <kernel/kutil.h>
+
+// #include <kernel/dbm_sl.h> // TODO: needs si_link// already moved to Singular/!
+
+// #include "CCRing.h" // Too old!
+#include <kernel/digitech.h>
+#include <kernel/eigenval.h>
+#include <kernel/fast_maps.h>
+#include <kernel/fast_mult.h>
+#include <kernel/febase.h>
+#include <kernel/fegetopt.h>
+
+#include <kernel/fglmgauss.h>
+#include <kernel/fglm.h>
+#include <kernel/fglmvec.h>
+
+////////#include "F5cData.h"
+#include <kernel/f5c.h>
+#include <kernel/f5data.h>
+#include <kernel/f5gb.h>
+#include <kernel/f5lists.h>
+////////#include <kernel/F5cLists.h>
+
+#include <kernel/gfan.h>
+
+
+#include <kernel/GMPrat.h>
+
+// #include "htmlhelp.h" // For Windows // 
+#include <kernel/hutil.h>
+// #include <kernel/Ideal.h> // Too old?
+
+
+#include <kernel/ideals.h>
+
+#include <kernel/kmatrix.h>
+#include <kernel/kstd1.h>
+#include <kernel/kstdfac.h>
+#include <kernel/khstd.h>
+
+#include <kernel/linearAlgebra.h>
+
+
+
+// #include "lplist.h" // Too old!
+#include <kernel/mmalloc.h>
+#include <kernel/multicnt.h>
+#include <kernel/npolygon.h>
+// #include <kernel/Number.h> // Too old?
+#include <kernel/page.h>
+// #include <kernel/Poly.h> // Too old?
+// #include <kernel/PowerSeries.h> // Too old?
+
+#include <kernel/preimage.h>
+
+#include <kernel/nc.h>
+
+#include <kernel/ratgring.h>
+#include <kernel/ringgb.h>
+#include <kernel/semic.h>
+#include <kernel/shiftgb.h>
+
+#include <kernel/spectrum.h>
+#include <kernel/splist.h>
+#include <kernel/stairc.h>
+#include <kernel/structs.h>
+#include <kernel/syz.h>
+// #include <kernel/testpoly.h> // Too old?
+
+#include <kernel/tgbgauss.h>
+#include <kernel/tgb.h>
+
+#include <kernel/timer.h>
+
+#include <kernel/units.h>
+#include <kernel/walkMain.h>
+#include <kernel/walkProc.h>
+#include <kernel/walkSupport.h>
+
+
+// #include <polys/clapconv.h> // due to factory? :(
+// #include <kernel/tgb_internal.h> // :(
+// #include <kernel/F4.h> // uses tgb_internal // :(
+// #include <kernel/IIntvec.h> // :(
+
+
+
 // headers in Singular/
 #include "attrib.h"
 #include "bbcone.h"
@@ -56,7 +244,7 @@
 
 
 // Source files in Singular/
-
+/*
 #include "attrib.cc"
 #include "bbcone.cc"
 #include "bbfan.cc"
@@ -124,3 +312,16 @@
 #include "walk.cc"
 #include "walk_ip.cc"
 #include "wrapper.cc"
+*/
+
+int main( int, char *argv[] ) 
+{
+  feInitResources(argv[0]);
+
+  StringSetS("ressources in use (as reported by feStringAppendResources(0):\n");
+  feStringAppendResources(0);
+  PrintS(StringAppendS("\n"));
+   
+  return 0;
+}
+
