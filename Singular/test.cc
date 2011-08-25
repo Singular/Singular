8aa03b2badd649c49b82f40c89165d11cee9fc4c
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-08-25 18:36:56 +0200

    ADD: improving tests

--- Singular/test.cc
+++ Singular/test.cc
@@ -322,6 +322,43 @@ int main( int, char *argv[] )
   StringSetS("ressources in use (as reported by feStringAppendResources(0):\n");
   feStringAppendResources(0);
   PrintS(StringAppendS("\n"));
+
+   
+   
+  // Libpolys tests:
+   
+  // construct the ring Z/32003[x,y,z]
+  // the variable names
+  char **n=(char**)omalloc(3*sizeof(char*));
+  n[0]=omStrDup("x");
+  n[1]=omStrDup("y");
+  n[2]=omStrDup("z2");
+
+  ring R=rDefault(32003,3,n);
+  // make R the default ring:
+  rChangeCurrRing(R);
+
+  // create the polynomial 1
+  poly p1=pISet(1);
+
+  // create tthe polynomial 2*x^3*z^2
+  poly p2=p_ISet(2,R);
+  pSetExp(p2,1,3);
+  pSetExp(p2,3,2);
+  pSetm(p2);
+
+  // print p1 + p2
+  pWrite(p1); printf(" + \n"); pWrite(p2); printf("\n");
+
+  // compute p1+p2
+  p1=p_Add_q(p1,p2,R); p2=NULL;
+  pWrite(p1); 
+
+  // clean up:
+  pDelete(&p1);
+   
+  rDelete(R);
+   
    
   return 0;
 }
