d8943718fef8cc5c08528be5fe63c8682ee71700
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-09-14 14:23:10 +0200

    ADD: simple tests for p_Totaldegree

--- kernel/test.cc
+++ kernel/test.cc
@@ -226,8 +226,10 @@ int main( int, char *argv[] )
   
   poly pp = pp_Mult_qq( p, p, R);
 
-  Print("p: "); p_Write0(p, R);
-  Print("; p*p : "); p_Write(pp, R); 
+  Print("p: "); p_Write0(p, R); Print(", deg(p): %d", p_Totaldegree(p, R)); assume( 1 == p_Totaldegree(p, R) );
+
+  Print("; p*p : "); p_Write0(pp, R); Print("deg(pp): %d\n", p_Totaldegree(pp, R)); assume( 2 == p_Totaldegree(pp, R) );
+
 
   p_Delete(&p, R);
   
