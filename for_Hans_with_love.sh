ab788d2fa2a3a3cfadef40aeb1619e81ac889f17
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-10-11 19:43:29 +0200

    CHG: minor build system improvements

--- for_Hans_with_love.sh
+++ for_Hans_with_love.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-autotools (){
-autoheader
-autoconf
-automake -a
-}
-
 TOP_DIR=$PWD
 
-for d in . libpolys factory; do
+autotools ()
+{
+  aclocal -I $TOP_DIR/m4
+  autoheader
+  autoconf
+  automake -a
+}
+
+
+for d in . factory libpolys; do
   echo "==============="
   echo "run libtoolize --force, aclocal -I $TOP_DIR/m4, autoconf, automake -a in $d"
   echo "---------------"
-  cd $d;
+  cd $d
   libtoolize --force
-  aclocal -I $TOP_DIR/m4
   autotools
   cd $TOP_DIR
   echo
@@ -24,8 +26,7 @@ for d in omalloc; do
   echo "==============="
   echo "run aclocal -I $TOP_DIR/m4, autotools in $d"
   echo "---------------"
-  cd $d;
-  aclocal -I $TOP_DIR/m4
+  cd $d
   autotools
   cd $TOP_DIR
   echo
