b184aa06942a5772e2b2dadf344a6531db932976
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-10-13 19:02:58 +0200

    ADD: one can use autoreconf, for instance as via autogen.sh for generating a buildsystem RM: INSTALL/mkinstalldirs/install-sh are generated automatically by autotools CHG: please use autogen.sh instead of for_Hans_with_love.sh

--- for_Hans_with_love.sh
+++ for_Hans_with_love.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 
-TOP_DIR=$PWD
+echo "NOTE: This script is obsolete! Please use autogen.sh instead."
 
-autotools ()
-{
-  aclocal -I $TOP_DIR/m4
-  autoheader
-  autoconf
-  automake -a
-}
-
-
-for d in . factory libpolys; do
-  echo "==============="
-  echo "run libtoolize --force, aclocal -I $TOP_DIR/m4, autoconf, automake -a in $d"
-  echo "---------------"
-  cd $d
-  libtoolize --force
-  autotools
-  cd $TOP_DIR
-  echo
-done
-
-for d in omalloc; do
-  echo "==============="
-  echo "run aclocal -I $TOP_DIR/m4, autotools in $d"
-  echo "---------------"
-  cd $d
-  autotools
-  cd $TOP_DIR
-  echo
-done
+./autogen.sh
