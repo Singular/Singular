21242c05ebf3f3a97a83383ee686f192d0fe85ae
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2011-10-14 16:42:32 +0200

    fix tr. 359

--- Singular/iparith.cc
+++ Singular/iparith.cc
@@ -6041,9 +6041,16 @@ static BOOLEAN jjSUBST_Id_X(leftv res, leftv u, leftv v,leftv w, int input_type)
 }
 static BOOLEAN jjMATRIX_Id(leftv res, leftv u, leftv v,leftv w)
 {
-  matrix m=mpNew((int)(long)v->Data(),(int)(long)w->Data());
+  int mi=(int)(long)v->Data();
+  int ni=(int)(long)w->Data();
+  if ((mi<1)||(ni<1))
+  {
+    WerrorS("matrix dimensions must be positive");
+    return TRUE;
+  }
+  matrix m=mpNew(mi,ni);
   ideal I=(ideal)u->CopyD(IDEAL_CMD);
-  int i=si_min(IDELEMS(I),(int)(long)v->Data()*(int)(long)w->Data());
+  int i=si_min(IDELEMS(I),mi*ni);
   //for(i=i-1;i>=0;i--)
   //{
   //  m->m[i]=I->m[i];
@@ -6051,22 +6058,36 @@ static BOOLEAN jjMATRIX_Id(leftv res, leftv u, leftv v,leftv w)
   //}
   memcpy(m->m,I->m,i*sizeof(poly));
   memset(I->m,0,i*sizeof(poly));
-  idDelete(&I);
+  id_Delete(&I,currRing);
   res->data = (char *)m;
   return FALSE;
 }
 static BOOLEAN jjMATRIX_Mo(leftv res, leftv u, leftv v,leftv w)
 {
+  int mi=(int)(long)v->Data();
+  int ni=(int)(long)w->Data();
+  if ((mi<1)||(ni<1))
+  {
+    WerrorS("matrix dimensions must be positive");
+    return TRUE;
+  }
   res->data = (char *)id_Module2formatedMatrix((ideal)u->CopyD(MODUL_CMD),
-           (int)(long)v->Data(),(int)(long)w->Data(),currRing);
+           mi,ni,currRing);
   return FALSE;
 }
 static BOOLEAN jjMATRIX_Ma(leftv res, leftv u, leftv v,leftv w)
 {
-  matrix m=mpNew((int)(long)v->Data(),(int)(long)w->Data());
+  int mi=(int)(long)v->Data();
+  int ni=(int)(long)w->Data();
+  if ((mi<1)||(ni<1))
+  {
+    WerrorS("matrix dimensions must be positive");
+    return TRUE;
+  }
+  matrix m=mpNew(mi,ni);
   matrix I=(matrix)u->CopyD(MATRIX_CMD);
-  int r=si_min(MATROWS(I),(int)(long)v->Data());
-  int c=si_min(MATCOLS(I),(int)(long)w->Data());
+  int r=si_min(MATROWS(I),mi);
+  int c=si_min(MATCOLS(I),ni);
   int i,j;
   for(i=r;i>0;i--)
   {
@@ -6076,7 +6097,7 @@ static BOOLEAN jjMATRIX_Ma(leftv res, leftv u, leftv v,leftv w)
       MATELEM(I,i,j)=NULL;
     }
   }
-  idDelete((ideal *)&I);
+  id_Delete((ideal *)&I,currRing);
   res->data = (char *)m;
   return FALSE;
 }
