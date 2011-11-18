444bcfc5c67574ab5371ac1f5780ce53fefd9537
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2011-11-18 23:34:37 +0100

    CHG: minor cleanup ADD: original Hans's test for standalone testing: tt.cc FIX: typo

---
+++ standalone.test/tt.cc
@@ -0,0 +1,55 @@
+// Example program how to use libsingular
+
+#include <Singular/libsingular.h>
+#include <unistd.h>
+
+#ifdef HAVE_FACTORY
+int mmInit(void) {return 1; } // ? due to SINGULAR!!!...???
+#endif
+
+
+int main( int, char *argv[] ) 
+{
+  feInitResources(argv[0]);
+
+  StringSetS("ressources in use (as reported by feStringAppendResources(0):\n");
+  feStringAppendResources(0);
+  PrintS(StringAppendS("\n"));
+
+//  // init path names etc.
+//  siInit(argv[0]);
+
+
+  // construct the ring Z/32003[x,y,z]
+  // the variable names
+  char **n=(char**)omalloc(3*sizeof(char*));
+  n[0]=omStrDup("x");
+  n[1]=omStrDup("y");
+  n[2]=omStrDup("z2");
+
+  ring R=rDefault(32003,3,n);
+  // make R the default ring:
+  rChangeCurrRing(R);
+
+  // create the polynomial 1
+  poly p1=p_ISet(1,R);
+
+  // create tthe polynomial 2*x^3*z^2
+  poly p2=p_ISet(2,R);
+  pSetExp(p2,1,3);
+  pSetExp(p2,3,2);
+  pSetm(p2);
+
+  // print p1 + p2
+  pWrite(p1); printf(" + \n"); pWrite(p2); printf("\n");
+
+  // compute p1+p2
+  p1=p_Add_q(p1,p2,R); p2=NULL;
+  pWrite(p1); 
+
+//   sleep(120);
+
+  // cleanup:
+  pDelete(&p1);
+  rKill(R);
+}
