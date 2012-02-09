e8bf2c0486c61c941222768bad1167994d78dd2d
Martin Lee <martinlee84@web.de>
2012-02-09 15:30:08 +0100

    chg: template instantiation is now part of *.a

--- factory/ftmpl_inst.cc
+++ factory/ftmpl_inst.cc
@@ -10,15 +10,15 @@
 //
 //}}}
 
-#include <factoryconf.h>
-
 #include <templates/ftmpl_array.cc>
 #include <templates/ftmpl_factor.cc>
 #include <templates/ftmpl_list.cc>
 #include <templates/ftmpl_functions.h>
 #include <templates/ftmpl_matrix.cc>
 
-#include <factory.h>
+#include <canonicalform.h>
+#include <cf_map.h>
+#include <cf_reval.h>
 
 
 //{{{ explicit template class instantiations
