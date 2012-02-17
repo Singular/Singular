e5c56c706f58fb6c25e5df2139f1aaf87a1e9326
Martin Lee <martinlee84@web.de>
2012-02-17 11:05:30 +0100

    chg: added docu for conversion to FLINT and back

--- factory/FLINTconvert.h
+++ factory/FLINTconvert.h
@@ -1,3 +1,16 @@
+/*****************************************************************************\
+ * Computer Algebra System SINGULAR
+\*****************************************************************************/
+/** @file FLINTconvert.h
+ *
+ * This file defines functions for conversion to FLINT (www.flintlib.org)
+ * and back.
+ *
+ * @author Martin Lee
+ *
+ **/
+/*****************************************************************************/
+
 #ifndef FLINT_CONVERT_H
 #define FLINT_CONVERT_H
 
@@ -18,19 +31,77 @@ extern "C"
 }
 #endif
 
-void convertCF2Fmpz (fmpz_t result, const CanonicalForm& f);
-void convertFacCF2Fmpz_poly_t (fmpz_poly_t result, const CanonicalForm& f);
-CanonicalForm convertFmpz2CF (fmpz_t coefficient);
-CanonicalForm convertFmpz_poly_t2FacCF (fmpz_poly_t poly, const Variable& x);
-void convertFacCF2nmod_poly_t (nmod_poly_t result, const CanonicalForm& f);
-CanonicalForm convertnmod_poly_t2FacCF (nmod_poly_t poly, const Variable& x);
-void convertCF2Fmpq (fmpq_t result, const CanonicalForm& f);
-CanonicalForm convertFmpq_poly_t2FacCF (fmpq_poly_t p, const Variable& x);
-void convertFacCF2Fmpq_poly_t (fmpq_poly_t result, const CanonicalForm& f);
-CFFList convertFLINTnmod_poly_factor2FacCFFList (nmod_poly_factor_t fac,
-                                                  mp_limb_t leadingCoeff,
-                                                  const Variable& x
-                                                 );
+/// conversion of a factory integer to fmpz_t
+void
+convertCF2Fmpz (fmpz_t result,         ///< [in,out] an fmpz_t
+                const CanonicalForm& f ///< [in] a CanonicalForm wrapping an
+                                       ///< integer
+               );
+
+/// conversion of a factory univariate polynomial over Z to a fmpz_poly_t
+void
+convertFacCF2Fmpz_poly_t (fmpz_poly_t result,    ///< [in,out] an fmpz_poly_t
+                          const CanonicalForm& f ///< [in] univariate poly over
+                                                 ///< Z
+                         );
+
+/// conversion of a FLINT integer to CanonicalForm
+CanonicalForm
+convertFmpz2CF (fmpz_t coefficient ///< [in] a FLINT integer
+               );
+
+/// conversion of a FLINT poly over Z to CanonicalForm
+CanonicalForm
+convertFmpz_poly_t2FacCF (fmpz_poly_t poly, ///< [in] an fmpz_poly_t
+                          const Variable& x ///< [in] variable the result should
+                                            ///< have
+                         );
+
+/// conversion of a factory univariate polynomials over Z/p (for word size p)
+/// to nmod_poly_t
+void
+convertFacCF2nmod_poly_t (nmod_poly_t result,    ///< [in, out] a nmod_poly_t
+                          const CanonicalForm& f ///< [in] univariate poly over
+                                                 ///< Z/p
+                         );
+
+/// conversion of a FLINT poly over Z/p to CanonicalForm
+CanonicalForm
+convertnmod_poly_t2FacCF (nmod_poly_t poly, ///< [in] a nmod_poly_t
+                          const Variable& x ///< [in] variable the result should
+                                            ///< have
+                         );
+
+/// conversion of a factory rationals to fmpq_t
+void
+convertCF2Fmpq (fmpq_t result,         ///< [in,out] an fmpq_t
+                const CanonicalForm& f ///< [in] a CanonicalForm wrapping a
+                                       ///< rational
+               );
+
+/// conversion of a factory univariate polynomials over Q to fmpq_poly_t
+void
+convertFacCF2Fmpq_poly_t (fmpq_poly_t result,    ///< [in,out] an fmpq_poly_t
+                          const CanonicalForm& f ///< [in] univariate poly over
+                                                 ///< Q
+                         );
+
+/// conversion of a FLINT poly over Q to CanonicalForm
+CanonicalForm
+convertFmpq_poly_t2FacCF (fmpq_poly_t p,    ///< [in] an fmpq_poly_t
+                          const Variable& x ///< [in] variable the result should
+                                            ///< have
+                         );
+
+/// conversion of a FLINT factorization over Z/p (for word size p) to a
+/// CFFList
+CFFList
+convertFLINTnmod_poly_factor2FacCFFList (
+                   nmod_poly_factor_t fac, ///< [in] a nmod_poly_factor_t
+                   mp_limb_t leadingCoeff, ///< [in] leading coefficient
+                   const Variable& x       ///< [in] variable the result should
+                                           ///< have
+                                        );
 
 #endif
 #endif
