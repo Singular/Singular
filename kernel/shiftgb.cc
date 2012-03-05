f82aa44bf4157b055f82aad15c6855d340458c9e
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2012-03-05 10:40:55 +0100

    fix: shiftgb::p_mLPshift by Benjamin Schnitzler

--- kernel/shiftgb.cc
+++ kernel/shiftgb.cc
@@ -148,7 +148,7 @@ poly p_mLPshift(poly p, int sh, int uptodeg, int lV, const ring r)
   //  number c = pGetCoeff(p);
   //  p_SetCoeff0(m,p_GetCoeff(p,r),r);
   p_SetComp(m,p_GetComp(p,r),r); // component is preserved
-  p_SetCoeff0(m,p_GetCoeff(p,r),r);  // coeff is preserved
+  p_SetCoeff0(m,n_Copy(p_GetCoeff(p,r),r),r);  // coeff is preserved
   return(m);
 }
 
