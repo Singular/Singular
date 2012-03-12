b6de72d910836e737f2b2f556d57560c5be3e9d9
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2012-03-12 13:09:01 +0100

    fix: conflicting types

--- Singular/simpleipc.h
+++ Singular/simpleipc.h
@@ -11,10 +11,10 @@ extern "C"
 
 int sipc_semaphore_init(int id, int count);
 int sipc_semaphore_exists(int id);
-void sipc_semaphore_acquire(int id);
+int sipc_semaphore_acquire(int id);
 int sipc_semaphore_try_acquire(int id);
 int sipc_semaphore_get_value(int id);
-void sipc_semaphore_release(int id);
+int sipc_semaphore_release(int id);
 
 int simpleipc_cmd(char *cmd, int id, int v);
 #ifdef __cplusplus
