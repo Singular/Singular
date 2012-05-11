0d1984ff9810b1f329e5aea72c3390fe48bc5e63
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2012-05-11 15:51:24 +0200

    better factory test (for 'make check')
    add: output test parameters: p,i
    chg: no immediate exit on failed test
    add: accumulate the return value

--- factory/test.cc
+++ factory/test.cc
@@ -23,7 +23,9 @@ return 1;
 
 int test2 (int p)
 {
+  int ret = 1;
   setCharacteristic (p);
+  printf ("p: %d, i: %d", p, 0);
   CanonicalForm d1= Variable (1) + Variable (2) + 1;
   CanonicalForm f1= Variable (1) - Variable (2) - 2;
   CanonicalForm g1= Variable (1) + Variable (2) + 2;
@@ -32,11 +34,12 @@ int test2 (int p)
   CanonicalForm g= d* power (g1, 2);
   CanonicalForm h= gcd (f,g);
   h /= Lc (h);
+  printf (", h==d? %d\n", (h == d));
   if (h != d)
-    return -1;
-  printf ("h==d? %d\n", (h == d));
+    ret = -1;
   for (int i= 3; i <= 11; i++)
   {
+    printf ("p: %d, i: %d", p, i);
     d1 += power (Variable (i), i);
     f1 -= power (Variable (i), i);
     g1 += power (Variable (i), i);
@@ -45,11 +48,11 @@ int test2 (int p)
     g= d*power (g1, 2);
     h= gcd (f,g);
     h /= Lc (h);
+    printf (", h==d? %d\n", (h == d));
     if (h != d)
-      return -1;
-    printf ("h==d? %d\n", (h == d));
+      ret = -1;
   }
-  return 1;
+  return ret;
 }
 
 /*int test5 (int p)
@@ -96,11 +99,15 @@ extern void feInitResources(const char* argv0 = NULL);
 
 int main( int, char *argv[] )
 {
+  int ret = 0;
+  
   feInitResources(argv[0]);
-   
+
+//  On (SW_USE_EZGCD); On (SW_USE_EZGCD_P); // TODO&NOTE: these switches lead to failed tests (with nonzero p)!
+  
   int t= test2 (0);
   if (t < 0)
-    return t;
+    ret = t;
   /*t= test5 (0);
   if (t < 0)
     return t;
@@ -115,12 +122,15 @@ int main( int, char *argv[] )
     return t;*/
   t= test2 (3);
   if (t < 0)
-    return t;
+    ret += t;
+  
   t= test2 (101);
   if (t < 0)
-    return t;
+    ret += t;
+  
   t= test2 (43051);
   if (t < 0)
-    return t;
-  return 0;
+    ret += t;
+  
+  return ret;
 }
