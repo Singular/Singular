e9927ff3cc13ebaddd8a4a7419c26390ff895c18
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2012-10-08 16:31:53 +0200

    fix: length computation in sLObject::Tail_Minus_mm_Mult_qq

--- kernel/kInline.h
+++ kernel/kInline.h
@@ -593,8 +593,10 @@ KINLINE void sLObject::Tail_Minus_mm_Mult_qq(poly m, poly q, int lq,
     poly _p = (t_p != NULL ? t_p : p);
     assume(_p != NULL);
 
-    pNext(_p) = p_Minus_mm_Mult_qq( pNext(_p), m, q, pLength, lq, 
+    int lp=pLength-1;
+    pNext(_p) = p_Minus_mm_Mult_qq( pNext(_p), m, q, lp, lq, 
                                     spNoether, tailRing );
+    pLength=lp+1;
 //    tailRing->p_Procs->p_Minus_mm_Mult_qq(pNext(_p), m, q, shorter,spNoether, tailRing, last);
 //    pLength += lq - shorter;
   }
