5caede8b6a6317efbf12384a421e0c5b35f8838d
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2012-11-02 14:28:44 +0100

    fix: int overflow in hash function

--- Singular/links/ndbm.cc
+++ Singular/links/ndbm.cc
@@ -427,7 +427,7 @@ static  int hitab[16]
  = {    61, 57, 53, 49, 45, 41, 37, 33,
         29, 25, 21, 17, 13,  9,  5,  1,
 };
-static  long hltab[64]
+static unsigned  long hltab[64]
  = {
         06100151277L,06106161736L,06452611562L,05001724107L,
         02614772546L,04120731531L,04665262210L,07347467531L,
@@ -451,8 +451,8 @@ static long dcalchash(datum item)
 {
   register int s, c, j;
   register char *cp;
-  register long hashl;
-  register int hashi;
+  register unsigned long hashl;
+  register unsigned int hashi;
 
   hashl = 0;
   hashi = 0;
@@ -466,7 +466,7 @@ static long dcalchash(datum item)
       c >>= 4;
     }
   }
-  return (hashl);
+  return (long)(hashl);
 }
 
 /*
