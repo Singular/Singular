b681a13f3e6750c57cc09046b6ac79ded5c57947
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2012-11-13 14:17:55 +0100

    chg: minor cleanup

--- kernel/kutil.cc
+++ kernel/kutil.cc
@@ -2911,7 +2911,6 @@ void initenterstrongPairs (poly h,int k,int ecart,int isFromQ,kStrategy strat, i
   if (!nIsOne(pGetCoeff(h)))
   {
     int j;
-    BOOLEAN new_pair=FALSE;
 
     for (j=0; j<=k; j++)
     {
@@ -2920,10 +2919,7 @@ void initenterstrongPairs (poly h,int k,int ecart,int isFromQ,kStrategy strat, i
 //         ((unsigned long) pGetCoeff(strat->S[j]) % (unsigned long) pGetCoeff(h) != 0))
       if ( iCompH == pGetComp(strat->S[j]) )
       {
-        {
-          if (enterOneStrongPoly(j,h,ecart,isFromQ,strat, atR))
-            new_pair=TRUE;
-        }
+        enterOneStrongPoly(j,h,ecart,isFromQ,strat, atR);
       }
     }
   }
