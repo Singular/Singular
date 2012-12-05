80d7722e6833c629fe2c07881a5d23b7c4a9b4e4
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2012-12-05 15:14:46 +0100

    fix: mylimits.h

--- libpolys/misc/mylimits.h
+++ libpolys/misc/mylimits.h
@@ -8,30 +8,7 @@
 #ifndef _MYLIMITS_H
 #define _MYLIMITS_H
 
-/* Maximum/minimum value an `signed int' can hold. */
-// #define MAX_INT_VAL        2147483647
-#ifndef MAX_INT_VAL
-#define MAX_INT_VAL     (2147483647)
-#endif
-
-#ifndef MIN_INT_VAL
-#define MIN_INT_VAL     (-MAX_INT_VAL-1)
-#endif
-
-
-// #define ULONG_MAX (~0UL) // ??
-#ifndef ULONG_MAX
-#define ULONG_MAX (~0UL)
-#endif
-
-// TODO: fixing the following BUG (!?) leads to wrong ring::bitmask
-// #if ~0UL != 4294967295UL
-#ifndef LONG_MAX
-#if ULONG_MAX != 4294967295UL
-#define LONG_MAX 9223372036854775807L
-#else
-#define LONG_MAX 2147483647L
-#endif
-#endif
+#include <limits.h>
+#define MAX_INT_VAL 0x7fffffff
 
 #endif /* _MYLIMITS_H */
