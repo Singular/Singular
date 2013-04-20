56695318df823daedacdd605bab7071dd387123a
Grischa Studzinski <grischa.studzinski@math.rwth-aachen.de>
2013-04-20 16:15:47 +0200

    second try

--- kernel/SDLeftGB.h
+++ kernel/SDLeftGB.h
@@ -13,11 +13,11 @@ namespace ShiftDVec::LeftGB
 
   void initenterpairs
     ( LObject* J,
-      int ecart, int isFromQ, kStrategy strat, int atR );
+      int ecart, int isFromQ, SD_kStrat strat, int atR );
 
   void GebauerMoeller
     ( uint** overlaps,
-      uint* sizesOvls, LObject* J, kStrategy strat );
+      uint* sizesOvls, LObject* J, SD_kStrat strat );
 
   void GMFilter
     ( LObject* J, TObject* p_i, TObject* p_k,
@@ -32,7 +32,7 @@ namespace ShiftDVec::LeftGB
       LObject* p_k, uint* k_ovls_j, uint size_k_ovls_j, SD_kStrat strat );
 
   void enterOverlaps
-    ( LObject* J, kStrategy strat, uint** overlaps,
+    ( LObject* J, SD_kStrat strat, uint** overlaps,
       uint* ovl_sizes, int isFromQ, int ecart, int atR );
 
 }
