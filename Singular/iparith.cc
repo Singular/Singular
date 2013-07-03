358f9d7899436a4f0ac58f23dc33e07dce976d44
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2013-07-03 14:23:19 +0200

    add: M[iv,i], M[i,iv], M[iv,iv] for bigintmat M, int i, intvec iv

--- Singular/iparith.cc
+++ Singular/iparith.cc
@@ -5646,6 +5646,8 @@ static BOOLEAN jjBRACK_Ma_I_IV(leftv res, leftv u, leftv v,leftv w)
     memcpy(u,&ut,sizeof(ut));
     if (u->Typ() == MATRIX_CMD)
       nok=jjBRACK_Ma(p,u,v,&t);
+    else if (u->Typ() == BIGINTMAT_CMD)
+      nok=jjBRACK_Bim(p,u,v,&t);
     else /* INTMAT_CMD */
       nok=jjBRACK_Im(p,u,v,&t);
     if (nok)
@@ -5694,6 +5696,8 @@ static BOOLEAN jjBRACK_Ma_IV_I(leftv res, leftv u, leftv v,leftv w)
     memcpy(u,&ut,sizeof(ut));
     if (u->Typ() == MATRIX_CMD)
       nok=jjBRACK_Ma(p,u,&t,w);
+    else if (u->Typ() == BIGINTMAT_CMD)
+      nok=jjBRACK_Bim(p,u,&t,w);
     else /* INTMAT_CMD */
       nok=jjBRACK_Im(p,u,&t,w);
     if (nok)
@@ -5748,6 +5752,8 @@ static BOOLEAN jjBRACK_Ma_IV_IV(leftv res, leftv u, leftv v,leftv w)
       memcpy(u,&ut,sizeof(ut));
       if (u->Typ() == MATRIX_CMD)
         nok=jjBRACK_Ma(p,u,&t1,&t2);
+      else if (u->Typ() == BIGINTMAT_CMD)
+        nok=jjBRACK_Bim(p,u,&t1,&t2);
       else /* INTMAT_CMD */
         nok=jjBRACK_Im(p,u,&t1,&t2);
       if (nok)
