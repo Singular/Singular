6d1057653519f0c13439b7e6d049320456b1ccb8
Martin Lee <martinlee84@web.de>
2013-08-14 17:42:25 +0200

    fix: handling of SW_RATIONAL

--- factory/facAbsBiFact.cc
+++ factory/facAbsBiFact.cc
@@ -222,6 +222,7 @@ int choosePoint (const CanonicalForm& F, int tdegF, CFArray& eval, bool rec)
 CFAFList absBiFactorizeMain (const CanonicalForm& G, bool full)
 {
   CanonicalForm F= bCommonDen (G)*G;
+  bool isRat= isOn (SW_RATIONAL);
   Off (SW_RATIONAL);
   F /= icontent (F);
   On (SW_RATIONAL);
@@ -243,6 +244,7 @@ differentevalpoint:
     p= choosePoint (F, tdegF, eval, rec);
     TIMING_END_AND_PRINT (fac_evalpoint, "time to find eval point: ");
 
+    //after here isOn (SW_RATIONAL)==false
     setCharacteristic (p);
     Fp=F.mapinto();
     factors= factorize (Fp);
@@ -254,6 +256,8 @@ differentevalpoint:
     {
       if (absIrredTest (Fp))
       {
+        if (isRat)
+          On (SW_RATIONAL);
         setCharacteristic(0);
         return CFAFList (CFAFactor (G, 1, 1));
       }
@@ -262,6 +266,8 @@ differentevalpoint:
         setCharacteristic (0);
         if (modularIrredTestWithShift (F))
         {
+          if (isRat)
+            On (SW_RATIONAL);
           return CFAFList (CFAFactor (G, 1, 1));
         }
         rec= true;
@@ -685,6 +691,11 @@ differentevalpoint:
     F= bufF;
     goto differentevalpoint;
   }
+
+  if (isRat)
+    On (SW_RATIONAL);
+  else
+    Off (SW_RATIONAL);
 
   return result;
 }
