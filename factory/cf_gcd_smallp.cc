0b55585558ce0de490624bf1fba3a7be6c134a46
Martin Lee <martinlee84@web.de>
2013-08-26 17:50:31 +0200

    fix: wrong assertions

--- factory/cf_gcd_smallp.cc
+++ factory/cf_gcd_smallp.cc
@@ -2123,6 +2123,9 @@ monicSparseInterpol (const CanonicalForm& F, const CanonicalForm& G,
   if (G.isUnivariate() && fdivides(G, F)) return G/Lc(G);
   if (F == G) return F/Lc(F);
 
+  ASSERT (degree (A, 1) == 0, "expected degree (F, 1) == 0");
+  ASSERT (degree (B, 1) == 0, "expected degree (G, 1) == 0");
+
   CFMap M,N;
   int best_level= myCompress (A, B, M, N, false);
 
@@ -2133,8 +2136,6 @@ monicSparseInterpol (const CanonicalForm& F, const CanonicalForm& G,
   B= M(B);
 
   Variable x= Variable (1);
-  ASSERT (degree (A, x) == 0, "expected degree (F, 1) == 0");
-  ASSERT (degree (B, x) == 0, "expected degree (G, 1) == 0");
 
   //univariate case
   if (A.isUnivariate() && B.isUnivariate())
@@ -2381,6 +2382,9 @@ nonMonicSparseInterpol (const CanonicalForm& F, const CanonicalForm& G,
   if (G.isUnivariate() && fdivides(G, F)) return G/Lc(G);
   if (F == G) return F/Lc(F);
 
+  ASSERT (degree (A, 1) == 0, "expected degree (F, 1) == 0");
+  ASSERT (degree (B, 1) == 0, "expected degree (G, 1) == 0");
+
   CFMap M,N;
   int best_level= myCompress (A, B, M, N, false);
 
@@ -2391,8 +2395,6 @@ nonMonicSparseInterpol (const CanonicalForm& F, const CanonicalForm& G,
   B= M(B);
 
   Variable x= Variable (1);
-  ASSERT (degree (A, x) == 0, "expected degree (F, 1) == 0");
-  ASSERT (degree (B, x) == 0, "expected degree (G, 1) == 0");
 
   //univariate case
   if (A.isUnivariate() && B.isUnivariate())
