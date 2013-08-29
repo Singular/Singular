f68bf8e9ce6bd78ab7a471424eed6803edd6066b
Oleksandr Motsak <motsak@mathematik.uni-kl.de>
2013-08-29 14:18:09 +0200

    Update due to corrected pull request #351

--- Singular/singmathic.cc
+++ Singular/singmathic.cc
@@ -525,6 +525,7 @@ BOOLEAN mathicgb(leftv result, leftv arg)
   return FALSE;
 }
 
+template class std::vector<Exponent>;
 template void mgb::computeGroebnerBasis<MathicToSingStream>
   (mgb::GroebnerInputIdealStream&, MathicToSingStream&);
 
