ecf4a6aec8765816e4b84c145f473f1ff6555d71
Martin Lee <martinlee84@web.de>
2013-10-04 16:16:25 +0200

    chg: added has_simple_Inverse to init in algext and transext

--- libpolys/polys/ext_fields/algext.cc
+++ libpolys/polys/ext_fields/algext.cc
@@ -1417,6 +1417,7 @@ BOOLEAN naInitChar(coeffs cf, void * infoStruct)
   cf->iNumberOfParameters = rVar(R);
   cf->pParameterNames = R->names;
   cf->cfParameter = naParameter;
+  cf->has_simple_Inverse= R->cf->has_simple_Inverse;
 
   if( nCoeff_is_Q(R->cf) )
   {
