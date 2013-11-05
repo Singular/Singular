a76a64e703bae29ffee8057ab2b16abb20f9158a
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2013-11-05 12:50:56 +0100

    chg: defer shutdown caused by SIGTERM if necessary

--- Singular/cntrlc.h
+++ Singular/cntrlc.h
@@ -18,6 +18,10 @@ void init_signals();
 /* TRUE for child of a fork or started with --batch*/
 extern BOOLEAN singular_in_batchmode;
 
+/* for deferring the call of m2_end() in SIGTERM handler if necessary */
+extern volatile BOOLEAN do_shutdown;
+extern volatile int defer_shutdown;
+
 /* If non-zero, do not prompt on SIGINT. Instead, use
  * cntlr_prompt_answer as answer (this could be set to 'a' to skip the
  * prompt and always assume the "abort" answer) . */
--- Singular/misc_ip.cc
+++ Singular/misc_ip.cc
@@ -1067,76 +1067,85 @@ void (*MP_Exit_Env_Ptr)()=NULL;
 #endif
 
 extern "C" {
+/* Note: We cannot use a mutex here because mutexes are not async-safe, but
+ * m2_end is called by sig_term_hdl(). Anyway, the race condition in the first
+ * few lines of m2_end() should not matter.
+ */
+volatile BOOLEAN m2_end_called = FALSE;
 
 void m2_end(int i)
 {
-  fe_reset_input_mode();
-  #ifdef PAGE_TEST
-  mmEndStat();
-  #endif
-  fe_reset_input_mode();
-  if (ssiToBeClosed_inactive)
+  if (!m2_end_called)
   {
-    link_list hh=ssiToBeClosed;
-    while(hh!=NULL)
+    m2_end_called = TRUE;
+    fe_reset_input_mode();
+#ifdef PAGE_TEST
+    mmEndStat();
+#endif
+    fe_reset_input_mode();
+    if (ssiToBeClosed_inactive)
     {
-      //Print("close %s\n",hh->l->name);
-      slPrepClose(hh->l);
-      hh=(link_list)hh->next;
-    }
-    ssiToBeClosed_inactive=FALSE;
-
-    idhdl h = currPack->idroot;
-    while(h != NULL)
-    {
-      if(IDTYP(h) == LINK_CMD)
+      link_list hh=ssiToBeClosed;
+      while(hh!=NULL)
       {
-        idhdl hh=h->next;
-        //Print("kill %s\n",IDID(h));
-        killhdl(h, currPack);
-        h = hh;
+        //Print("close %s\n",hh->l->name);
+        slPrepClose(hh->l);
+        hh=(link_list)hh->next;
+      }
+      ssiToBeClosed_inactive=FALSE;
+
+      idhdl h = currPack->idroot;
+      while(h != NULL)
+      {
+        if(IDTYP(h) == LINK_CMD)
+        {
+          idhdl hh=h->next;
+          //Print("kill %s\n",IDID(h));
+          killhdl(h, currPack);
+          h = hh;
+        }
+        else
+        {
+          h = h->next;
+        }
+      }
+      hh=ssiToBeClosed;
+      while(hh!=NULL)
+      {
+        //Print("close %s\n",hh->l->name);
+        slClose(hh->l);
+        hh=ssiToBeClosed;
+      }
+    }
+    if (!singular_in_batchmode)
+    {
+      if (i<=0)
+      {
+        if (TEST_V_QUIET)
+        {
+          if (i==0)
+            printf("Auf Wiedersehen.\n");
+          else
+            printf("\n$Bye.\n");
+        }
+        //#ifdef sun
+        //  #ifndef __svr4__
+        //    _cleanup();
+        //    _exit(0);
+        //  #endif
+        //#endif
+        i=0;
       }
       else
       {
-        h = h->next;
+        printf("\nhalt %d\n",i);
       }
     }
-    hh=ssiToBeClosed;
-    while(hh!=NULL)
-    {
-      //Print("close %s\n",hh->l->name);
-      slClose(hh->l);
-      hh=ssiToBeClosed;
-    }
+#ifdef HAVE_MPSR
+    if (MP_Exit_Env_Ptr!=NULL) (*MP_Exit_Env_Ptr)();
+#endif
+    exit(i);
   }
-  if (!singular_in_batchmode)
-  {
-    if (i<=0)
-    {
-      if (TEST_V_QUIET)
-      {
-        if (i==0)
-          printf("Auf Wiedersehen.\n");
-        else
-          printf("\n$Bye.\n");
-      }
-      //#ifdef sun
-      //  #ifndef __svr4__
-      //    _cleanup();
-      //    _exit(0);
-      //  #endif
-      //#endif
-      i=0;
-    }
-    else
-    {
-      printf("\nhalt %d\n",i);
-    }
-  }
-  #ifdef HAVE_MPSR
-  if (MP_Exit_Env_Ptr!=NULL) (*MP_Exit_Env_Ptr)();
-  #endif
-  exit(i);
 }
 }
 
