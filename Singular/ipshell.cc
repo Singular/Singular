dc0968ad6a8ec8f3784a57ca270f0c440981fff8
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2013-11-13 10:26:15 +0100

    add: better error message (ring from list)

--- Singular/ipshell.cc
+++ Singular/ipshell.cc
@@ -2440,6 +2440,13 @@ ring rCompose(const lists  L, const BOOLEAN check_comp)
         goto rCompose_err;
       }
     }
+    if (R->block0[j]>R->N)
+    {
+      Werror("not enough variables (%d) for ordering block %d, scanned so far:",R->N,j+1);
+      for(int ii=0;ii<=j;ii++)
+        Werror("ord[%d]: %s from v%d to v%d\n",ii+1,rSimpleOrdStr(R->order[ii]),R->block0[ii],R->block1[ii]);
+      goto rCompose_err;
+    }
     if (check_comp)
     {
       BOOLEAN comp_order=FALSE;
