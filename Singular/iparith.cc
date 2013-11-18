5a097ed0e22afe4b66b603eb41164323d4d60430
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2013-11-18 11:55:59 +0100

    chg: allow digits in package names (during auto-load)

--- Singular/iparith.cc
+++ Singular/iparith.cc
@@ -707,7 +707,7 @@ static BOOLEAN jjCOLCOL(leftv res, leftv u, leftv v)
         if(isupper(u->name[0]))
         {
           const char *c=u->name+1;
-          while((*c!='\0')&&(islower(*c))) c++;
+          while((*c!='\0')&&(islower(*c)||(isdigit(*c)))) c++;
           if (*c!='\0')
             name_err=1;
           else
