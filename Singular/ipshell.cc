edc88a27729d96c18acfbac4d3b4aae2199e9ad4
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2014-04-16 18:02:42 +0200

    fix: setting currRingHdl after rKill

--- Singular/ipshell.cc
+++ Singular/ipshell.cc
@@ -5593,6 +5593,12 @@ void rKill(idhdl h)
     else
     {
       currRingHdl=rFindHdl(r,currRingHdl);
+      if ((currRingHdl==NULL)&&(currRing->idroot==NULL))
+      {
+        for (int i=myynest;i>=0;i--)
+	  if (iiLocalRing[i]==currRing) return;
+        currRing=NULL;
+      }
     }
   }
 }
