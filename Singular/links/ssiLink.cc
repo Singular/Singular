a20c03aa7edd244d8436397e95aed181ed59f745
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2014-05-05 16:39:43 +0200

    fix: ssi attribute p1

--- Singular/links/ssiLink.cc
+++ Singular/links/ssiLink.cc
@@ -786,10 +786,6 @@ void ssiReadBlackbox(leftv res, si_link l)
 void ssiReadAttrib(leftv res, si_link l)
 {
   ssiInfo *d=(ssiInfo*)l->data;
-  leftv tmp=ssiRead1(l);
-  memcpy(res,tmp,sizeof(sleftv));
-  memset(tmp,0,sizeof(sleftv));
-  omFreeSize(tmp,sizeof(sleftv));
   BITSET fl=(BITSET)s_readint(d->f_read);
   int nr_of_attr=s_readint(d->f_read);
   if (nr_of_attr>0)
@@ -797,6 +793,13 @@ void ssiReadAttrib(leftv res, si_link l)
     for(int i=1;i<nr_of_attr;i++)
     {
     }
+  }
+  leftv tmp=ssiRead1(l);
+  memcpy(res,tmp,sizeof(sleftv));
+  memset(tmp,0,sizeof(sleftv));
+  omFreeSize(tmp,sizeof(sleftv));
+  if (nr_of_attr>0)
+  {
   }
   res->flag=fl;
 }
@@ -1431,9 +1434,14 @@ BOOLEAN ssiWrite(si_link l, leftv data)
     int tt=data->Typ();
     void *dd=data->Data();
     attr *aa=data->Attribute();
+    BOOLEAN with_attr=FALSE;
     if (((*aa)!=NULL)||(data->flag!=0))
     {
-      fprintf(d->f_write,"21 %d ",data->flag);
+      attr a=*aa;
+      int n=0;
+      while(a!=NULL) { n++; a=a->next;}
+      fprintf(d->f_write,"21 %d %d ",data->flag,n);
+      a=*aa;
     }
     if ((dd==NULL) && (data->name!=NULL) && (tt==0)) tt=DEF_CMD;
       // return pure undefined names as def
@@ -1532,14 +1540,6 @@ BOOLEAN ssiWrite(si_link l, leftv data)
               return TRUE;
             }
             break;
-    }
-    if (((*aa)!=NULL)||(data->flag!=0))
-    {
-      attr a=*aa;
-      int n=0;
-      while(a!=NULL) { n++; a=a->next;}
-      fprintf(d->f_write,"%d ",n);
-      a=*aa;
     }
     if (d->level<=1) { fputc('\n',d->f_write); fflush(d->f_write); }
     data=data->next;
@@ -2111,7 +2111,7 @@ BOOLEAN ssiGetDump(si_link l)
 // 18 intmat
 // 19 bigintmat <r> <c> ...
 // 20 blackbox <name> 1 <len> ...
-// 21 attrib <bit-attribs> <data> <len> <a-name1> <val1>...
+// 21 attrib <bit-attrib> <len> <a-name1> <val1>... <data>
 //
 // 98: verify version: <ssi-version> <MAX_TOK> <OPT1> <OPT2>
 // 99: quit Singular
