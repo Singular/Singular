f06f5aeae9629f9a35cf2ea539962eb91c619ab5
Martin Lee <martinlee84@web.de>
2014-06-20 12:33:52 +0200

    chg: moved coprimality test and change of representation mod n from cf_gcd to cfGcdUtil

---
+++ factory/cfGcdUtil.h
@@ -0,0 +1,11 @@
+#ifndef CF_GCD_UTIL_H
+#define CF_GCD_UTIL_H
+bool
+gcd_test_one ( const CanonicalForm & f, const CanonicalForm & g, bool swap, int & d );
+
+CanonicalForm
+balance_p ( const CanonicalForm & f, const CanonicalForm & q, const CanonicalForm & qh );
+
+CanonicalForm
+balance_p ( const CanonicalForm & f, const CanonicalForm & q );
+#endif
