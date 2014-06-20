5b4953b0435d257dfff1b9b6848b8c2a28e0f347
Martin Lee <martinlee84@web.de>
2014-06-20 12:32:37 +0200

    chg: moved univariate gcd / extgcd from cf_gcd.cc to cfUnivarGcd

---
+++ factory/cfUnivarGcd.h
@@ -0,0 +1,29 @@
+#ifndef CF_UNIVAR_GCD_H
+#define CF_UNIVAR_GCD_H
+
+#ifdef HAVE_NTL
+#include <NTL/ZZX.h>
+#include "NTLconvert.h"
+bool isPurePoly(const CanonicalForm & );
+#ifndef HAVE_FLINT
+CanonicalForm gcd_univar_ntl0( const CanonicalForm &, const CanonicalForm & );
+CanonicalForm gcd_univar_ntlp( const CanonicalForm &, const CanonicalForm & );
+#endif
+#endif
+
+#ifdef HAVE_FLINT
+#include "FLINTconvert.h"
+CanonicalForm gcd_univar_flint0 (const CanonicalForm &, const CanonicalForm &);
+CanonicalForm gcd_univar_flintp (const CanonicalForm &, const CanonicalForm &);
+#endif
+
+#ifndef HAVE_NTL
+CanonicalForm gcd_poly_univar0( const CanonicalForm & F, const CanonicalForm & G, bool primitive );
+#endif
+
+/*BEGINPUBLIC*/
+CanonicalForm
+extgcd ( const CanonicalForm & f, const CanonicalForm & g, CanonicalForm & a, CanonicalForm & b );
+/*ENDPUBLIC*/
+
+#endif
