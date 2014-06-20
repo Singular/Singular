de19ddf2a986e8f35cb54289bfe42174f8c30b34
Martin Lee <martinlee84@web.de>
2014-06-20 13:18:27 +0200

    chg: moved subresultant gcd from cf_gcd to cfSubResGcd

---
+++ factory/cfSubResGcd.h
@@ -0,0 +1,9 @@
+#ifndef CF_SUB_RES_GCD_H
+#define CF_SUB_RES_GCD_H
+
+CanonicalForm
+gcd_poly_p( const CanonicalForm & f, const CanonicalForm & g );
+
+CanonicalForm
+gcd_poly_0( const CanonicalForm & f, const CanonicalForm & g );
+#endif
