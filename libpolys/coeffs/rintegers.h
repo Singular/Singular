57982a42df697d6d85207ebaed7e6675c458304d
Claus Fieker <fieker@mathematik.uni-kl.de>
2014-07-22 12:34:15 +0200

    "fix" the SI==3 case in rintegers and add the missing bits in rmodulon again Try a short patch to keep it simple

--- libpolys/coeffs/rintegers.h
+++ libpolys/coeffs/rintegers.h
@@ -9,8 +9,17 @@
 #ifdef HAVE_RINGS
 #include <coeffs/coeffs.h>
 
-extern int nrzExp;
-extern NATNUMBER nrzModul;
+#if SI_INTEGER_VARIANT == 3
+#define SR_HDL(A) ((long)(A))
+#define SR_INT    1L
+#define INT_TO_SR(INT)  ((number) (((long)INT << 2) + SR_INT))
+#define SR_TO_INT(SR)   (((long)SR) >> 2)
+#define n_Z_IS_SMALL(A)     (SR_HDL(A) & SR_INT)
+#define INT_IS_SMALL(A) ( ((A << 1) >> 1) == A )
+#endif
+
+//extern int nrzExp;
+//extern NATNUMBER nrzModul;
 
 BOOLEAN nrzInitChar    (coeffs r,  void * parameter);
 number  nrzCopy        (number a, const coeffs r);
