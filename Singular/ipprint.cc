8fec1744a3a9bea1854b58d99979f51ac2c46026
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2014-08-01 17:54:51 +0200

    print(ring/cring)

--- Singular/ipprint.cc
+++ Singular/ipprint.cc
@@ -198,10 +198,33 @@ static BOOLEAN ipPrint_MA(leftv u)
 static BOOLEAN ipPrint_RING(leftv u)
 {
   ring r=(ring)u->Data();
+  PrintS("polynomial ring, over a ");
+  if (rField_is_Ring(r))
+  {
+    if (rField_is_Domain(r)) PrintS("domain");
+    else                     PrintS("ring (with zero-divisors)");
+  }
+  else PrintS("field");
+  if (r->OrdSgn==1) PrintS(", global"); else PrintS(", local/mixed");
+  PrintS(" ordering\n");
   rWrite(r, TRUE);
   return FALSE;
 }
 
+#ifdef SINGULAR_4_1
+static BOOLEAN ipPrint_CRING(leftv u)
+{
+  coeffs r=(coeffs)u->Data();
+  if (nCoeff_is_Ring(r))
+  {
+    if (nCoeff_is_Domain(r)) PrintS("domain: ");
+    else                     PrintS("ring (with zero-divisors): ");
+  }
+  else PrintS("field: ");
+  PrintS(r->cfCoeffName(r));
+  return FALSE;
+}
+#endif
 /*2
 * print for: vector
 */
@@ -274,6 +297,12 @@ BOOLEAN jjPRINT(leftv res, leftv u)
       case QRING_CMD:
         bo=ipPrint_RING(u);
         break;
+
+      #ifdef SINGULAR_4_1
+      case CRING_CMD:
+        bo=ipPrint_CRING(u);
+        break;
+      #endif
 
       default:
         u->Print();
