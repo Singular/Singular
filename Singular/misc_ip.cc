e1f7b90928acd7361bde9165ba1f8a5a8b541fc3
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2014-10-05 17:45:42 +0200

    version stuff: use SINGULAR_VERSION

--- Singular/misc_ip.cc
+++ Singular/misc_ip.cc
@@ -784,7 +784,7 @@ extern "C"
 char * versionString(/*const bool bShowDetails = false*/ )
 {
   StringSetS("");
-  StringAppend("Singular for %s version %s (%s, %d bit) %s #%s",
+  StringAppend("Singular for %s version %s (%d, %d bit) %s #%s",
                S_UNAME, VERSION, // SINGULAR_VERSION,
                SINGULAR_VERSION, SIZEOF_VOIDP*8, singular_date, GIT_VERSION);
   StringAppendS("\nwith\n\t");
