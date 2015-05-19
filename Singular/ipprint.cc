d17626a425a7f885e8beae33d32aabcef9f886eb
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2015-05-19 19:26:40 +0200

    fix: printing of 0xn matrices

--- Singular/ipprint.cc
+++ Singular/ipprint.cc
@@ -62,7 +62,7 @@ static BOOLEAN ipPrint_INTMAT(leftv u)
 */
 static void ipPrint_MA0(matrix m, const char *name)
 {
-  if (MATCOLS(m)>0)
+  if ((MATCOLS(m)>0)&&(MATROWS(m)>0))
   {
     char **s=(char **)omAlloc(MATCOLS(m)*MATROWS(m)*sizeof(char*));
     char *ss;
