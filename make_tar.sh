fd1c508b3b844b14a70a4c459126ca364442e969
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2015-09-29 16:52:23 +0200

    make-<tar.sh: remove some unused stuff from tar

--- make_tar.sh
+++ make_tar.sh
@@ -5,6 +5,9 @@ export VERSION
 git archive --prefix=singular-$VERSION/ HEAD |tar xf -
 mkdir singular-$VERSION/doc
 cp doc/*.man singular-$VERSION/doc/.
+command rm singular-$VERSION/.gdbinit singular-$VERSION/Singular/.gdbinit
+command rm -rf singular-$VERSION/Singular/dyn_modules/bigintm/
+command rm -rf singular-$VERSION/modules
 tar cf singular-$VERSION.tar singular-$VERSION
 gzip -9 -f singular-$VERSION.tar
 command rm -rf singular-$VERSION
