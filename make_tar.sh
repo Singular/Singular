fe8ff2f6758abfd3adc3eeb069f5c72a077f180b
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2015-10-29 15:05:59 +0100

    4-0-2p2: updated emacs files, include singular.hlp/.idx and *.htm

--- make_tar.sh
+++ make_tar.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-VERSION=4.0.2p1
+VERSION=4.0.2p2
 export VERSION
 
 git archive --prefix=singular-$VERSION/ HEAD |tar xf -
 mkdir singular-$VERSION/doc
 cp doc/*.man singular-$VERSION/doc/.
+if test -e doc/html
+then
+  mkdir singular-$VERSION/doc/html
+  cp doc/html/* singular-$VERSION/doc/html/.
+fi
+if test -e doc/singular.idx
+then
+  cp doc/singular.idx singular-$VERSION/doc/.
+  cp doc/singular.hlp singular-$VERSION/doc/.
+fi
+
 command rm singular-$VERSION/.gdbinit singular-$VERSION/*/.gdbinit singular-$VERSION/*/*/.gdbinit singular-$VERSION/*/*/*/.gdbinit
 command rm singular-$VERSION/IntegerProgramming/README
 command rm -rf singular-$VERSION/modules
