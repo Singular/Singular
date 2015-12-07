bcbaa03e6d10ff5892a5e4ba83c2ba12f27ab946
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2015-12-07 16:52:09 +0100

    flint: fmpq_poly for SINGULAR_4_1

---
+++ libpolys/coeffs/flintcf_Q.h
@@ -0,0 +1,20 @@
+/****************************************
+*  Computer Algebra System SINGULAR     *
+****************************************/
+
+/*
+* ABSTRACT: flint: fmpq_poly
+*/
+
+#ifndef FLINTCF_Q_H
+#define FLINTCF_Q_H
+
+#include <misc/auxiliary.h>
+#ifdef SINGULAR_4_1
+#ifdef HAVE_FLINT
+BOOLEAN flintQ_InitChar(coeffs cf, void * infoStruct);
+#endif
+#endif
+
+#endif
+
