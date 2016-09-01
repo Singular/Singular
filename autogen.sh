d4e6a9eab50cd94d6a7a4660b9b587bfe8242de6
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2016-09-01 12:48:24 +0200

    fix: enabling line number in omalloc debug routines

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@ cd `dirname "$0"`
 
 # -d --warnings=all
 autoreconf  -v -f -i
-find . -name configure -exec sed -i "s/-g -O2/-g/g" {} \; -print
+find . -name configure -exec sed -i 's/\"-g -O2\"/\"-g\"/g' {} \; -print
 
 cd -
 
