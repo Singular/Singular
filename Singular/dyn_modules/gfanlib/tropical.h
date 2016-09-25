163a62e5ff6545885674d663b5d4880ed4f488a6
Yue <ren@mathematik.uni-kl.de>
2016-09-25 12:56:04 +0200

    chg: added maximalGroebnerCone to tropical.h

--- Singular/dyn_modules/gfanlib/tropical.h
+++ Singular/dyn_modules/gfanlib/tropical.h
@@ -7,6 +7,7 @@
 extern int tropicalVerboseLevel;
 
 
+gfan::ZCone maximalGroebnerCone(const ideal &I, const ring &r);
 gfan::ZCone homogeneitySpace(ideal I, ring r);
 void tropical_setup(SModulFunctions* p);
 
