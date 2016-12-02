174bcf3262ce524919d5ef06586fee3331f4cd94
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2016-12-02 16:27:32 +0100

    opt: conversion singular -> factory for small int

--- factory/singext.cc
+++ factory/singext.cc
@@ -63,14 +63,22 @@ int gf_value (const CanonicalForm & f )
     return ((intptr_t)ff) >>2;
 }
 
-CanonicalForm
-make_cf ( const mpz_ptr n )
+#ifndef ABS
+#define ABS(x) ((x)<0?(-(x)):(x))
+#endif
+
+CanonicalForm make_cf ( const mpz_ptr n )
 {
+    if (ABS(n->_mp_size)<=1)
+    {
+      long value=mpz_get_si(n);
+      if(value >= MINIMMEDIATE && value <= MAXIMMEDIATE )
+        return CanonicalForm(int2imm( value ));
+    }
     return CanonicalForm( CFFactory::basic( n ) );
 }
 
-CanonicalForm
-make_cf ( const mpz_ptr n, const mpz_ptr d, bool normalize )
+CanonicalForm make_cf ( const mpz_ptr n, const mpz_ptr d, bool normalize )
 {
     return CanonicalForm( CFFactory::rational( n, d, normalize ) );
 }
