ce6598f96dd41ca20e618a48bd95a2d655bab165
Hans Schönemann <hannes@mathematik.uni-kl.de>
2017-05-11 14:12:34 +0200

    better constants for __cplusplus (clang3.7, gcc5.1)

--- omalloc/omalloc.h
+++ omalloc/omalloc.h
@@ -13,7 +13,8 @@
 
 #ifdef __cplusplus
 extern "C" {
-  #if __cplusplus>201402L
+  #if __cplusplus >= 201402L
+  /* clang 3.7, gcc 5.1 sets 201402L */
   #define REGISTER
   #else
   #define REGISTER register
