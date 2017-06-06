3f670141ec98f324e5ec638e4272b9f95a758943
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2017-06-06 13:48:25 +0200

    fix: factory: cfChineseCached

--- factory/cf_chinese.cc
+++ factory/cf_chinese.cc
@@ -261,9 +261,10 @@ static inline CanonicalForm chin_mul_inv(CanonicalForm a, CanonicalForm b, int i
     return inv[ind];
 }
 
+void out_cf(const char *s1,const CanonicalForm &f,const char *s2);
 void chineseRemainderCached(CFArray &a, CFArray &n, CanonicalForm &xnew, CanonicalForm &prod, CFArray &inv)
 {
-  CanonicalForm p, sum(0L); prod(1L);
+  CanonicalForm p, sum=0L; prod=1L;
   int i;
   int len=n.size();
 
