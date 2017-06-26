eb6abdcb6b4e3f9d214c3ba0ea00dbfe5eac287f
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2017-06-26 15:02:41 +0200

    fix: clean DENOMINATOR_LIST when changing/killing the base ring

--- Singular/denom_list.cc
+++ Singular/denom_list.cc
@@ -10,21 +10,21 @@ lists get_denom_list()
   int size;
   denominator_list d = DENOMINATOR_LIST;
   size=0;
-  while(d!=0)
-    {
-      size++;
-      d=d->next;
-    }
+  while(d!=NULL)
+  {
+    size++;
+    d=d->next;
+  }
   lists l=(lists)omAlloc(sizeof(*l));
   l->Init(size);
   d=DENOMINATOR_LIST;
   for (int i=0;i<size;i++)
-    {
-      l->m[i].rtyp=NUMBER_CMD;
-      l->m[i].data=d->n;
-      d=d->next;
-      omFree(DENOMINATOR_LIST);
-      DENOMINATOR_LIST=d;
-    }
+  {
+    l->m[i].rtyp=NUMBER_CMD;
+    l->m[i].data=d->n;
+    d=d->next;
+    omFree(DENOMINATOR_LIST);
+    DENOMINATOR_LIST=d;
+  }
   return l;
 }
