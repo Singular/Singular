43498ec0e457ff84ab9aad9afc957d1a7d1e66b5
Andreas Steenpass <steenpass@mathematik.uni-kl.de>
2017-08-04 17:51:37 +0200

    chg: unify indentation
    Nothing is changed.

--- kernel/GBEngine/syz4.cc
+++ kernel/GBEngine/syz4.cc
@@ -1,9 +1,9 @@
-/****************************************
-*  Computer Algebra System SINGULAR     *
-****************************************/
+/***************************************
+ *  Computer Algebra System SINGULAR   *
+ ***************************************/
 /*
-* ABSTRACT: resolutions
-*/
+ * ABSTRACT: resolutions
+ */
 
 #include <kernel/GBEngine/syz.h>
 #include <omalloc/omalloc.h>
@@ -42,7 +42,7 @@ static void update_variables(std::vector<bool> &variables, const ideal L)
 }
 
 static inline bool check_variables(const std::vector<bool> &variables,
-    const poly m)
+        const poly m)
 {
     const ring R = currRing;
     for (int j = R->N; j > 0; j--) {
@@ -77,83 +77,82 @@ static void initialize_lts_hash(lts_hash &C, const ideal L)
 
 poly FindReducer(const poly multiplier, const poly t, const lts_hash *m_div)
 {
-  const ring r = currRing;
-  lts_hash::const_iterator m_itr = m_div->find(p_GetComp(t, currRing));
-  if (m_itr == m_div->end()) {
-    return NULL;
-  }
-  lts_vector::const_iterator m_current = (m_itr->second).begin();
-  lts_vector::const_iterator m_finish  = (m_itr->second).end();
-  const poly q = p_New(r);
-  pNext(q) = NULL;
-  p_MemSum_LengthGeneral(q->exp, multiplier->exp, t->exp, r->ExpL_Size);
-  const unsigned long m_not_sev = ~p_GetShortExpVector(q, r);
-  for( ; m_current != m_finish; ++m_current) {
-    if (m_current->sev & m_not_sev
-        || unlikely(!(_p_LmDivisibleByNoComp(m_current->lt, q, r)))) {
-        continue;
+    const ring r = currRing;
+    lts_hash::const_iterator m_itr = m_div->find(p_GetComp(t, currRing));
+    if (m_itr == m_div->end()) {
+        return NULL;
     }
-    const poly p = m_current->lt;
-    const int k  = m_current->comp;
-    p_MemAdd_NegWeightAdjust(q, r);
-    p_ExpVectorDiff(q, q, p, r); // (LM(product) / LM(L[k]))
-    p_SetComp(q, k + 1, r);
-    p_Setm(q, r);
-    number n = n_Mult(p_GetCoeff(multiplier, r), p_GetCoeff(t, r), r);
-    p_SetCoeff0(q, n_InpNeg(n, r), r);
-    return q;
-  }
-  p_LmFree(q, r);
-  return NULL;
+    lts_vector::const_iterator m_current = (m_itr->second).begin();
+    lts_vector::const_iterator m_finish  = (m_itr->second).end();
+    const poly q = p_New(r);
+    pNext(q) = NULL;
+    p_MemSum_LengthGeneral(q->exp, multiplier->exp, t->exp, r->ExpL_Size);
+    const unsigned long m_not_sev = ~p_GetShortExpVector(q, r);
+    for( ; m_current != m_finish; ++m_current) {
+        if (m_current->sev & m_not_sev
+                || unlikely(!(_p_LmDivisibleByNoComp(m_current->lt, q, r)))) {
+            continue;
+        }
+        const poly p = m_current->lt;
+        const int k  = m_current->comp;
+        p_MemAdd_NegWeightAdjust(q, r);
+        p_ExpVectorDiff(q, q, p, r); // (LM(product) / LM(L[k]))
+        p_SetComp(q, k + 1, r);
+        p_Setm(q, r);
+        number n = n_Mult(p_GetCoeff(multiplier, r), p_GetCoeff(t, r), r);
+        p_SetCoeff0(q, n_InpNeg(n, r), r);
+        return q;
+    }
+    p_LmFree(q, r);
+    return NULL;
 }
 
 static poly TraverseTail_test(poly multiplier, const int tail,
-   const ideal previous_module, const std::vector<bool> &variables,
-   const lts_hash *m_div);
+        const ideal previous_module, const std::vector<bool> &variables,
+        const lts_hash *m_div);
 
 static inline poly ReduceTerm_test(poly multiplier, poly term4reduction,
-    const ideal previous_module, const std::vector<bool> &variables,
-    const lts_hash *m_div)
+        const ideal previous_module, const std::vector<bool> &variables,
+        const lts_hash *m_div)
 {
-  const ring r = currRing;
-  poly s = FindReducer(multiplier, term4reduction, m_div);
-  if( s == NULL )
-  {
-    return NULL;
-  }
-  const int c = p_GetComp(s, r) - 1;
-  const poly t
-      = TraverseTail_test(s, c, previous_module, variables, m_div);
-  if( t != NULL )
-    s = p_Add_q(s, t, r);
-  return s;
+    const ring r = currRing;
+    poly s = FindReducer(multiplier, term4reduction, m_div);
+    if( s == NULL )
+    {
+        return NULL;
+    }
+    const int c = p_GetComp(s, r) - 1;
+    const poly t = TraverseTail_test(s, c, previous_module, variables, m_div);
+    if( t != NULL )
+        s = p_Add_q(s, t, r);
+    return s;
 }
 
 static poly ComputeImage_test(poly multiplier, const int t,
-    const ideal previous_module, const std::vector<bool> &variables,
-    const lts_hash *m_div)
+        const ideal previous_module, const std::vector<bool> &variables,
+        const lts_hash *m_div)
 {
-  const poly tail = previous_module->m[t]->next;
-  if(tail != NULL)
-  {
-    if (!check_variables(variables, multiplier))
+    const poly tail = previous_module->m[t]->next;
+    if(tail != NULL)
     {
-      return NULL;
+        if (!check_variables(variables, multiplier))
+        {
+            return NULL;
+        }
+        sBucket_pt sum = sBucketCreate(currRing);
+        for(poly p = tail; p != NULL; p = pNext(p))   // iterate over the tail
+        {
+            const poly rt = ReduceTerm_test(multiplier, p, previous_module,
+                    variables, m_div);
+            sBucket_Add_p(sum, rt, pLength(rt));
+        }
+        poly s;
+        int l;
+        sBucketClearAdd(sum, &s, &l);
+        sBucketDestroy(&sum);
+        return s;
     }
-    sBucket_pt sum = sBucketCreate(currRing);
-    for(poly p = tail; p != NULL; p = pNext(p))   // iterate over the tail
-    {
-      const poly rt = ReduceTerm_test(multiplier, p, previous_module,
-          variables, m_div);
-      sBucket_Add_p(sum, rt, pLength(rt));
-    }
-    poly s;
-    int l;
-    sBucketClearAdd(sum, &s, &l);
-    sBucketDestroy(&sum);
-    return s;
-  }
-  return NULL;
+    return NULL;
 }
 
 #define CACHE 1
@@ -161,15 +160,13 @@ static poly ComputeImage_test(poly multiplier, const int t,
 #if CACHE
 struct CCacheCompare_test
 {
-  inline bool operator() (const poly& l, const poly& r)
-    const
-  {
-    return (p_LmCmp(l, r, currRing) == -1);
-  }
+    inline bool operator() (const poly& l, const poly& r) const
+    {
+        return (p_LmCmp(l, r, currRing) == -1);
+    }
 };
 
-typedef std::map<poly, poly, CCacheCompare_test>
-  TP2PCache_test;
+typedef std::map<poly, poly, CCacheCompare_test> TP2PCache_test;
 typedef std::map<int, TP2PCache_test> TCache_test;
 
 static TCache_test m_cache_test;
@@ -179,7 +176,7 @@ static TCache_test m_cache_test;
 static void delete_cache()
 {
     for (TCache_test::iterator it = m_cache_test.begin();
-        it != m_cache_test.end(); it++) {
+            it != m_cache_test.end(); it++) {
         TP2PCache_test& T = it->second;
         for (TP2PCache_test::iterator vit = T.begin(); vit != T.end(); vit++) {
             p_Delete((&(vit->second)), currRing);
@@ -192,77 +189,78 @@ static void delete_cache()
 #endif   // CACHE
 
 static poly TraverseTail_test(poly multiplier, const int tail,
-    const ideal previous_module, const std::vector<bool> &variables,
-    const lts_hash *m_div)
+        const ideal previous_module, const std::vector<bool> &variables,
+        const lts_hash *m_div)
 {
-  const ring& r = currRing;
+    const ring& r = currRing;
 #if CACHE
-  TCache_test::iterator top_itr = m_cache_test.find(tail);
-  if ( top_itr != m_cache_test.end() )
-  {
-     TP2PCache_test& T = top_itr->second;
-     TP2PCache_test::iterator itr = T.find(multiplier);
-     if( itr != T.end() )
-     {
-       if( itr->second == NULL )
-         return (NULL);
-       poly p = p_Copy(itr->second, r);
-       if( !n_Equal( pGetCoeff(multiplier), pGetCoeff(itr->first), r) )
-       {
-         number n = n_Div( pGetCoeff(multiplier), pGetCoeff(itr->first), r);
-         p = p_Mult_nn(p, n, r);
-         n_Delete(&n, r);
-       }
-       return p;
-     }
-     const poly p = ComputeImage_test(multiplier, tail, previous_module,
-         variables, m_div);
-     itr = T.find(multiplier);
-     if( itr == T.end() )
-     {
-       T.insert(TP2PCache_test::value_type(p_Head(multiplier,
-         r), p) );
-       return p_Copy(p, r);
-     }
-     return p;
-  }
-#endif   // CACHE
-  const poly p = ComputeImage_test(multiplier, tail, previous_module,
-      variables, m_div);
-#if CACHE
-  top_itr = m_cache_test.find(tail);
-  if ( top_itr != m_cache_test.end() )
-  {
-    TP2PCache_test& T = top_itr->second;
-    TP2PCache_test::iterator itr = T.find(multiplier);
-    if( itr == T.end() )
+    TCache_test::iterator top_itr = m_cache_test.find(tail);
+    if ( top_itr != m_cache_test.end() )
     {
-      T.insert(TP2PCache_test::value_type(p_Head(multiplier,
-          r), p));
-      return p_Copy(p, r);
+        TP2PCache_test& T = top_itr->second;
+        TP2PCache_test::iterator itr = T.find(multiplier);
+        if( itr != T.end() )
+        {
+            if( itr->second == NULL )
+                return (NULL);
+            poly p = p_Copy(itr->second, r);
+            if( !n_Equal( pGetCoeff(multiplier), pGetCoeff(itr->first), r) )
+            {
+                number n = n_Div( pGetCoeff(multiplier), pGetCoeff(itr->first),
+                        r);
+                p = p_Mult_nn(p, n, r);
+                n_Delete(&n, r);
+            }
+            return p;
+        }
+        const poly p = ComputeImage_test(multiplier, tail, previous_module,
+                variables, m_div);
+        itr = T.find(multiplier);
+        if( itr == T.end() )
+        {
+            T.insert(TP2PCache_test::value_type(p_Head(multiplier,
+                            r), p) );
+            return p_Copy(p, r);
+        }
+        return p;
     }
-    return p;
-  }
-  TP2PCache_test T;
-  T.insert(TP2PCache_test::value_type(p_Head(multiplier, r),
-    p));
-  m_cache_test.insert( TCache_test::value_type(tail, T) );
-  return p_Copy(p, r);
+#endif   // CACHE
+    const poly p = ComputeImage_test(multiplier, tail, previous_module,
+            variables, m_div);
+#if CACHE
+    top_itr = m_cache_test.find(tail);
+    if ( top_itr != m_cache_test.end() )
+    {
+        TP2PCache_test& T = top_itr->second;
+        TP2PCache_test::iterator itr = T.find(multiplier);
+        if( itr == T.end() )
+        {
+            T.insert(TP2PCache_test::value_type(p_Head(multiplier,
+                            r), p));
+            return p_Copy(p, r);
+        }
+        return p;
+    }
+    TP2PCache_test T;
+    T.insert(TP2PCache_test::value_type(p_Head(multiplier, r),
+                p));
+    m_cache_test.insert( TCache_test::value_type(tail, T) );
+    return p_Copy(p, r);
 #else
-  return p;
+    return p;
 #endif   // CACHE
 }
 
 static poly lift_ext_LT(const poly a, const ideal previous_module,
-    const std::vector<bool> &variables, const lts_hash *m_div)
+        const std::vector<bool> &variables, const lts_hash *m_div)
 {
-  const ring R = currRing;
-  poly t1 = ComputeImage_test(a, p_GetComp(a, R)-1,
-      previous_module, variables, m_div);
-  poly t2 = TraverseTail_test(a->next, p_GetComp(a->next, R)-1,
-      previous_module, variables, m_div);
-  t1 = p_Add_q(t1, t2, R);
-  return t1;
+    const ring R = currRing;
+    poly t1 = ComputeImage_test(a, p_GetComp(a, R)-1,
+            previous_module, variables, m_div);
+    poly t2 = TraverseTail_test(a->next, p_GetComp(a->next, R)-1,
+            previous_module, variables, m_div);
+    t1 = p_Add_q(t1, t2, R);
+    return t1;
 }
 
 /*****************************************************************************/
@@ -322,7 +320,7 @@ static poly syzHeadExtFrame(const ideal G, const int i, const int j)
     pSetCoeff0(head, n_Init(1, r->cf));
     poly head_ext = p_Init(r);
     pSetCoeff0(head_ext, n_InpNeg(n_Div(pGetCoeff(f_i), pGetCoeff(f_j), r->cf),
-        r->cf));
+                r->cf));
     long exp_i, exp_j, lcm;
     for (int k = (int)r->N; k > 0; k--) {
         exp_i = p_GetExp(f_i, k, r);
@@ -342,7 +340,7 @@ static poly syzHeadExtFrame(const ideal G, const int i, const int j)
 typedef ideal syzM_i_Function(ideal, int, syzHeadFunction);
 
 static ideal syzM_i_unsorted(const ideal G, const int i,
-    syzHeadFunction *syzHead)
+        syzHeadFunction *syzHead)
 {
     ideal M_i = NULL;
     int comp = pGetComp(G->m[i]);
@@ -366,7 +364,7 @@ static ideal syzM_i_unsorted(const ideal G, const int i,
 }
 
 static ideal syzM_i_sorted(const ideal G, const int i,
-    syzHeadFunction *syzHead)
+        syzHeadFunction *syzHead)
 {
     ideal M_i = NULL;
     int comp = pGetComp(G->m[i]);
@@ -417,13 +415,13 @@ static int compare_Mi(const void* a, const void *b)
     int comp_b = p_GetComp(p_b, r);
     cmp = (comp_a > comp_b) - (comp_a < comp_b);
     if (cmp != 0) {
-         return cmp;
+        return cmp;
     }
     int deg_a = p_Deg(p_a, r);
     int deg_b = p_Deg(p_b, r);
     cmp = (deg_a > deg_b) - (deg_a < deg_b);
     if (cmp != 0) {
-         return cmp;
+        return cmp;
     }
     int exp_a[r->N+1];
     int exp_b[r->N+1];
@@ -439,7 +437,7 @@ static int compare_Mi(const void* a, const void *b)
 }
 
 static ideal computeFrame(const ideal G, syzM_i_Function syzM_i,
-    syzHeadFunction *syzHead)
+        syzHeadFunction *syzHead)
 {
     ideal *M = (ideal *)omalloc((G->ncols-1)*sizeof(ideal));
     for (int i = G->ncols-2; i >= 0; i--) {
@@ -458,7 +456,7 @@ static ideal computeFrame(const ideal G, syzM_i_Function syzM_i,
 }
 
 static void computeLiftings(const resolvente res, const int index,
-    std::vector<bool> &variables)
+        std::vector<bool> &variables)
 {
     if (index > 1) {   // we don't know if the input is a reduced SB
         update_variables(variables, res[index-1]);
@@ -467,7 +465,7 @@ static void computeLiftings(const resolvente res, const int index,
     initialize_lts_hash(*hash_previous_module, res[index-1]);
     for (int j = res[index]->ncols-1; j >= 0; j--) {
         res[index]->m[j]->next->next = lift_ext_LT(res[index]->m[j],
-            res[index-1], variables, hash_previous_module);
+                res[index-1], variables, hash_previous_module);
     }
     delete_lts_hash(hash_previous_module);
     delete(hash_previous_module);
@@ -477,7 +475,7 @@ static void computeLiftings(const resolvente res, const int index,
 }
 
 static int computeResolution(resolvente &res, const int max_index,
-    syzHeadFunction *syzHead)
+        syzHeadFunction *syzHead)
 {
     int index = 0;
     if (!idIs0(res[index]) && index < max_index) {
@@ -539,7 +537,7 @@ syStrategy syFrank(const ideal arg, const int length, const char *method)
     int new_length = computeResolution(res, length-1, syzHead);
     if (new_length < length) {
         res = (resolvente)omReallocSize(res, (length+1)*sizeof(ideal),
-            (new_length+1)*sizeof(ideal));
+                (new_length+1)*sizeof(ideal));
     }
     insert_ext_induced_LTs(res, new_length);
     result->fullres = res;
