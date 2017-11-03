991a3d680308a70780b0f1f8b2e3e38ac46cdf78
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2017-11-03 16:39:20 +0100

    fix: for xalloc: do not call mp_set_memory_functions

--- Singular/tesths.cc
+++ Singular/tesths.cc
@@ -53,13 +53,15 @@ extern int siInit(char *);
 
 int mmInit( void )
 {
-#if defined(OMALLOC_USES_MALLOC) || defined(X_OMALLOC)
+#ifndef X_OMALLOC
+#if defined(OMALLOC_USES_MALLOC)
     /* in mmstd.c, for some architectures freeSize() unconditionally uses the *system* free() */
     /* sage ticket 5344: http://trac.sagemath.org/sage_trac/ticket/5344 */
     /* do not rely on the default in Singular as libsingular may be different */
     mp_set_memory_functions(omMallocFunc,omReallocSizeFunc,omFreeSizeFunc);
 #else
     mp_set_memory_functions(malloc,reallocSize,freeSize);
+#endif
 #endif
   return 1;
 }
