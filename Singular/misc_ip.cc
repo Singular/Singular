89c03b97dade0e1b8d5319126602eca2fec873cf
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2017-11-04 12:40:12 +0100

    fix: minimal memory leak in siInit

--- Singular/misc_ip.cc
+++ Singular/misc_ip.cc
@@ -1326,7 +1326,7 @@ void siInit(char *name)
   basePack=(package)omAlloc0(sizeof(*basePack));
   currPack=basePack;
   idhdl h;
-  h=enterid("Top", 0, PACKAGE_CMD, &IDROOT, TRUE);
+  h=enterid("Top", 0, PACKAGE_CMD, &IDROOT, FALSE);
   IDPACKAGE(h)=basePack;
   IDPACKAGE(h)->language = LANG_TOP;
   currPackHdl=h;
