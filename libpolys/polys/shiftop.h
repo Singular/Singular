8c73fdea988c87d699605750b3180fa69edf5d10
Karim Abou Zeid <karim23697@gmail.com>
2018-06-14 14:25:47 +0200

    Last -> Length

--- libpolys/polys/shiftop.h
+++ libpolys/polys/shiftop.h
@@ -12,6 +12,6 @@ poly shift_pp_Mult_Coeff_mm_DivSelectMult_STUB(poly p,const poly m, const poly a
 poly shift_pp_Mult_Coeff_mm_DivSelect_STUB(poly p, const poly m, int &shorter, const ring r);
 void p_mLPUnShift(poly m, const ring ri);
 void p_LPUnShift(poly p, const ring ri);
-void p_LPExpVappend(int *m1ExpV, int *m2ExpV, int m1Last, int m2Last, const ring ri);
-void p_LPExpVprepend(int *m1ExpV, int *m2ExpV, int m1Last, int m2Last, const ring ri);
+void p_LPExpVappend(int *m1ExpV, int *m2ExpV, int m1Length, int m2Length, const ring ri);
+void p_LPExpVprepend(int *m1ExpV, int *m2ExpV, int m1Length, int m2Length, const ring ri);
 void WriteLPExpV(int *expV, ring ri);
