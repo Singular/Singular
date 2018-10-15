0deadb2ee38d2fe2e741fd6ae19ae7639fd70bb9
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2018-10-15 14:35:48 +0200

    fix: less dep. on Singular/table.h

--- Singular/ipconv.h
+++ Singular/ipconv.h
@@ -7,8 +7,8 @@
 * ABSTRACT: interpreter: converting types
 */
 #include "kernel/structs.h"
-#include "Singular/table.h"
 
+extern const struct sConvertTypes dConvertTypes[];
 int iiTestConvert (int inputType, int outputType, const struct sConvertTypes *dCT=dConvertTypes);
 BOOLEAN iiConvert (int inputType, int outputType, int index,
                    leftv input, leftv output, const struct sConvertTypes *dCT=dConvertTypes);
