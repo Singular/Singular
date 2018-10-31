47e2d7528470d0ede405e76f42cb1942b5025915
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2018-10-31 15:05:02 +0100

    opt: NTL_AVOID_BRANCHING in ff_inv

--- factory/ffops.cc
+++ factory/ffops.cc
@@ -51,8 +51,11 @@ int ff_newinv ( const int a )
       r2 -= r1 * q;
       if (r2 == 1)
       {
-        if (y2 < 0)
-          y2 += p;
+#if defined(NTL_AVOID_BRANCHING)
+        y2 += (y2 >> 31) & p;
+#else
+        if (y2 < 0) y2 += p;
+#endif
         ff_invtab[y2] = a;
         return (ff_invtab[a] = y2);
       }
@@ -61,8 +64,11 @@ int ff_newinv ( const int a )
       r1 -= r2 * q;
       if (r1 == 1)
       {
-        if (y1 < 0)
-          y1 += p;
+#if defined(NTL_AVOID_BRANCHING)
+        y1 += (y1 >> 31) & p;
+#else
+        if (y1 < 0) y1 += p;
+#endif
         ff_invtab[y1] = a;
         return (ff_invtab[a] = y1);
       }
@@ -89,20 +95,24 @@ int ff_biginv ( const int a )
       r2 -= r1 * q;
       if (r2 == 1)
       {
-        if (y2 > 0)
-          return y2;
-        else
-          return p + y2;
+#if defined(NTL_AVOID_BRANCHING)
+        y2 += (y2 >> 31) & p;
+#else
+        if (y2 < 0) y2+=p;
+        return y2;
+#endif
       }
       q = r1 / r2;
       y1 -= y2 * q;
       r1 -= r2 * q;
       if (r1 == 1)
       {
-        if (y1 > 0)
-          return y1;
-        else
-          return p + y1;
+#if defined(NTL_AVOID_BRANCHING)
+        y1 += (y1 >> 31) & p;
+#else
+        if (y1 < 0) y1+=p;
+        return y1;
+#endif
       }
     }
 }
