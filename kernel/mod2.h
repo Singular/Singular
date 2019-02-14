a14e9fc229a9ab5a71fa4b5e709ab718ddfc6b20
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2019-02-14 12:05:52 +0100

    version number: 4.1.2

--- kernel/mod2.h
+++ kernel/mod2.h
@@ -15,10 +15,10 @@
 #define HAVE_SVD
 
 #undef VERSION
-#define VERSION "4.1.1"
+#define VERSION "4.1.2"
 #define SINGULAR_MAJOR_VERSION 4
 #define SINGULAR_MINOR_VERSION 1
-#define SINGULAR_SUB_VERSION 1
+#define SINGULAR_SUB_VERSION 2
 #define S_ROOT_DIR ""
 
 /*******************************************************************
@@ -84,7 +84,7 @@
 #define ALIGN_8
 #endif
 
-#define SINGULAR_PATCHLEVEL 4
+#define SINGULAR_PATCHLEVEL 0
 #define SINGULAR_VERSION ((SINGULAR_MAJOR_VERSION*1000 + SINGULAR_MINOR_VERSION*100 + SINGULAR_SUB_VERSION*10)+SINGULAR_PATCHLEVEL)
 
 /*******************************************************************
