1c9d9824e066f96f9c671ab35777b37f9ba9124f
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2020-01-21 13:08:05 +0100

    adapt to Singulars history (must set SINGULARHIST)

--- Singular/dyn_modules/machinelearning/machinelearning.cc
+++ Singular/dyn_modules/machinelearning/machinelearning.cc
@@ -9,59 +9,66 @@
 
 static BOOLEAN predictHelp(leftv result, leftv arg)
 {
-	char *buffer[5];
-	int lengths[5];
-	char filename[15] = "~/.history";
-	int i;
-	lists L = (lists)omAllocBin(slists_bin);
+        char *buffer[5];
+        int lengths[5];
+        char *filename = getenv("SINGULARHIST");
+	if (filename==NULL) filename=(char*)"~/.history";
+        int i;
+        lists L = (lists)omAllocBin(slists_bin);
 
-	buffer[0] = NULL;
-	buffer[1] = NULL;
-	buffer[2] = NULL;
-	buffer[3] = NULL;
-	buffer[4] = NULL;
+        buffer[0] = NULL;
+        buffer[1] = NULL;
+        buffer[2] = NULL;
+        buffer[3] = NULL;
+        buffer[4] = NULL;
 
-	if (write_history(NULL)) {
-		printf("Failed to write history\n");
-		return TRUE;
-	}
+        if (write_history(filename))
+        {
+                printf("Failed to write history\n");
+                return TRUE;
+        }
 
-	if (!ml_initialise()) {
-		Print("Initialise of ml failed.");
-		PrintLn();
-		/* Notify singular that an error occured */
-		return TRUE;
-	}
+        if (!ml_initialise())
+        {
+                WerrorS("Initialise of ml failed.");
+                /* Notify singular that an error occured */
+                return TRUE;
+        }
 
-	ml_make_prediction(filename, buffer, lengths, _omStrDup);
+        ml_make_prediction(filename, buffer, lengths, _omStrDup);
 
-	L->Init(5);
+        L->Init(5);
 
-	for (i = 0; i < 5; i++) {
-		//printf("prediction %d: %s\n", i, buffer[i]);
-		L->m[i].rtyp = STRING_CMD;
-		L->m[i].data = buffer[i];
-	}
+        for (i = 0; i < 5; i++) {
+                //printf("prediction %d: %s\n", i, buffer[i]);
+                L->m[i].rtyp = STRING_CMD;
+                L->m[i].data = buffer[i];
+        }
 
-	// pass the resultant list to the res datastructure
-	result->rtyp=LIST_CMD;
-	result->data=(void *)L;
+        // pass the resultant list to the res datastructure
+        result->rtyp=LIST_CMD;
+        result->data=(void *)L;
 
-	ml_finalise();
-	return FALSE;
+        ml_finalise();
+        return FALSE;
 
-	//result->rtyp=NONE; // set the result type
+        //result->rtyp=NONE; // set the result type
 }
 
 // initialisation of the module
 extern "C" int SI_MOD_INIT(machinelearning)(SModulFunctions* psModulFunctions)
 {
-	// this is the initialization routine of the module
-	// adding the routine predictHelp:
-	psModulFunctions->iiAddCproc(
-			(currPack->libname? currPack->libname: ""),
-			"predictHelp",// for the singular interpreter
-			FALSE, // should enter the global name space
-			predictHelp); // the C/C++ routine
-	return MAX_TOK;
+        char *filename = getenv("SINGULARHIST");
+	if (filename==NULL)
+	{
+	  WarnS("SINGULARHIST is not set - no history available, using ~/.history");
+	}
+        // this is the initialization routine of the module
+        // adding the routine predictHelp:
+        psModulFunctions->iiAddCproc(
+                        (currPack->libname? currPack->libname: ""),
+                        "predictHelp",// for the singular interpreter
+                        FALSE, // should enter the global name space
+                        predictHelp); // the C/C++ routine
+        return MAX_TOK;
 }
