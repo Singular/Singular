8ef09976cab9319aebf4ecf752611a3085686c52
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2020-05-18 18:24:04 +0200

    fix: module should have extension .so, also for OsX

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@ cd `dirname "$0"`
 # -d --warnings=all
 autoreconf  -v -f -i
 find . -name configure -exec sed -i -e 's/\"-g -O2\"/\"-g\"/g' {} \; -exec chmod +x {} \; -print
+sed -i -e 's/echo .bundle/echo .so/g' libpolys/configure
 touch */configure */configure.ac */aclocal.m4 */configure */Makefile.am */Makefile.in */_config.h.in configure aclocal.m4 Makefile.am Makefile.in _config.h.in
 
 cd -
