5de96dc9e2d1e82cec4f3996f521041242192cf8
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2020-10-16 12:21:14 +0200

    opt: On(SW_USE_QGCD) is required for charseries_l.tst

--- Singular/extra.cc
+++ Singular/extra.cc
@@ -3247,7 +3247,6 @@ static BOOLEAN jjEXTENDED_SYSTEM(leftv res, leftv h)
           Print("EZGCD:%d (use EZGCD for gcd of polynomials in char 0)\n",isOn(SW_USE_EZGCD));
           Print("EZGCD_P:%d (use EZGCD_P for gcd of polynomials in char p)\n",isOn(SW_USE_EZGCD_P));
           Print("CRGCD:%d (use chinese Remainder for gcd of polynomials in char 0)\n",isOn(SW_USE_CHINREM_GCD));
-          Print("QGCD:%d (use QGCD for gcd of polynomials in alg. ext.)\n",isOn(SW_USE_QGCD));
           Print("homog:%d (use homog. test for factorization of polynomials)\n",singular_homog_flag);
           return FALSE;
         }
@@ -3264,7 +3263,6 @@ static BOOLEAN jjEXTENDED_SYSTEM(leftv res, leftv h)
           if (strcmp(s,"EZGCD")==0) { if (d) On(SW_USE_EZGCD); else Off(SW_USE_EZGCD); } else
           if (strcmp(s,"EZGCD_P")==0) { if (d) On(SW_USE_EZGCD_P); else Off(SW_USE_EZGCD_P); } else
           if (strcmp(s,"CRGCD")==0) { if (d) On(SW_USE_CHINREM_GCD); else Off(SW_USE_CHINREM_GCD); } else
-          if (strcmp(s,"QGCD")==0) { if (d) On(SW_USE_QGCD); else Off(SW_USE_QGCD); } else
           if (strcmp(s,"homog")==0) { if (d) singular_homog_flag=1; else singular_homog_flag=0; } else
           return TRUE;
           return FALSE;
