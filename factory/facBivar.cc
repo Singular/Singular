f60ab1f6c47a09dc3472f9d3cabd270622c46327
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2021-01-28 14:34:32 +0100

    fix: absFactorize

--- factory/facBivar.cc
+++ factory/facBivar.cc
@@ -612,7 +612,7 @@ CFList biFactorize (const CanonicalForm& F, const Variable& v)
         break;
     for (CFListIterator iter= uniFactors; iter.hasItem(); iter++)
       iter.getItem()= replacevar (iter.getItem(), vv, v);
-    prune (vv);
+    //prune (vv);
   }
 
   On (SW_RATIONAL);
