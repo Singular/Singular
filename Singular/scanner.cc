ea17839d9e90762aee63762e0df2b1c4ed0dbee6
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2023-10-17 14:31:11 +0200

    freebsd/clang do not like "register" in scanner.cc

--- Singular/scanner.cc
+++ Singular/scanner.cc
@@ -801,9 +801,9 @@ YY_MALLOC_DECL
 
 YY_DECL
 	{
-	register yy_state_type yy_current_state;
-	register char *yy_cp, *yy_bp;
-	register int yy_act;
+	REGISTER yy_state_type yy_current_state;
+	REGISTER char *yy_cp, *yy_bp;
+	REGISTER int yy_act;
 
 #line 121 "scanner.l"
 
@@ -854,7 +854,7 @@ YY_DECL
 yy_match:
 		do
 			{
-			register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
+			REGISTER YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
 			if ( yy_accept[yy_current_state] )
 				{
 				yy_last_accepting_state = yy_current_state;
@@ -1549,9 +1549,9 @@ case YY_STATE_EOF(asstring):
 
 static int yy_get_next_buffer()
 	{
-	register char *dest = yy_current_buffer->yy_ch_buf;
-	register char *source = yytext_ptr;
-	register int number_to_move, i;
+	REGISTER char *dest = yy_current_buffer->yy_ch_buf;
+	REGISTER char *source = yytext_ptr;
+	REGISTER int number_to_move, i;
 	int ret_val;
 
 	if ( yy_c_buf_p > &yy_current_buffer->yy_ch_buf[yy_n_chars + 1] )
@@ -1681,14 +1681,14 @@ static int yy_get_next_buffer()
 
 static yy_state_type yy_get_previous_state()
 	{
-	register yy_state_type yy_current_state;
-	register char *yy_cp;
+	REGISTER yy_state_type yy_current_state;
+	REGISTER char *yy_cp;
 
 	yy_current_state = yy_start;
 
 	for ( yy_cp = yytext_ptr + YY_MORE_ADJ; yy_cp < yy_c_buf_p; ++yy_cp )
 		{
-		register YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
+		REGISTER YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
 		if ( yy_accept[yy_current_state] )
 			{
 			yy_last_accepting_state = yy_current_state;
@@ -1720,10 +1720,10 @@ static yy_state_type yy_try_NUL_trans( yy_current_state )
 yy_state_type yy_current_state;
 #endif
 	{
-	register int yy_is_jam;
-	register char *yy_cp = yy_c_buf_p;
+	REGISTER int yy_is_jam;
+	REGISTER char *yy_cp = yy_c_buf_p;
 
-	register YY_CHAR yy_c = 1;
+	REGISTER YY_CHAR yy_c = 1;
 	if ( yy_accept[yy_current_state] )
 		{
 		yy_last_accepting_state = yy_current_state;
@@ -1744,14 +1744,14 @@ yy_state_type yy_current_state;
 
 #ifndef YY_NO_UNPUT
 #ifdef YY_USE_PROTOS
-static void yyunput( int c, register char *yy_bp )
+static void yyunput( int c, REGISTER char *yy_bp )
 #else
 static void yyunput( c, yy_bp )
 int c;
-VAR register char *yy_bp;
+VAR REGISTER char *yy_bp;
 #endif
 	{
-	register char *yy_cp = yy_c_buf_p;
+	REGISTER char *yy_cp = yy_c_buf_p;
 
 	/* undo effects of setting up yytext */
 	*yy_cp = yy_hold_char;
@@ -1759,10 +1759,10 @@ VAR register char *yy_bp;
 	if ( yy_cp < yy_current_buffer->yy_ch_buf + 2 )
 		{ /* need to shift things up to make room */
 		/* +2 for EOB chars. */
-		register int number_to_move = yy_n_chars + 2;
-		register char *dest = &yy_current_buffer->yy_ch_buf[
+		REGISTER int number_to_move = yy_n_chars + 2;
+		REGISTER char *dest = &yy_current_buffer->yy_ch_buf[
 					yy_current_buffer->yy_buf_size + 2];
-		register char *source =
+		REGISTER char *source =
 				&yy_current_buffer->yy_ch_buf[number_to_move];
 
 		while ( source > yy_current_buffer->yy_ch_buf )
@@ -2223,7 +2223,7 @@ yyconst char *s2;
 VAR int n;
 #endif
 	{
-	register int i;
+	REGISTER int i;
 	for ( i = 0; i < n; ++i )
 		s1[i] = s2[i];
 	}
@@ -2237,7 +2237,7 @@ static int yy_flex_strlen( s )
 yyconst char *s;
 #endif
 	{
-	register int n;
+	REGISTER int n;
 	for ( n = 0; s[n]; ++n )
 		;
 
