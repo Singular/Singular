4cf733b2521a0ad9cb602f3dd16c7a81ab977991
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2024-02-13 15:29:32 +0100

    4.3.2p15

--- kernel/mod2.h
+++ kernel/mod2.h
@@ -19,7 +19,7 @@
 #define SINGULAR_MAJOR_VERSION 4
 #define SINGULAR_MINOR_VERSION 3
 #define SINGULAR_SUB_VERSION 2
-#define SINGULAR_PATCHLEVEL 14
+#define SINGULAR_PATCHLEVEL 15
 #define S_ROOT_DIR ""
 
 /*******************************************************************
