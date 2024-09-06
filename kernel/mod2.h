8548498e6cd973aa99711d9094d796d65c8b567b
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2024-09-06 09:51:22 +0200

    assprimeszerodim_l crashed on munk sometimes

--- kernel/mod2.h
+++ kernel/mod2.h
@@ -61,7 +61,8 @@
 
 /* define for parallel processes with shared memory */
 #ifndef __CCYGWIN__
-  #if (!defined(__APPLE__)) ||(!defined(__x86_64__))
+  //#if (!defined(__APPLE__)) ||(!defined(__x86_64__))
+  #if (!defined(__APPLE__))
     #define HAVE_VSPACE 1
   #endif
 #endif
