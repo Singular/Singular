ffefd38633b4dbf6ab0441d603014c9aff9b2fcc
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2024-09-10 14:54:34 +0200

    VSPACE on MacOS

--- kernel/mod2.h
+++ kernel/mod2.h
@@ -61,7 +61,7 @@
 
 /* define for parallel processes with shared memory */
 #ifndef __CCYGWIN__
-  #if (!defined(__APPLE__)) ||(!defined(__x86_64__))
+  #if (!defined(__APPLE__))
     #define HAVE_VSPACE 1
   #endif
 #endif
